6d67bc8090d112325d6fefc8729fda7acafd0a22
Steven Peters <scpeters@osrfoundation.org>
2013-07-30 15:17:09 -0700

    sdformat.rb: Update to 1.4.5
    Update to 1.4.5 version, which has some bug fixes.

--- sdformat.rb
+++ sdformat.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Sdformat < Formula
   homepage 'http://gazebosim.org/sdf.html'
-  url 'http://gazebosim.org/assets/distributions/sdformat-1.4.4.tar.bz2'
-  sha1 '090ea7cc3051f1f9bc4092c10a5b3097bddf0929'
+  url 'http://gazebosim.org/assets/distributions/sdformat-1.4.5.tar.bz2'
+  sha1 '278b6013fca4ecb054e998cd46c833df630d6a67'
 
   depends_on 'boost'
   depends_on 'cmake' => :build
