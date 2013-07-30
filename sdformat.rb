357903c407db314223cc51afe86561d40f35b05a
Steven Peters <scpeters@osrfoundation.org>
2013-07-30 14:47:00 -0700

    sdformat.rb: added test
    Test with pkg-config

--- sdformat.rb
+++ sdformat.rb
@@ -27,11 +27,6 @@ class Sdformat < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test sdformat`.
-    system "false"
+    system "pkg-config --modversion sdformat"
   end
 end
