7ab1de8a5c4a0d7fe006be083865530a80743b26
Steven Peters <scpeters@osrfoundation.org>
2013-07-30 12:12:16 -0700

    New formula: sdformat.rb
    First pass at sdformat 1.4.0

---
+++ sdformat.rb
@@ -0,0 +1,37 @@
+require 'formula'
+
+class Sdformat < Formula
+  homepage 'http://gazebosim.org/sdf.html'
+  url 'http://gazebosim.org/assets/distributions/sdformat-1.4.0.tar.bz2'
+  sha1 '5e4a79ee29792b7be41b2db557dc6260fa2909e0'
+
+  depends_on 'boost'
+  depends_on 'cmake' => :build
+  depends_on 'doxygen'
+  depends_on 'tinyxml'
+
+  def install
+    ENV.m64
+
+    cmake_args = [
+      "-DCMAKE_BUILD_TYPE='Release'",
+      "-DCMAKE_INSTALL_PREFIX='#{prefix}'",
+      "-Wno-dev"
+    ]
+    cmake_args << ".."
+
+    mkdir "build" do
+      system "cmake", *cmake_args
+      system "make install"
+    end
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test sdformat`.
+    system "false"
+  end
+end
