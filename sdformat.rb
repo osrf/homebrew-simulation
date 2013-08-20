de69940119aae1d33c7cafd2ade051050859cf22
Steven Peters <scpeters@osrfoundation.org>
2013-08-20 15:14:16 -0700

    sdformat: update to 1.4.6
    Update to 1.4.6, adds tags for gps, audio, wireless sensors.

--- sdformat.rb
+++ sdformat.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Sdformat < Formula
   homepage 'http://gazebosim.org/sdf.html'
-  url 'http://gazebosim.org/assets/distributions/sdformat-1.4.5.tar.bz2'
-  sha1 '278b6013fca4ecb054e998cd46c833df630d6a67'
+  url 'http://gazebosim.org/assets/distributions/sdformat-1.4.6.tar.bz2'
+  sha1 'b170321f25566f66b6d383a7bf23987c0f6a50d7'
 
   depends_on 'boost'
   depends_on 'cmake' => :build
