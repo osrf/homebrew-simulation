0b33cd4e97eb7d42cb18a9f32b997491c015861a
Steven Peters <scpeters@osrfoundation.org>
2013-10-01 22:56:42 -0700

    sdformat: add HEAD based on sdf_1.4 branch

--- sdformat.rb
+++ sdformat.rb
@@ -4,6 +4,7 @@ class Sdformat < Formula
   homepage 'http://gazebosim.org/sdf.html'
   url 'http://gazebosim.org/assets/distributions/sdformat-1.4.8.tar.bz2'
   sha1 'c690874edee1b37efc61382d1dcd5cc1eb892a59'
+  head 'https://bitbucket.org/osrf/sdformat', :branch => 'sdf_1.4', :using => :hg
 
   depends_on 'boost'
   depends_on 'cmake' => :build
