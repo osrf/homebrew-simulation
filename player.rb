856869714a369c895bd388a4f0b51d06800fe9a6
Steven Peters <scpeters@osrfoundation.org>
2014-02-17 15:09:33 -0700

    Player 3.0.2 formula without dependencies

---
+++ player.rb
@@ -0,0 +1,14 @@
+require "formula"
+
+class Player < Formula
+  homepage "http://playerstage.sourceforge.net"
+  url "https://downloads.sourceforge.net/project/playerstage/Player/3.0.2/player-3.0.2.tar.gz"
+  sha1 "34931ca57148db01202afd08fdc647cc5fdc884c"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+end
