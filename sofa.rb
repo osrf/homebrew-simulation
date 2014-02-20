333ee6a14098d40292cf6d603e45ad1ca0d9df81
Steven Peters <scpeters@osrfoundation.org>
2014-02-20 09:14:17 -0700

    Sofa framework
    Add formula for sofa framework, an open source framework
    for simulating soft bodies in medical contexts.

---
+++ sofa.rb
@@ -0,0 +1,19 @@
+require "formula"
+
+class Sofa < Formula
+  homepage "http://www.sofa-framework.org"
+  head "svn://scm.gforge.inria.fr/svn/sofa/trunk/Sofa"
+
+  depends_on "cmake" => :build
+  depends_on :x11
+  depends_on 'glew'
+  depends_on 'libpng'
+  depends_on 'qt' => 'with-qt3support'
+
+  def install
+    # For some reason, cmake must be invoked twice
+    system "cmake", ".", *std_cmake_args
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+end
