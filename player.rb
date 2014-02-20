30ffb52bd45c6bd2f39c42ef3dc90ef835265a35
Steven Peters <scpeters@osrfoundation.org>
2014-02-20 01:59:22 -0700

    Player: add head

--- player.rb
+++ player.rb
@@ -4,6 +4,7 @@ class Player < Formula
   homepage "http://playerstage.sourceforge.net"
   url "https://downloads.sourceforge.net/project/playerstage/Player/3.0.2/player-3.0.2.tar.gz"
   sha1 "34931ca57148db01202afd08fdc647cc5fdc884c"
+  head "http://svn.code.sf.net/p/playerstage/svn/code/player/trunk"
 
   depends_on "cmake" => :build
 
