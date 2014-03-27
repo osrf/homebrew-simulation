20d92db20d67c0d97ec24393e08bc6261ede2eeb
Steven Peters <scpeters@osrfoundation.org>
2014-03-27 14:25:22 -0700

    Gazebo 1.9.5: simplify formula

--- gazebo.rb
+++ gazebo.rb
@@ -4,7 +4,7 @@ class Gazebo < Formula
   homepage 'http://gazebosim.org'
   url 'http://gazebosim.org/assets/distributions/gazebo-1.9.5.tar.bz2'
   sha1 '1bf5d66a402f0b1e8e43a62e4f13e9b4f7d727b7'
-  head 'https://bitbucket.org/osrf/gazebo', :branch => 'default', :using => :hg
+  head 'https://bitbucket.org/osrf/gazebo', :branch => 'gazebo_1.9', :using => :hg
 
   depends_on 'cmake'  => :build
   depends_on 'pkg-config' => :build
@@ -22,39 +22,27 @@ class Gazebo < Formula
   depends_on 'tinyxml'
 
   depends_on 'bullet' => [:optional, 'shared', 'double-precision']
-  depends_on 'dartsim/dart/dartsim' => [:optional, 'core-only']
   depends_on 'ffmpeg' => :optional
   depends_on 'player' => :optional
-  depends_on 'simbody' => :optional
   # can't figure out how to specify optional gem dependency
   #depends_on 'ronn' => [:ruby, :optional]
 
   def patches
-    patches = [
+    [
       'https://gist.githubusercontent.com/scpeters/9199370/raw/afe595587e38737c537124a3652db99de026c272/brew_python_fix.patch',
     ]
-    if build.head?
-      patches << 'https://gist.githubusercontent.com/scpeters/9199351/raw/6c90b487def89bff54ad5ad0688110d806063aa0/disable_gdal.patch'
-    end
-    patches
   end
 
   def install
     ENV.m64
 
-    cmake_args = [
-      "-DCMAKE_BUILD_TYPE='Release'",
-      "-DCMAKE_INSTALL_PREFIX='#{prefix}'",
-      "-DCMAKE_FIND_FRAMEWORK=LAST",
-      "-Wno-dev",
-      "-DENABLE_TESTS_COMPILATION:BOOL=False",
-      "-DFORCE_GRAPHIC_TESTS_COMPILATION:BOOL=True",
-    ]
-    #cmake_args.concat(std_cmake_args)
-    cmake_args << ".."
+    cmake_args = std_cmake_args.select { |arg| arg.match(/CMAKE_BUILD_TYPE/).nil? }
+    cmake_args << "-DCMAKE_BUILD_TYPE=Release"
+    cmake_args << "-DENABLE_TESTS_COMPILATION:BOOL=False"
+    cmake_args << "-DFORCE_GRAPHIC_TESTS_COMPILATION:BOOL=True"
 
     mkdir "build" do
-      system "cmake", *cmake_args
+      system "cmake", "..", *cmake_args
       system "make install"
     end
   end
