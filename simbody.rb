cc5e515505041ea857968102c773b145bd4c3d25
Steven Peters <scpeters@osrfoundation.org>
2014-03-27 13:25:17 -0700

    Simbody 3.4

--- simbody.rb
+++ simbody.rb
@@ -2,42 +2,19 @@ require 'formula'
 
 class Simbody < Formula
   homepage 'https://simtk.org/home/simbody'
-  url 'https://github.com/simbody/simbody/archive/Simbody-3.3.zip'
-  sha1 'a67ee802fb410a1c346718a48ac0ede2f7fa56ea'
+  url 'https://github.com/simbody/simbody/archive/Simbody-3.4.zip'
+  sha1 'd9ef925167474601b49140c5557b84ffa701bfd2'
   head 'https://github.com/simbody/simbody.git', :branch => 'master'
 
   depends_on 'cmake' => :build
 
-  # fix install location of cmake config files
-  def patches; DATA unless build.head?; end
-
   def install
     ENV.m64
 
-    cmake_args = [
-      "-DSimTK_INSTALL_PREFIX='#{prefix}'",
-    ]
-    cmake_args.concat(std_cmake_args)
-    cmake_args << ".."
-
     mkdir "build" do
-      system "cmake", *cmake_args
-      system "make", "doxygen" if build.head?
+      system "cmake", "..", *std_cmake_args
+      system "make", "doxygen"
       system "make", "install"
     end
   end
-
 end
-
-__END__
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 8968e2f..0a9b1b1 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -557,4 +557,5 @@ FILE(GLOB TOPLEVEL_DOCS doc/*.pdf doc/*.txt)
- INSTALL(FILES ${TOPLEVEL_DOCS} DESTINATION doc)
-
- FILE(GLOB INSTALLED_CMAKE_STUFF cmake/*.cmake cmake/*.txt)
--INSTALL(FILES ${INSTALLED_CMAKE_STUFF} DESTINATION share/cmake)
-+INSTALL(FILES ${INSTALLED_CMAKE_STUFF} DESTINATION share/Simbody/cmake)
-+INSTALL(FILES cmake/FindSimbody.cmake DESTINATION share/Simbody/cmake/ RENAME SimbodyConfig.cmake)
