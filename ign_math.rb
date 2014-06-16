5af114c71442eeac419589bc43a674c72c068752
Steven Peters <scpeters@osrfoundation.org>
2014-06-16 16:12:38 -0700

    ign_math: head-only formula

---
+++ ign_math.rb
@@ -0,0 +1,19 @@
+require "formula"
+
+class IgnMath < Formula
+  head "https://bitbucket.org/ignitionrobotics/ign_math", :branch => 'default', :using => :hg
+
+  depends_on "cmake" => :build
+  depends_on "doxygen" => [:build, :optional]
+
+  def install
+    cmake_args = std_cmake_args.select { |arg| arg.match(/CMAKE_BUILD_TYPE/).nil? }
+    cmake_args << "-DCMAKE_BUILD_TYPE=Release"
+    system "cmake", ".", *cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
