7eca8c3bed6ca1a85655b98c805c3a319982c994
Steven Peters <scpeters@osrfoundation.org>
2014-06-24 00:57:41 -0700

    robot_msgs: use default branch for head

--- robot_msgs.rb
+++ robot_msgs.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class RobotMsgs < Formula
   homepage "https://bitbucket.org/osrf/robot_msgs"
-  url 'http://www.gazebosim.org/assets/distributions/robot-msgs-0.2.0.tar.bz2'
+  url 'http://gazebosim.org/assets/distributions/robot-msgs-0.2.0.tar.bz2'
   sha1 '38c1460a56ec9d91fe7f4fdf0fbe63baa412184a'
-  head "https://bitbucket.org/osrf/robot_msgs", :branch => 'c++11', :using => :hg
+  head "https://bitbucket.org/osrf/robot_msgs", :branch => 'default', :using => :hg
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
