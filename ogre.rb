a042e6cda69a2ac4ac80fa7aa55c21a321984e73
Steven Peters <scpeters@osrfoundation.org>
2014-06-30 14:34:06 -0700

    ogre: use downloads.sourceforge.net url per brew audit

--- ogre.rb
+++ ogre.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Ogre < Formula
   homepage 'http://www.ogre3d.org/'
-  url 'https://sourceforge.net/projects/ogre/files/ogre/1.7/ogre_src_v1-7-4.tar.bz2'
+  url 'http://downloads.sourceforge.net/project/ogre/ogre/1.7/ogre_src_v1-7-4.tar.bz2'
   version '1.7.4'
   sha1 'e989b96eacc2c66f8cf8a19dae6cfd962a165207'
   head 'https://bitbucket.org/sinbad/ogre', :branch => 'v1-9', :using => :hg
