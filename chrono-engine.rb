53300ea9b12ea56cbdc9d333b8118aab2b375fbc
Steven Peters <scpeters@osrfoundation.org>
2014-08-20 10:27:14 -0400

    chrono-engine: add patch to fix irrlicht demos
    The irrlicht demos can't get the correct path to the
    data folder used to store textures and meshes.
    This uses an absolute path instead of a relative path.
    Submitted upstream in:
    https://github.com/projectchrono/chrono/pull/25

--- chrono-engine.rb
+++ chrono-engine.rb
@@ -14,6 +14,9 @@ class ChronoEngine < Formula
     cause 'HACD problem'
   end
 
+  # Fix irrlicht demos: incorrect path to data folder
+  patch :DATA if build.head?
+
   def install
     cmake_args = std_cmake_args
     cmake_args << '-DENABLE_UNIT_IRRLICHT=True' if build.with? 'irrlicht' and build.head?
@@ -34,3 +37,46 @@ class ChronoEngine < Formula
     return s.empty? ? nil : s
   end
 end
+__END__
+diff --git a/src/CMakeLists.txt b/src/CMakeLists.txt
+index b1a1f43..839cf44 100644
+--- a/src/CMakeLists.txt
++++ b/src/CMakeLists.txt
+@@ -199,6 +199,8 @@ ELSEIF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+        endif()
+     SET (CPACK_SYSTEM_NAME "OSX-x64")
+ ENDIF()
++SET (CHRONO_DATA_PATH ${CMAKE_INSTALL_PREFIX}/data/)
++SET (CH_BUILDFLAGS "${CH_BUILDFLAGS} -DCHRONO_DATA_PATH=\"${CHRONO_DATA_PATH}\"")
+ 
+ #-----------------------------------------------------------------------------
+ # Some global  C++ definitions and includes that are used 
+diff --git a/src/demos/irrlicht/CMakeLists.txt b/src/demos/irrlicht/CMakeLists.txt
+index 8bcb338..3e2f7b8 100644
+--- a/src/demos/irrlicht/CMakeLists.txt
++++ b/src/demos/irrlicht/CMakeLists.txt
+@@ -50,7 +50,7 @@ FOREACH(PROGRAM ${DEMOS})
+ 
+   SET_TARGET_PROPERTIES(${PROGRAM}  PROPERTIES
+     FOLDER demos
+-    COMPILE_FLAGS "${CH_BUILDFLAGS}"
++    COMPILE_FLAGS "${CH_BUILDFLAGS} -DDATA_PREFIX=${CMAKE_INSTALL_PREFIX}/data"
+     LINK_FLAGS "${CH_LINKERFLAG_EXE}"
+     )
+ 
+diff --git a/src/physics/ChGlobal.cpp b/src/physics/ChGlobal.cpp
+index 1e46aa4..7be7e76 100644
+--- a/src/physics/ChGlobal.cpp
++++ b/src/physics/ChGlobal.cpp
+@@ -84,7 +84,9 @@ int GetUniqueIntID()
+ // Functions for manipulating the Chrono data directory
+ // -----------------------------------------------------------------------------
+ 
+-static std::string chrono_data_path("../data/");
++#define STRINGIFY(s) XSTR(s)
++#define XSTR(s) #s
++static std::string chrono_data_path(STRINGIFY(CHRONO_DATA_PATH));
+ 
+ // Set the path to the Chrono data directory (ATTENTION: not thread safe)
+ void SetChronoDataPath(const std::string& path)
+
