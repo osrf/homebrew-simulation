5e568e66f7f69e91d6546f69302ac65a594596e8
Steven Peters <scpeters@osrfoundation.org>
2014-12-18 15:22:36 -0800

    ignition-transport 0.4.4
    Also use regular protobuf formula instead of our fork

--- ignition-transport.rb
+++ ignition-transport.rb
@@ -2,13 +2,15 @@ require "formula"
 
 class IgnitionTransport < Formula
   homepage "http://ignitionrobotics.org"
+  url 'http://gazebosim.org/assets/distributions/ignition-transport-0.4.4.tar.bz2'
+  sha1 '301fdd12b1901d3760ce570e920ae61e261fbd7b'
   head "https://bitbucket.org/ignitionrobotics/ign-transport", :branch => 'default', :using => :hg
 
   depends_on "cmake" => :build
   depends_on "doxygen" => [:build, :optional]
   depends_on "pkg-config" => :build
 
-  depends_on "osrf/simulation/protobuf" => "c++11"
+  depends_on "protobuf"
   depends_on "protobuf-c" => :build
   depends_on "ossp-uuid"
   depends_on "zeromq"
