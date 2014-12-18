c958ad6ab6370e5be97cda032417531869051b9e
Steven Peters <scpeters@osrfoundation.org>
2014-12-18 11:49:42 -0800

    simbody 3.5

--- simbody.rb
+++ simbody.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Simbody < Formula
   homepage 'https://simtk.org/home/simbody'
-  url 'https://github.com/simbody/simbody/archive/Simbody-3.4.1.tar.gz'
-  sha1 'd15501f3d0782b48fdfde0ad3c7ec261df13ba54'
+  url 'https://github.com/simbody/simbody/archive/Simbody-3.5.tar.gz'
+  sha1 'b2724c75c399865e1b9112db6c1cf785177591cd'
   head 'https://github.com/simbody/simbody.git', :branch => 'master'
 
   depends_on 'cmake' => :build
