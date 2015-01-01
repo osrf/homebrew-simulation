c601c2270b731249fc58af599c2f8cdb841052b9
Steven Peters <scpeters@osrfoundation.org>
2015-01-01 00:24:32 -0800

    simbody: fix sha1 for 3.5.1

--- simbody.rb
+++ simbody.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Simbody < Formula
   homepage 'https://simtk.org/home/simbody'
   url 'https://github.com/simbody/simbody/archive/Simbody-3.5.1.tar.gz'
-  sha1 'd11eeecee8fe5e13e885a38c087843fce3399d19'
+  sha1 '7db86ae232c579527a8bae6103316f68e2d99ba0'
   head 'https://github.com/simbody/simbody.git', :branch => 'master'
 
   depends_on 'cmake' => :build
