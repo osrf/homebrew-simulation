407f97df53cd6627e4814bb28f1a3a7e7ec13ee5
Steven Peters <scpeters@osrfoundation.org>
2015-03-03 16:03:37 -0800

    sdformat 2.3.1
    Also reorder dependencies and don't use external URDF for now,

--- sdformat.rb
+++ sdformat.rb
@@ -2,23 +2,23 @@ require 'formula'
 
 class Sdformat < Formula
   homepage 'http://gazebosim.org/sdf.html'
-  url 'http://gazebosim.org/distributions/sdformat/releases/sdformat-2.3.0.tar.bz2'
-  sha1 '604cb8a3a90d426abc7178dc90ba0cd884867473'
-  head 'https://bitbucket.org/osrf/sdformat', :branch => 'sdf_2.0', :using => :hg
+  url 'http://gazebosim.org/distributions/sdformat/releases/sdformat-2.3.1.tar.bz2'
+  sha1 '18a7dca5fb406106e8fc644d45c4feee923ef5e1'
+  head 'https://bitbucket.org/osrf/sdformat', :branch => 'sdf_2.3', :using => :hg
 
-  depends_on 'boost'
   depends_on 'cmake' => :build
   depends_on 'pkg-config' => :build
+
+  depends_on 'boost'
+  depends_on 'doxygen'
   depends_on 'ros/deps/urdfdom'
   depends_on 'ros/deps/urdfdom_headers'
-  depends_on 'doxygen'
   depends_on 'tinyxml'
 
   def install
     ENV.m64
 
     cmake_args = [
-      "-DUSE_EXTERNAL_URDF:BOOL=True",
       "-DCMAKE_BUILD_TYPE='Release'",
       "-DCMAKE_INSTALL_PREFIX='#{prefix}'",
       "-Wno-dev"
