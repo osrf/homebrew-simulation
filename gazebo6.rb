ec0df85a269fdb747b57b73ac810680c228cf6c0
Steven Peters <scpeters@osrfoundation.org>
2015-03-21 01:24:47 -0700

    gazebo6: 6.0.2prerelease

---
+++ gazebo6.rb
@@ -0,0 +1,51 @@
+require 'formula'
+
+class Gazebo6 < Formula
+  homepage 'http://gazebosim.org'
+  url 'http://gazebosim.org/distributions/gazebo/releases/gazebo6-prerelease-6.0.2.tar.bz2'
+  sha1 '4d9d20f9c5aaadaf1d69681d880d56c9923aad1d'
+  version '6.0.2prerelease'
+  head 'https://bitbucket.org/osrf/gazebo', :branch => 'default', :using => :hg
+
+  depends_on 'cmake'  => :build
+  depends_on 'pkg-config' => :build
+
+  depends_on 'boost'
+  depends_on 'doxygen'
+  depends_on 'freeimage'
+  depends_on 'libtar'
+  depends_on 'ogre'
+  depends_on 'protobuf'
+  depends_on 'protobuf-c'
+  depends_on 'qt'
+  depends_on 'sdformat3'
+  depends_on 'tbb'
+  depends_on 'tinyxml'
+
+  depends_on 'bullet' => [:recommended, 'shared', 'double-precision']
+  depends_on 'dartsim/dart/dartsim' => [:optional, 'core-only']
+  depends_on 'ffmpeg' => :optional
+  depends_on 'gdal' => :optional
+  depends_on 'gts' => :optional
+  depends_on 'player' => :optional
+  depends_on 'simbody' => :recommended
+
+  patch do
+    # Fix build when homebrew python is installed
+    url 'https://gist.githubusercontent.com/scpeters/9199370/raw/afe595587e38737c537124a3652db99de026c272/brew_python_fix.patch'
+    sha1 'eaa6f843ab1264810c0c0a81ff3c52232fd49d12'
+  end
+
+  def install
+    ENV.m64
+
+    cmake_args = std_cmake_args.select { |arg| arg.match(/CMAKE_BUILD_TYPE/).nil? }
+    cmake_args << "-DCMAKE_BUILD_TYPE=Release"
+    cmake_args << "-DENABLE_TESTS_COMPILATION:BOOL=False"
+
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make install"
+    end
+  end
+end
