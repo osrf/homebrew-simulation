ddc56e9da3bfb8bbe0cb01bb1a06b536e6e9dd91
Steven Peters <scpeters@osrfoundation.org>
2015-05-29 23:00:18 -0700

    ignition-math2 2.1.1

--- ignition-math2.rb
+++ ignition-math2.rb
@@ -1,7 +1,7 @@
 class IgnitionMath2 < Formula
   homepage "http://ignitionrobotics.org"
-  url "http://gazebosim.org/distributions/ign-math/releases/ignition-math2-2.0.3.tar.bz2"
-  sha256 "27cf03eaf998f5d153b749372d8e968ce13e6fa481611688f0deb8935a2be5fa"
+  url "http://gazebosim.org/distributions/ign-math/releases/ignition-math2-2.1.1.tar.bz2"
+  sha256 "629f5e65af960e6d6773062984b0c0c6c977c7d2245eba9979db17a9f17058fa"
   head "https://bitbucket.org/ignitionrobotics/ign-math", :branch => "default", :using => :hg
 
   depends_on "cmake" => :build
