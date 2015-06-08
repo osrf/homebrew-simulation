3b22f9a0db1a83946db432438b4dd3196ec7e708
Steven Peters <scpeters@osrfoundation.org>
2015-06-08 16:49:59 -0700

    sdformat3: 3.0.5prerelease

--- sdformat3.rb
+++ sdformat3.rb
@@ -1,8 +1,8 @@
 class Sdformat3 < Formula
   homepage "http://sdformat.org"
-  url "http://gazebosim.org/distributions/sdformat/releases/sdformat3-prerelease-3.0.4.tar.bz2"
-  sha256 "bab3957e3c0f19dceed67a4471e0c110cb0e99e5636ede6887491e496215e2d0"
-  version "3.0.4prerelease"
+  url "http://gazebosim.org/distributions/sdformat/releases/sdformat3-prerelease-3.0.5.tar.bz2"
+  sha256 "c4d8c79dcf5a754127c07ad93451ce3976312d7c9bc4a648e8a6d4f9aff7b3a3"
+  version "3.0.5prerelease"
   head "https://bitbucket.org/osrf/sdformat", :branch => "default", :using => :hg
 
   depends_on "cmake" => :build
