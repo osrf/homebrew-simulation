ec811b6965445e1be1eb75cd19fddd758cd99711
Steven Peters <scpeters@osrfoundation.org>
2015-09-23 13:31:40 -0700

    cppzmq: fix style and audit complaints

--- cppzmq.rb
+++ cppzmq.rb
@@ -1,11 +1,10 @@
-require 'formula'
-
 class Cppzmq < Formula
-  homepage 'https://github.com/zeromq/cppzmq'
-  url 'https://github.com/zeromq/cppzmq.git'
-  version '0.0.0-brew'
+  desc "High-level C++ binding for ZeroMQ"
+  homepage "https://github.com/zeromq/cppzmq"
+  url "https://github.com/zeromq/cppzmq.git"
+  version "0.0.0-brew"
 
-  depends_on 'zeromq'
+  depends_on "zeromq"
 
   def install
     include.install "zmq.hpp"
