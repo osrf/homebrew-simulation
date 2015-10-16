325f3d13b2cd8ba84deead8762788e7b1185ce31
Steven Peters <scpeters@osrfoundation.org>
2015-10-16 16:03:25 -0700

    gazebo7: use tar.bz2 instead of zip

--- gazebo7.rb
+++ gazebo7.rb
@@ -1,7 +1,7 @@
 class Gazebo7 < Formula
   desc "Gazebo robot simulator"
   homepage "http://gazebosim.org"
-  url "https://bitbucket.org/osrf/gazebo/get/1969aea.zip"
+  url "https://bitbucket.org/osrf/gazebo/get/1969aea.tar.bz2"
   version "7.0.0-20150824-1969aea"
   sha256 "12d697b80e24333e10af17d1db875f562428435fd058d30911fa25e00ff6e544"
   head "https://bitbucket.org/osrf/gazebo", :branch => "default", :using => :hg
