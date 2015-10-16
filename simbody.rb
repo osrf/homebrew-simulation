f56dcb460d7cafa48a5b2ca3dc5af3eaa621563f
Steven Peters <scpeters@osrfoundation.org>
2015-10-16 03:14:04 -0700

    simbody: yosemite bottle

--- simbody.rb
+++ simbody.rb
@@ -5,6 +5,11 @@ class Simbody < Formula
   sha256 "8005fbdb16c6475f98e13b8f1423b0e9951c193681c2b0d19ae5b711d7e24ec1"
   head "https://github.com/simbody/simbody.git", :branch => "master"
 
+  bottle do
+    root_url "http://gazebosim.org/distributions/simbody/releases"
+    sha256 "69186db06f20a5dd3da38429cd96e3b5b8e900cb02bc1db6803f2d06435df09f" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
 
