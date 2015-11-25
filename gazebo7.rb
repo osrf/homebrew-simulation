edb95e37f757c24d38b13dcc23f41223612da1b4
Steven Peters <scpeters@osrfoundation.org>
2015-11-24 22:59:36 -0800

    gazebo7: require sdformat4
    This is needed by gazebo pull request 1992
    https://bitbucket.org/osrf/gazebo/pull-requests/1992

--- gazebo7.rb
+++ gazebo7.rb
@@ -19,7 +19,7 @@ class Gazebo7 < Formula
   depends_on "protobuf"
   depends_on "protobuf-c"
   depends_on "qt"
-  depends_on "sdformat3"
+  depends_on "sdformat4"
   depends_on "tbb"
   depends_on "tinyxml"
   depends_on "tinyxml2"
