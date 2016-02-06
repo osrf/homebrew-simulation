ac1eec2e155602d840d5c90056eaae8def3ee493
Steven Peters <scpeters@osrfoundation.org>
2016-02-05 16:56:04 -0800

    gazebo8: new formula
    adds dependency on qwt for plotting

---
+++ gazebo8.rb
@@ -0,0 +1,57 @@
+class Gazebo8 < Formula
+  desc "Gazebo robot simulator"
+  homepage "http://gazebosim.org"
+  url "https://bitbucket.org/osrf/gazebo/get/f62d54eda.tar.gz"
+  version "8.0.0-20160205-f62d54eda"
+  sha256 "16b536166e9f8535fd27f4f8dc4f0765784c0f3c1d10f1a3990a53323ded05bd"
+  head "https://bitbucket.org/osrf/gazebo", :branch => "default", :using => :hg
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "boost"
+  depends_on "doxygen"
+  depends_on "freeimage"
+  depends_on "ignition-math2"
+  depends_on "ignition-transport1"
+  depends_on "libtar"
+  depends_on "ogre"
+  depends_on "protobuf"
+  depends_on "protobuf-c"
+  depends_on "qt"
+  depends_on "qwt"
+  depends_on "sdformat4"
+  depends_on "tbb"
+  depends_on "tinyxml"
+  depends_on "tinyxml2"
+
+  depends_on "bullet" => [:recommended, "with-shared", "with-double-precision"]
+  depends_on "dartsim/dart/dartsim4" => [:optional, "core-only"]
+  depends_on "ffmpeg" => :optional
+  depends_on "gdal" => :optional
+  depends_on "gts" => :optional
+  depends_on "player" => :optional
+  depends_on "simbody" => :recommended
+
+  patch do
+    # Fix build when homebrew python is installed
+    url "https://gist.githubusercontent.com/scpeters/9199370/raw/afe595587e38737c537124a3652db99de026c272/brew_python_fix.patch"
+    sha256 "c4774f64c490fa03236564312bd24a8630963762e25d98d072e747f0412df18e"
+  end
+
+  def install
+    ENV.m64
+
+    cmake_args = std_cmake_args
+    cmake_args << "-DENABLE_TESTS_COMPILATION:BOOL=False"
+
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "gz", "sdf"
+  end
+end
