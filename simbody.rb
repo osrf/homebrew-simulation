74df663d21aeb80eefbf9105143755c6e16f023c
Steven Peters <scpeters@osrfoundation.org>
2016-02-08 15:49:44 -0800

    simbody: remove formula name from description

--- simbody.rb
+++ simbody.rb
@@ -1,5 +1,5 @@
 class Simbody < Formula
-  desc "Simbody multibody physics API"
+  desc "Multibody physics API"
   homepage "https://simtk.org/home/simbody"
   url "https://github.com/simbody/simbody/archive/Simbody-3.5.3.tar.gz"
   sha256 "8005fbdb16c6475f98e13b8f1423b0e9951c193681c2b0d19ae5b711d7e24ec1"
