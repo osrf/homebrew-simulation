3d3d501213c8359b99cb48f1462a223b6f31cc6d
Steven Peters <scpeters@osrfoundation.org>
2016-06-03 18:07:58 -0700

    ignition-msgs: add bottle block

--- ignition-msgs.rb
+++ ignition-msgs.rb
@@ -4,6 +4,11 @@ class IgnitionMsgs < Formula
   sha256 "d0a00e6eb74de9e5f4e3f269263a75dc664d8503af50734dbb8c92f79bf180e4"
   head "https://bitbucket.org/ignitionrobotics/ign-msgs", :branch => "default", :using => :hg
 
+  bottle do
+    root_url "http://gazebosim.org/distributions/ign-msgs/releases"
+    sha256 "0123456789ABCDE0123456789ABCDE0123456789ABCDEFFF0123456789ABCDEF" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
 
