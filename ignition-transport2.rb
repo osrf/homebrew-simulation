9a2b1a53d6fbe0af7fa56d71e777596665762064
Steven Peters <scpeters@osrfoundation.org>
2016-09-16 14:41:20 -0700

    ignition-transport2: new test
    also patch and bump to fix pkg-config file

--- ignition-transport2.rb
+++ ignition-transport2.rb
@@ -3,7 +3,7 @@ class IgnitionTransport2 < Formula
   homepage "http://ignitionrobotics.org"
   url "http://gazebosim.org/distributions/ign-transport/releases/ignition-transport2-2.0.0.tar.bz2"
   sha256 "186c3ae57843607568c2b756a6980dcb93305adcf676029a138865e0111651ed"
-  revision 1
+  revision 2
 
   head "https://bitbucket.org/ignitionrobotics/ign-transport", :branch => "default", :using => :hg
 
@@ -25,12 +25,34 @@ class IgnitionTransport2 < Formula
   depends_on "zeromq"
   depends_on "cppzmq"
 
+  patch do
+    # Fix for pkg-config file
+    url "https://bitbucket.org/ignitionrobotics/ign-transport/commits/9fc8a2312ba92aa3a6cad22dcd2fe4e6084a465c/raw/"
+    sha256 "1bb1ee054798e13190d27e87c168d63053dde3ae20f7b99dddbe43beb811d7ca"
+  end
+
   def install
     system "cmake", ".", *std_cmake_args
     system "make", "install"
   end
 
   test do
-    system "pkg-config", "--modversion", "ignition-transport2"
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <iostream>
+      #include <ignition/transport.hh>
+      int main() {
+        ignition::transport::Node node;
+        return 0;
+      }
+    EOS
+    system "pkg-config", "ignition-transport2"
+    cflags = `pkg-config --cflags ignition-transport2`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lignition-transport2",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
   end
 end
