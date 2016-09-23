cb78b6d03181bc97b69ff7b9404cf68b92ad892d
Steven Peters <scpeters@osrfoundation.org>
2016-09-22 17:39:58 -0700

    ignition-msgs: patch for protobuf3

--- ignition-msgs.rb
+++ ignition-msgs.rb
@@ -2,6 +2,8 @@ class IgnitionMsgs < Formula
   homepage "https://bitbucket.org/ignitionrobotics/ign-msgs"
   url "http://gazebosim.org/distributions/ign-msgs/releases/ignition-msgs-0.5.0.tar.bz2"
   sha256 "d27c4dacce646ef013965742ec2fea207677937f3e3264870fe64d112e655b09"
+  revision 1
+
   head "https://bitbucket.org/ignitionrobotics/ign-msgs", :branch => "default", :using => :hg
 
   bottle do
@@ -16,6 +18,12 @@ class IgnitionMsgs < Formula
   depends_on "pkg-config" => :run
   depends_on "protobuf"
   depends_on "protobuf-c" => :build
+
+  patch do
+    # Fix for pkg-config file
+    url "https://bitbucket.org/ignitionrobotics/ign-msgs/commits/e657fc2970f611df3a30570f78ec797366eac26e/raw/"
+    sha256 "3a63ba7e302f482a43cca0272bd747c984690179563655ac4bb9ac7e55fe6154"
+  end
 
   def install
     system "cmake", ".", *std_cmake_args
