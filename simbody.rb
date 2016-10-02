3e5c24bf5de07eef847fa8f3da9718624dfc0118
Steven Peters <scpeters@osrfoundation.org>
2016-10-02 00:06:05 -0700

    simbody: patch pkg-config and add test

--- simbody.rb
+++ simbody.rb
@@ -12,6 +12,7 @@ class Simbody < Formula
 
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
+  depends_on "pkg-config" => :run
 
   def install
     ENV.m64
@@ -21,5 +22,44 @@ class Simbody < Formula
       system "make", "doxygen"
       system "make", "install"
     end
+  end
+
+  patch do
+    # Fix pkg-config file
+    url "https://github.com/scpeters/simbody/commit/a37deef08af530b57601251900fadee0d0be6cfd.diff"
+    sha256 "0b99f7b6df46ddcc43094b29ba834896490fe5a76ceaa15f646b3b4579fed51e"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include "simbody/Simbody.h"
+      using namespace SimTK;
+      int main() {
+        // Create the system.
+        MultibodySystem system;
+        SimbodyMatterSubsystem matter(system);
+        GeneralForceSubsystem forces(system);
+        Force::UniformGravity gravity(forces, matter, Vec3(0, -9.8, 0));
+        Body::Rigid pendulumBody(MassProperties(1.0, Vec3(0), Inertia(1)));
+        pendulumBody.addDecoration(Transform(), DecorativeSphere(0.1));
+        MobilizedBody::Pin pendulum1(matter.Ground(), Transform(Vec3(0)),
+                                     pendulumBody, Transform(Vec3(0, 1, 0)));
+        MobilizedBody::Pin pendulum2(pendulum1, Transform(Vec3(0)),
+                                     pendulumBody, Transform(Vec3(0, 1, 0)));
+        // Initialize the system and state.
+        system.realizeTopology();
+        State state = system.getDefaultState();
+        pendulum2.setRate(state, 5.0);
+        // Simulate it.
+        RungeKuttaMersonIntegrator integ(system);
+        TimeStepper ts(system, integ);
+        ts.initialize(state);
+        ts.stepTo(50.0);
+      }
+    EOS
+    system "pkg-config", "simbody"
+    flags = `pkg-config --cflags --libs simbody`.split(" ")
+    system ENV.cxx, "test.cpp", *flags, "-o", "test"
+    system "./test"
   end
 end
