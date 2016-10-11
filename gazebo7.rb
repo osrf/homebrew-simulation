f317f02bcdc0171d92360d2ccb9b0bb148fb41ea
osrfbuild <osrfbuild@osrfoundation.org>
2016-10-11 09:39:50 -0700

    gazebo7 7.4.0 (#152)
    * gazebo7 7.4.0
    
    * remove patches
    
    * gazebo7 7.4.0 bottle

--- gazebo7.rb
+++ gazebo7.rb
@@ -1,15 +1,14 @@
 class Gazebo7 < Formula
   desc "Gazebo robot simulator"
   homepage "http://gazebosim.org"
-  url "http://gazebosim.org/distributions/gazebo/releases/gazebo-7.3.1.tar.bz2"
-  sha256 "aeff031d9b1b72c94b06b2a87790e6b561f4cba0e1dce155269108b3237e5c62"
-  revision 6
+  url "http://gazebosim.org/distributions/gazebo/releases/gazebo-7.4.0.tar.bz2"
+  sha256 "a033b2273383f16e5dd5b5bfe597551dc3618b98e64abecfa8a41bdddd6542f7"
 
   head "https://bitbucket.org/osrf/gazebo", :branch => "gazebo7", :using => :hg
 
   bottle do
     root_url "http://gazebosim.org/distributions/gazebo/releases"
-    sha256 "29febabf2cb612b2937b8d89e2ba25d0f9abb7b68dc67b3f488bd0c80305a53a" => :yosemite
+    sha256 "3ec0b76c6a88dac79604e58e9e447dd010a9b3cf66a18f8e93c7f6ff2247616a" => :yosemite
   end
 
   depends_on "cmake" => :build
@@ -51,18 +50,6 @@ class Gazebo7 < Formula
     url "https://gist.githubusercontent.com/scpeters/9199370/raw/afe595587e38737c537124a3652db99de026c272/brew_python_fix.patch"
     sha256 "c4774f64c490fa03236564312bd24a8630963762e25d98d072e747f0412df18e"
   end
-
-  patch do
-    # Fix for compatibility with tinyxml2 4.0
-    url "https://bitbucket.org/osrf/gazebo/commits/90b82ee6efe79d83dc9fa4f9706e050e658d1221/raw/"
-    sha256 "73f3724ce0123cd71968b8b03822d45a3b893ca40f746feed76870c9b29e7603"
-  end unless build.head?
-
-  patch do
-    # Fix for compatibility with boost 1.62
-    url "https://bitbucket.org/osrf/gazebo/commits/f1ad6c032f87d9991cc2eb57830c6c14e07a73d8/raw/"
-    sha256 "17435adcc74d70685fcd88893bd9012fd15f80f78a35b7f17f12c22c399c4638"
-  end unless build.head?
 
   def install
     ENV.m64
