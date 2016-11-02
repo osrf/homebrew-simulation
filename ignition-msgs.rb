6c30b26d1857fdf971f2ab9199859432fdc75169
osrfbuild <osrfbuild@osrfoundation.org>
2016-11-02 09:56:00 -0700

    ignition-msgs 0.6.0 (#155)
    * ignition-msgs 0.6.0
    
    * ignition-msgs: remove old patch
    
    * ignition-msgs 0.6.0 bottle

--- ignition-msgs.rb
+++ ignition-msgs.rb
@@ -1,14 +1,13 @@
 class IgnitionMsgs < Formula
   homepage "https://bitbucket.org/ignitionrobotics/ign-msgs"
-  url "http://gazebosim.org/distributions/ign-msgs/releases/ignition-msgs-0.5.0.tar.bz2"
-  sha256 "d27c4dacce646ef013965742ec2fea207677937f3e3264870fe64d112e655b09"
-  revision 1
+  url "http://gazebosim.org/distributions/ign-msgs/releases/ignition-msgs-0.6.0.tar.bz2"
+  sha256 "aaac3f5ffdbc0e81afde4aa2c297bec52293cfa4091adde9d6fd539089cfd269"
 
   head "https://bitbucket.org/ignitionrobotics/ign-msgs", :branch => "default", :using => :hg
 
   bottle do
     root_url "http://gazebosim.org/distributions/ign-msgs/releases"
-    sha256 "6c152138cef486382e0bdc19e5201a9fe44e455cb360d817550ef076b728c70a" => :yosemite
+    sha256 "fff1219fdaffb02daed63db6ada32c65ea8da5a8c72212c1937031a48c08b179" => :yosemite
   end
 
   depends_on "cmake" => :build
@@ -18,12 +17,6 @@ class IgnitionMsgs < Formula
   depends_on "pkg-config" => :run
   depends_on "protobuf"
   depends_on "protobuf-c" => :build
-
-  patch do
-    # Fix for pkg-config file
-    url "https://bitbucket.org/ignitionrobotics/ign-msgs/commits/e657fc2970f611df3a30570f78ec797366eac26e/raw/"
-    sha256 "3a63ba7e302f482a43cca0272bd747c984690179563655ac4bb9ac7e55fe6154"
-  end
 
   def install
     system "cmake", ".", *std_cmake_args
