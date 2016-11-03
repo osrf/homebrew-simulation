a2b649cad5e177fdd66bcab86b89699a32da8b7a
Steven Peters <scpeters@osrfoundation.org>
2016-11-02 19:22:19 -0700

    ignition-msgs: test building with cmake (#157)
    * ignition-msgs: test building with cmake
    
    * ignition-msgs: bump revision
    
    * ignition-msgs: add desc, cmake as :run depend
    
    * ignition-msgs: fix yosemite test env
    
    * ignition-msgs 0.6.0_1 bottle

--- ignition-msgs.rb
+++ ignition-msgs.rb
@@ -1,17 +1,18 @@
 class IgnitionMsgs < Formula
+  desc "Middleware protobuf messages for robotics"
   homepage "https://bitbucket.org/ignitionrobotics/ign-msgs"
   url "http://gazebosim.org/distributions/ign-msgs/releases/ignition-msgs-0.6.0.tar.bz2"
   sha256 "aaac3f5ffdbc0e81afde4aa2c297bec52293cfa4091adde9d6fd539089cfd269"
+  revision 1
 
   head "https://bitbucket.org/ignitionrobotics/ign-msgs", :branch => "default", :using => :hg
 
   bottle do
     root_url "http://gazebosim.org/distributions/ign-msgs/releases"
-    sha256 "fff1219fdaffb02daed63db6ada32c65ea8da5a8c72212c1937031a48c08b179" => :yosemite
+    sha256 "fb18db87fb211d1cf5c169ef7d0fe26d7f0fcd4364eb65c3228d9b3692845c9f" => :yosemite
   end
 
-  depends_on "cmake" => :build
-
+  depends_on "cmake" => :run
   depends_on "ignition-math2"
   depends_on "ignition-tools" => :recommended
   depends_on "pkg-config" => :run
@@ -31,6 +32,16 @@ class IgnitionMsgs < Formula
         return 0;
       }
     EOS
+    (testpath/"CMakeLists.txt").write <<-EOS.undent
+      cmake_minimum_required(VERSION 2.8 FATAL_ERROR)
+      find_package(ignition-msgs0 QUIET REQUIRED)
+      set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${IGNITION-MSGS_CXX_FLAGS}")
+      include_directories(${IGNITION-MSGS_INCLUDE_DIRS})
+      link_directories(${IGNITION-MSGS_LIBRARY_DIRS})
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ${IGNITION-MSGS_LIBRARIES})
+    EOS
+    # test building with pkg-config
     system "pkg-config", "ignition-msgs0"
     cflags = `pkg-config --cflags ignition-msgs0`.split(" ")
     system ENV.cc, "test.cpp",
@@ -40,5 +51,13 @@ class IgnitionMsgs < Formula
                    "-lc++",
                    "-o", "test"
     system "./test"
+    # test building with cmake
+    mkdir "build" do
+      ENV.delete("MACOSX_DEPLOYMENT_TARGET")
+      ENV.delete("SDKROOT")
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
   end
 end
