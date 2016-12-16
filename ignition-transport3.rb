34e11e6bd64dad2f9330ef406e41a3f667bb2fad
osrfbuild <osrfbuild@osrfoundation.org>
2016-12-16 15:06:44 -0800

    ignition-transport3 3.0.0 (#169)
    * ignition-transport3 3.0.0
    
    * ignition-transport3 3.0.0 bottle

---
+++ ignition-transport3.rb
@@ -0,0 +1,51 @@
+class IgnitionTransport3 < Formula
+  desc "Transport middleware for robotics"
+  homepage "http://ignitionrobotics.org"
+  url "http://gazebosim.org/distributions/ign-transport/releases/ignition-transport3-3.0.0.tar.bz2"
+  sha256 "687e81e4ed1c640bd6c2eabba7959572dad424ae6903cffd1b35cee780aa2192"
+
+  head "https://bitbucket.org/ignitionrobotics/ign-transport", :branch => "default", :using => :hg
+
+  bottle do
+    root_url "http://gazebosim.org/distributions/ign-transport/releases"
+    cellar :any
+    sha256 "6f0273862ffa8c2896e365610ee1fb85edc3d0aa2abd2f638520ca0f0ef09a35" => :yosemite
+  end
+
+  depends_on "cmake" => :build
+  depends_on "doxygen" => [:build, :optional]
+  depends_on "pkg-config" => :run
+
+  depends_on "ignition-msgs"
+  depends_on "ignition-tools"
+  depends_on "protobuf"
+  depends_on "protobuf-c" => :build
+  depends_on "ossp-uuid"
+  depends_on "zeromq"
+  depends_on "cppzmq"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <iostream>
+      #include <ignition/transport.hh>
+      int main() {
+        ignition::transport::Node node;
+        return 0;
+      }
+    EOS
+    system "pkg-config", "ignition-transport3"
+    cflags = `pkg-config --cflags ignition-transport3`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lignition-transport3",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+  end
+end
