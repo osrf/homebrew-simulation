cbaa0be2d19d37edf9f943eb4d9ee40ebf24baa8
Lorenz Meier <lorenz@px4.io>
2017-01-02 20:16:46 +0100

    Add bottle for Ogre

--- ogre.rb
+++ ogre.rb
@@ -9,9 +9,11 @@ class Ogre < Formula
   head "https://bitbucket.org/sinbad/ogre", :branch => "v1-9", :using => :hg
 
   bottle do
-    root_url "http://gazebosim.org/distributions/ogre/releases"
+    #root_url "http://gazebosim.org/distributions/ogre/releases"
+    root_url "http://px4-travis.s3.amazonaws.com/toolchain/bottles"
     sha256 "6d31ade42664a78b97be8339b86b3ef8ab5d41d36bc9e274cd11fb11874f8b81" => :el_capitan
     sha256 "677884f99bf5763ff31c49a9977354e032c894bb0658d2e88a8e1c59edf1ddb4" => :yosemite
+    sha256 "2cf91dae3a94b6f405cf9a449213f60f005ce661c471af8cf193974d7b558695" => :sierra
   end
 
   option "with-cg"
