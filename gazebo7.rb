a8a9f5900fce59de472d0141d85b94a304818fa6
Steven Peters <scpeters@osrfoundation.org>
2017-01-03 19:52:13 -0800

    gazebo7: patch to use qt4-no-webkit (#182)
    * gazebo7: patch to use qt4-no-webkit
    * gazebo7 7.4.0_4 el_capitan bottle
    * gazebo7 7.4.0_4 yosemite bottle

--- gazebo7.rb
+++ gazebo7.rb
@@ -3,14 +3,14 @@ class Gazebo7 < Formula
   homepage "http://gazebosim.org"
   url "http://gazebosim.org/distributions/gazebo/releases/gazebo-7.4.0.tar.bz2"
   sha256 "a033b2273383f16e5dd5b5bfe597551dc3618b98e64abecfa8a41bdddd6542f7"
-  revision 3
+  revision 4
 
   head "https://bitbucket.org/osrf/gazebo", :branch => "gazebo7", :using => :hg
 
   bottle do
     root_url "http://gazebosim.org/distributions/gazebo/releases"
-    sha256 "bf5b7d5aee51abb822f84ef584fbe46daf300c8ab06cdbbb1be822c1df6efdc3" => :el_capitan
-    sha256 "68274d6c7b1a84ba3cbdabdb4328e3afe8e548cf405252b08176fa0637267588" => :yosemite
+    sha256 "a5a28af11d0462697fae9154928e668a8979d022496b14f20ec9f3cda36e49de" => :el_capitan
+    sha256 "d68b54ec9c6368d96a75634b54e610c0156e686b335713e2bd7953036cd15481" => :yosemite
   end
 
   depends_on "cmake" => :build
@@ -25,7 +25,7 @@ class Gazebo7 < Formula
   depends_on "ogre"
   depends_on "protobuf"
   depends_on "protobuf-c"
-  depends_on "qt"
+  depends_on "qt4-no-webkit"
   depends_on "sdformat4"
   depends_on "tbb"
   depends_on "tinyxml"
@@ -51,6 +51,18 @@ class Gazebo7 < Formula
     # Fix build when homebrew python is installed
     url "https://gist.githubusercontent.com/scpeters/9199370/raw/afe595587e38737c537124a3652db99de026c272/brew_python_fix.patch"
     sha256 "c4774f64c490fa03236564312bd24a8630963762e25d98d072e747f0412df18e"
+  end
+
+  patch do
+    # Remove dependence on webkit from qt4, part 1
+    url "https://bitbucket.org/osrf/gazebo/commits/12bbbf5f0968eeb26be7f3d0c441d83edd070dac/raw/"
+    sha256 "77bf176e2fcb8bc4fe751ad0fd205df390a1e9aa9bca2ad42620a4362257286c"
+  end
+
+  patch do
+    # Remove dependence on webkit from qt4, part2
+    url "https://bitbucket.org/osrf/gazebo/commits/33a8a3c3d631f2c48056d4c35942a9899e1db585/raw/"
+    sha256 "16df786e372926aebc24c4d2f28d0e6fa72b83f3e7372c9c5d82271e73c9a93f"
   end
 
   def install
