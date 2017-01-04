917d49b8c0ef2bf938fb5ede17db07e9a01b3e43
Steven Peters <scpeters@osrfoundation.org>
2017-01-04 15:50:50 -0800

    bump revision

--- ogre.rb
+++ ogre.rb
@@ -4,7 +4,7 @@ class Ogre < Formula
   url "https://downloads.sourceforge.net/project/ogre/ogre/1.7/ogre_src_v1-7-4.tar.bz2"
   version "1.7.4"
   sha256 "afa475803d9e6980ddf3641dceaa53fcfbd348506ed67893c306766c166a4882"
-  revision 1
+  revision 2
 
   head "https://bitbucket.org/sinbad/ogre", :branch => "v1-9", :using => :hg
 
