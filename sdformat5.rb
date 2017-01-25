bbd6b16407f25a9d38ec430407133141434fd15e
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-01-25 20:10:56 +0100

    New sdformat5 formula

---
+++ sdformat5.rb
@@ -0,0 +1,67 @@
+class Sdformat4 < Formula
+  desc "Simulation Description Format"
+  homepage "http://sdformat.org"
+  url "http://gazebosim.org/distributions/sdformat/releases/sdformat-5.0.0.tar.bz2"
+  sha256 "stub"
+
+  head "https://bitbucket.org/osrf/sdformat", :branch => "default", :using => :hg
+
+  bottle do
+    root_url "http://gazebosim.org/distributions/sdformat/releases"
+  end
+
+  depends_on "cmake" => :build
+
+  depends_on "boost"
+  depends_on "doxygen"
+  depends_on "ignition-math3"
+  depends_on "pkg-config" => :run
+  depends_on "ros/deps/urdfdom" => :optional
+  depends_on "tinyxml"
+
+  conflicts_with "sdformat", :because => "Differing version of the same formula"
+  conflicts_with "sdformat3", :because => "Differing version of the same formula"
+  conflicts_with "sdformat4", :because => "Differing version of the same formula"
+
+  def install
+    ENV.m64
+
+    cmake_args = std_cmake_args
+    cmake_args << "-DUSE_EXTERNAL_URDF:BOOL=True" if build.with? "urdfdom"
+    cmake_args << ".."
+
+    mkdir "build" do
+      system "cmake", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <iostream>
+      #include "sdf/sdf.hh"
+      const std::string sdfString(
+        "<sdf version='1.5'>"
+        "  <model name='example'>"
+        "    <link name='link'>"
+        "      <sensor type='gps' name='mysensor' />"
+        "    </link>"
+        "  </model>"
+        "</sdf>");
+      int main() {
+        sdf::SDF modelSDF;
+        modelSDF.SetFromString(sdfString);
+        std::cout << modelSDF.ToString() << std::endl;
+      }
+    EOS
+    system "pkg-config", "sdformat"
+    cflags = `pkg-config --cflags sdformat`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lsdformat",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+  end
+end
