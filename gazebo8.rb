632cab19534affb4e857ad6972037c3bf8736e51
Steven Peters <scpeters@osrfoundation.org>
2017-02-22 17:36:37 -0800

    bump gazebo8 revision

--- gazebo8.rb
+++ gazebo8.rb
@@ -4,8 +4,7 @@ class Gazebo8 < Formula
   url "http://gazebosim.org/distributions/gazebo/releases/gazebo-8.0.0.tar.bz2"
   sha256 "ea733be6946ac5c538bf207ba01f3a6d6afa456d0b70455f7066b19d722f0d12"
   version_scheme 1
-
-  revision 3
+  revision 4
 
   head "https://bitbucket.org/osrf/gazebo", :branch => "default", :using => :hg
 
