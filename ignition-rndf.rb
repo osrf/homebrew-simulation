8fd690ac2e43de7278bf62fbbd3f0900fe4fa64a
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-04-19 00:40:35 +0200

    Import rndf from my repo

---
+++ ignition-rndf.rb
@@ -0,0 +1,50 @@
+class IgnitionRndf < Formula
+  desc "Ignition RNDF is a library for parsing RNDF road network files"
+  homepage "http://ignitionrobotics.org"
+  url "http://gazebosim.org/distributions/ign-rndf/releases/ignition-rndf-0.1.2.tar.bz2"
+  sha256 "700bca1bb7af3ad02250dac9eef78a283742300239d5fc4494a18a8d419be307"
+
+  head "https://bitbucket.org/ignitionrobotics/ign-rndf", :branch => "default", :using => :hg
+
+  bottle do
+    root_url "http://gazebosim.org/distributions/ign-rndf/releases"
+    cellar :any
+    sha256 "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF" => :el_capitan
+    sha256 "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF" => :yosemite
+  end
+
+  depends_on "cmake" => :build
+  depends_on "ignition-math3"
+  depends_on "pkg-config" => :run
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+    #include "ignition/rndf/UniqueId.hh"
+    #include "ignition/rndf/RNDFNode.hh"
+
+    int main() {
+      int segmentId = 1;
+      int laneId = 2;
+      int waypointId = 3;
+      UniqueId id(segmentId, laneId, waypointId);
+      RNDFNode rndfNode(id);
+    }
+    EOS
+    system "pkg-config", "ignition-rndf"
+    cflags = `pkg-config --cflags ignition-rndf`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lignition-rndf",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+  end
+end
