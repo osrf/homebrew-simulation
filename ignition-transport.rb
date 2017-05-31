98edaf5809927080284281ff8c84c250608fbd5b
Steven Peters <scpeters@openrobotics.org>
2017-05-30 20:37:06 -0700

    ignition-transport: new bottles (#248)
    * ignition-transport: new bottles
    
    * ignition-transport 1.4.0_3 sierra bottle
    
    * ignition-transport 1.4.0_3 el_capitan bottle
    
    * ignition-transport 1.4.0_3 yosemite bottle

--- ignition-transport.rb
+++ ignition-transport.rb
@@ -3,16 +3,16 @@ class IgnitionTransport < Formula
   homepage "http://ignitionrobotics.org"
   url "http://gazebosim.org/distributions/ign-transport/releases/ignition-transport-1.4.0.tar.bz2"
   sha256 "bc612e9781f9cab81cc4111ed0de07c4838303f67c25bc8b663d394b40a8f5d4"
-  revision 2
+  revision 3
 
   head "https://bitbucket.org/ignitionrobotics/ign-transport", :branch => "ign-transport1", :using => :hg
 
   bottle do
-    rebuild 1
     root_url "http://gazebosim.org/distributions/ign-transport/releases"
     cellar :any
-    sha256 "5dd18f7722794e4a21dfd9b7638c1461e5634824daa56f5107384129c6a42917" => :el_capitan
-    sha256 "dd02042ff09da445a4aa0d139f1b673d8f8bf49c025b5cce434938a8ead56d1e" => :yosemite
+    sha256 "946ea884129efa3fc41ea4ba26d2b350129964f4a2db7635d5e6fecd454ef6a5" => :sierra
+    sha256 "3fceab90ba1bfb7da79bf11b3df44315dbfb373809f61dcae67a8933ecc6b090" => :el_capitan
+    sha256 "ff83f3fd94ec91e2ccf09f7aff2d3b59490c2713beb516eb7e9f3935b55ae722" => :yosemite
   end
 
   depends_on "cmake" => :build
