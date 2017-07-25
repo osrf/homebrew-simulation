0766d5a5c17516e4950dbb5cf6bf4080ab8376ed
osrfbuild <osrfbuild@osrfoundation.org>
2017-07-25 20:21:17 +0200

    sdformat4 4.3.2 (#260)
    * sdformat4 4.3.2
    
    * sdformat4: patch for cmake 3.9
    
    * sdformat4 4.3.2 el_capitan bottle
    
    * sdformat4 4.3.2 yosemite bottle
    
    * sdformat4 4.3.2 sierra bottle

--- sdformat4.rb
+++ sdformat4.rb
@@ -1,17 +1,16 @@
 class Sdformat4 < Formula
   desc "Simulation Description Format"
   homepage "http://sdformat.org"
-  url "http://gazebosim.org/distributions/sdformat/releases/sdformat-4.3.1.tar.bz2"
-  sha256 "37e3a3b0199bbcb6147bc2ff42ff84685bf0bf57cca2660ba7f945805932b5c9"
-  revision 1
+  url "http://gazebosim.org/distributions/sdformat/releases/sdformat-4.3.2.tar.bz2"
+  sha256 "82147307f0d30fd8bd4dee253cca3e0b6864be5cb2b47249382cd78b466bd9ee"
 
   head "https://bitbucket.org/osrf/sdformat", :branch => "sdf4", :using => :hg
 
   bottle do
     root_url "http://gazebosim.org/distributions/sdformat/releases"
-    sha256 "b2b2bbda333589c2e61c826cdfb0d5176acea5ad89d7721a9a47895b0595dfe7" => :sierra
-    sha256 "31e3464c0044a6a7cfc8a3980005762c6120e00ab5f65c657240c8e4edf294eb" => :el_capitan
-    sha256 "b4a42dd5ba07255892199f729ec86a7e0a820ee79c7b952077b282272a2bdb90" => :yosemite
+    sha256 "34858e26cf9a0e33ecd7addfde558aaa11999b4047ae1073cf9573e468c7709c" => :sierra
+    sha256 "f4182a61110e28b1c8aeaf7216747645433fc8dbb914fc64f81680f2ef9ebd7f" => :el_capitan
+    sha256 "018e27cca1edd9e4e5741be8a85c3fcfdf905ef228b34848b54c7c8c24c072cb" => :yosemite
   end
 
   depends_on "cmake" => :build
@@ -26,6 +25,12 @@ class Sdformat4 < Formula
   conflicts_with "sdformat", :because => "Differing version of the same formula"
   conflicts_with "sdformat3", :because => "Differing version of the same formula"
   conflicts_with "sdformat5", :because => "Differing version of the same formula"
+
+  patch do
+    # Fix for cmake 3.9
+    url "https://bitbucket.org/osrf/sdformat/commits/3e1d3f3bd0387b548d347b423a2bea39a7872003/raw/"
+    sha256 "b7fda735c24bf152c44213368575c1addc9ab91d4125611157f79bef32a36250"
+  end
 
   def install
     ENV.m64
