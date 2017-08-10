40efff34a33ea88b7abbcdbe17d5136ca7eb7b4a
osrfbuild <osrfbuild@osrfoundation.org>
2017-08-10 19:57:10 +0200

    sdformat5 5.2.0 (#264)
    * sdformat5 5.2.0
    
    * sdformat5 5.2.0 el_capitan bottle
    
    * sdformat5 5.2.0 yosemite bottle
    
    * sdformat5 5.2.0 sierra bottle

--- sdformat5.rb
+++ sdformat5.rb
@@ -1,18 +1,17 @@
 class Sdformat5 < Formula
   desc "Simulation Description Format"
   homepage "http://sdformat.org"
-  url "http://gazebosim.org/distributions/sdformat/releases/sdformat-5.1.0.tar.bz2"
-  sha256 "ccb9b7628a3cd171536e37487bf4c2fd1df909940714e7818e6fd396a3ef01eb"
-  revision 1
+  url "http://gazebosim.org/distributions/sdformat/releases/sdformat-5.2.0.tar.bz2"
+  sha256 "b7ba88275c28c3c26fe245b1ac4aad7337c2fd53d6c1e1c94c04f359f2309d51"
 
   head "https://bitbucket.org/osrf/sdformat", :branch => "default", :using => :hg
 
   bottle do
     root_url "http://gazebosim.org/distributions/sdformat/releases"
     cellar :any
-    sha256 "7dd215ca8927e7de9cd47bdf6026cc1ed54899361440e48f7b2a52a524cc4d2e" => :sierra
-    sha256 "82ee5fb56e16092e713fb8d8ebe667ab7ff55909eafc16c913d170e82a76d680" => :el_capitan
-    sha256 "b24c1fe5e82bae43d78cbdc09d8b8ac1c96c6342e129c74d5e80e4d1c66321d1" => :yosemite
+    sha256 "eafc4554b050f12e423752d78c6e39c5bb1edb751ebe8af6f2fbe19e49e87d46" => :sierra
+    sha256 "c2ef934ffb5eb602838839dec4d41a72e2ae912b5e8b662c12f6f8353696a848" => :el_capitan
+    sha256 "24579cc4802dfa7c6244ab046456d32b6b32d7e1d263333c2cb7634bdece5a66" => :yosemite
   end
 
   depends_on "cmake" => :build
