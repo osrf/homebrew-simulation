638d81eb9021d46cbf95e28e8812c20b24d5374e
chapulina <lupoubel@hotmail.com>
2017-09-06 21:58:14 -0700

    New snapshot of ignition-common: 0.4.0~20170906~ebf4b53 (#271)
    * New snapshot of ignition-common: 0.4.0~20170906~ebf4b53
    
    * ignition-common 0.4.0~20170906~ebf4b53 sierra bottle
    
    * ignition-common 0.4.0~20170906~ebf4b53 el_capitan bottle
    
    * ignition-common 0.4.0~20170906~ebf4b53 yosemite bottle

--- ignition-common.rb
+++ ignition-common.rb
@@ -2,17 +2,17 @@ class IgnitionCommon < Formula
   desc "Common libraries for robotics applications"
   homepage "https://bitbucket.org/ignitionrobotics/ign-common"
   # url "http://gazebosim.org/distributions/ign-common/releases/ignition-common-0.4.0.tar.bz2"
-  url "https://bitbucket.org/ignitionrobotics/ign-common/get/2071c29973ed10a53275c875199be36fb3dc8196.tar.gz"
-  version "0.4.0~20170720~2071c29"
-  sha256 "cb2bfbd60cfb353c28778f391bb65a13dc31168d9be4478e13f66ba0b9232a88"
+  url "https://bitbucket.org/ignitionrobotics/ign-common/get/ebf4b53ee7633d541172953a213ca4009fc122f2.tar.gz"
+  version "0.4.0~20170906~ebf4b53"
+  sha256 "c28f27c50fad98360b143c1bed270183f7260105b7a21669463400c80efc7ed2"
 
   head "https://bitbucket.org/ignitionrobotics/ign-common", :branch => "default", :using => :hg
 
   bottle do
     root_url "http://gazebosim.org/distributions/ign-common/releases"
-    sha256 "bf56bd002a3bedae78e7369b51d483d370ef67e9df54f5a0c113c17c9e95053b" => :sierra
-    sha256 "d452ceab673157a6b6e8bf0a5ed01a45b66453b9e9c93babb7361a108426b0b8" => :el_capitan
-    sha256 "4e739efdeeb59ca08f8f62307dda205dfc9800ac899bd4e73ef07696ac4e7c69" => :yosemite
+    sha256 "25483747704aaa04f1d86423e67c195f6f50b57297049d751e7d966a388b6f82" => :sierra
+    sha256 "167ab634b6f3afeb0ae72c53c5c6752f3f5d81248b6a1762909b20dc5f00265d" => :el_capitan
+    sha256 "ca64d8eca4af6b1a5ff3588c6a58e8c091a9c3d45bf38edd5e336064b79dfc13" => :yosemite
   end
 
   depends_on "cmake" => :build
