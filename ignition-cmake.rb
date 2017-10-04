b27a22f97c973e0bbddfa495a2689013223d118b
Steven Peters <scpeters@openrobotics.org>
2017-10-04 13:09:30 -0700

    ignition-cmake: new formula (#287)
    * ignition-cmake: new formula
    
    * fix audit and test
    
    * one more audit
    
    * ignition-cmake 0.1.0~20171003~c82f9d3 sierra bottle
    
    * use 1 bottle for multiple OS releases

---
+++ ignition-cmake.rb
@@ -0,0 +1,40 @@
+class IgnitionCmake < Formula
+  desc "CMake helper functions for building robotic applications"
+  homepage "http://ignitionrobotics.org"
+  url "https://bitbucket.org/ignitionrobotics/ign-cmake/get/c82f9d30b12b9fc217eeb96c6c1016739c326dbf.tar.gz"
+  version "0.1.0~20171003~c82f9d3"
+  sha256 "1ade377912a62e82b5561e14cc1de4816bd558b5687206c7ecf6f9626cf18e2e"
+
+  head "https://bitbucket.org/ignitionrobotics/ign-cmake", :branch => "default", :using => :hg
+
+  bottle do
+    root_url "http://gazebosim.org/distributions/ign-cmake/releases"
+    cellar :any_skip_relocation
+    sha256 "023dad858a1521773636347a3ff1755f416e770d12581e6ff736f8b79dd342bd" => :yosemite_or_later
+  end
+
+  depends_on "cmake" => :run
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<-EOS.undent
+      cmake_minimum_required(VERSION 3.5.1 FATAL_ERROR)
+      find_package(ignition-cmake0 REQUIRED)
+      ign_configure_project(test 0.1.0)
+      ign_configure_build(QUIT_IF_BUILD_ERRORS)
+      #ign_create_packages()
+    EOS
+    %w[doc include src test].each do |dir|
+      mkdir dir do
+        touch "CMakeLists.txt"
+      end
+    end
+    mkdir "build" do
+      system "cmake", ".."
+    end
+  end
+end
