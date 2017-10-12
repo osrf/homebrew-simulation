919161a7f0d0992367fce9bb5fa60964a4b3d7dd
Steven Peters <scpeters@openrobotics.org>
2017-10-12 07:52:57 -0700

    ignition-common depend on ignition-math4 (#297)
    * ignition-common: add test using cmake
    
    * ignition-common: update, use ignition-math4
    
    * ignition-common -> ignition-common0, add aliases
    
    * fix cmake test
    
    * patch to find ignition-math4 in cmake config
    
    * ignition-common: LIBRARY_PATH for gettext in test
    
    * ignition-gui fix dependency names
    
    * ignition-common: use new commit instead of patch
    
    * ignition-common0 0.4.0~20171011~4184017 high_sierra bottle
    
    * ignition-common0 0.4.0~20171011~4184017 sierra bottle
    
    * ignition-common0 0.4.0~20171011~4184017 el_capitan bottle

--- ignition-common0.rb
+++ ignition-common0.rb
@@ -1,30 +1,28 @@
-class IgnitionCommon < Formula
+class IgnitionCommon0 < Formula
   desc "Common libraries for robotics applications"
   homepage "https://bitbucket.org/ignitionrobotics/ign-common"
   # url "http://gazebosim.org/distributions/ign-common/releases/ignition-common-0.4.0.tar.bz2"
-  url "https://bitbucket.org/ignitionrobotics/ign-common/get/ebf4b53ee7633d541172953a213ca4009fc122f2.tar.gz"
-  version "0.4.0~20170906~ebf4b53"
-  sha256 "c28f27c50fad98360b143c1bed270183f7260105b7a21669463400c80efc7ed2"
+  url "https://bitbucket.org/ignitionrobotics/ign-common/get/41840173c59de679f80cb9a94c94645d618311cd.tar.gz"
+  version "0.4.0~20171011~4184017"
+  sha256 "4887e157c8b91acf5e1912d1443178c18384140792a92047914463c1b6a841f0"
 
   head "https://bitbucket.org/ignitionrobotics/ign-common", :branch => "default", :using => :hg
 
   bottle do
     root_url "http://gazebosim.org/distributions/ign-common/releases"
-    sha256 "37024f64d990765753406631b27a02c11bf0e5c036b0ee30190e9f1a5595d7e2" => :high_sierra
-    sha256 "25483747704aaa04f1d86423e67c195f6f50b57297049d751e7d966a388b6f82" => :sierra
-    sha256 "167ab634b6f3afeb0ae72c53c5c6752f3f5d81248b6a1762909b20dc5f00265d" => :el_capitan
-    sha256 "ca64d8eca4af6b1a5ff3588c6a58e8c091a9c3d45bf38edd5e336064b79dfc13" => :yosemite
+    sha256 "7efad0b723d7ef3a5e76cae3abeef60df127108003cac31eac51f3195b3206d8" => :high_sierra
+    sha256 "c488c4f2b4a3c4a85d6bb594b32478a530579f52087f183d602ad5d0fe0816d1" => :sierra
+    sha256 "87798d0e3f89ffa5ca18e35995f045218bfa3a9408cfada6ce427266d5adcb98" => :el_capitan
   end
-
-  depends_on "cmake" => :build
 
   depends_on "ffmpeg"
   depends_on "freeimage"
   depends_on "gts"
-  depends_on "ignition-math3"
+  depends_on "ignition-math4"
   depends_on "ossp-uuid"
   depends_on "tinyxml2"
 
+  depends_on "cmake" => :run
   depends_on "pkg-config" => :run
 
   def install
@@ -51,6 +49,12 @@ class IgnitionCommon < Formula
         return 0;
       }
     EOS
+    (testpath/"CMakeLists.txt").write <<-EOS.undent
+      cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
+      find_package(ignition-common0 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ${IGNITION-COMMON_LIBRARIES})
+    EOS
     system "pkg-config", "ignition-common0"
     cflags = `pkg-config --cflags ignition-common0`.split(" ")
     system ENV.cc, "test.cpp",
@@ -60,5 +64,14 @@ class IgnitionCommon < Formula
                    "-lc++",
                    "-o", "test"
     system "./test"
+    # test building with cmake
+    mkdir "build" do
+      ENV.delete("MACOSX_DEPLOYMENT_TARGET")
+      ENV.delete("SDKROOT")
+      ENV.append "LIBRARY_PATH", Formula["gettext"].opt_lib
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
   end
 end
