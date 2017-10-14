02e6bbca1575e7cd2e8c85dc32515c7d614de1eb
Steven Peters <scpeters@openrobotics.org>
2017-10-13 17:54:23 -0700

    ignition-gui bottle rebuild 1

--- ignition-gui.rb
+++ ignition-gui.rb
@@ -8,6 +8,7 @@ class IgnitionGui < Formula
   head "https://bitbucket.org/ignitionrobotics/ign-gui", :branch => "default", :using => :hg
 
   bottle do
+    rebuild 1
     root_url "http://gazebosim.org/distributions/ign-gui/releases"
     sha256 "a435130d701ebdaa58bd29833a26d53c612ae2163a195b0124ad71f0e54675d8" => :high_sierra
     sha256 "8660bbd506869aebc3c01114af9fa651d4ee67eaa9259748a7fc4204aeb01c08" => :sierra
