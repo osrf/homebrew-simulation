83930722ccda86e4a40e5b1ff901012b19da5595
osrfbuild <osrfbuild@osrfoundation.org>
2017-10-27 01:40:48 +0200

    sdformat4 4.4.0 (#300)
    * sdformat4 4.4.0
    
    * sdformat4: remove old patch
    
    * sdformat4 4.4.0 sierra bottle
    
    * sdformat4 4.4.0 el_capitan bottle
    
    * sdformat4 4.4.0 high_sierra bottle

--- sdformat4.rb
+++ sdformat4.rb
@@ -1,17 +1,16 @@
 class Sdformat4 < Formula
   desc "Simulation Description Format"
   homepage "http://sdformat.org"
-  url "http://gazebosim.org/distributions/sdformat/releases/sdformat-4.3.2.tar.bz2"
-  sha256 "82147307f0d30fd8bd4dee253cca3e0b6864be5cb2b47249382cd78b466bd9ee"
-  revision 2
+  url "http://gazebosim.org/distributions/sdformat/releases/sdformat-4.4.0.tar.bz2"
+  sha256 "4424a984f69d3333f087e7aae1d8fa5aec61ad52e09be39e2f5e2cb69ade1527"
 
   head "https://bitbucket.org/osrf/sdformat", :branch => "sdf4", :using => :hg
 
   bottle do
     root_url "http://gazebosim.org/distributions/sdformat/releases"
-    sha256 "f953082c101bf3673a2e7b3628ce844ca0d5d58444576bfa880c7ad13f017517" => :high_sierra
-    sha256 "8422a94e44c665505f383ff89b42ebab0411a8fab07da72703b7367b2cf522a1" => :sierra
-    sha256 "6e9d06b39e95c2e22165b6738570c36cca8a096f451fe020db8b6c402de3705f" => :el_capitan
+    sha256 "018a65f72c69052e6f0b3e2bb91502ca5dcdfddd761ece04728cec5dc68c7eae" => :high_sierra
+    sha256 "8a4e951e776dba134ffd411af52dea6b430fef837bbe4d18aef403983d70c4fe" => :sierra
+    sha256 "a9c6cd2409d740f014ff04266530bebc3874162a46117054f8d8522b02ff2efe" => :el_capitan
     sha256 "04fec31df772e8d128a44272da9ae3eefa6e57ebeee9f62b957169dc3a400b0f" => :yosemite
   end
 
@@ -27,12 +26,6 @@ class Sdformat4 < Formula
   conflicts_with "sdformat", :because => "Differing version of the same formula"
   conflicts_with "sdformat3", :because => "Differing version of the same formula"
   conflicts_with "sdformat5", :because => "Differing version of the same formula"
-
-  patch do
-    # Fix for cmake 3.9
-    url "https://bitbucket.org/osrf/sdformat/commits/3e1d3f3bd0387b548d347b423a2bea39a7872003/raw/"
-    sha256 "b7fda735c24bf152c44213368575c1addc9ab91d4125611157f79bef32a36250"
-  end
 
   def install
     ENV.m64
