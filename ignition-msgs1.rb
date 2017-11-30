240c84d91277afd4235e786c41f4e5f9e8db5882
osrfbuild <osrfbuild@osrfoundation.org>
2017-11-30 02:16:08 +0100

    ignition-msgs 1.0.0~pre3 (#316)
    * ignition-msgs 1.0.0~pre3
    
    * add version_scheme, enable bottles
    
    * ignition-msgs1 1.0.0~pre3 high_sierra bottle
    
    * ignition-msgs1 1.0.0~pre3 el_capitan bottle
    
    * ignition-msgs1 1.0.0~pre3 sierra bottle

--- ignition-msgs1.rb
+++ ignition-msgs1.rb
@@ -1,18 +1,19 @@
 class IgnitionMsgs1 < Formula
   desc "Middleware protobuf messages for robotics"
   homepage "https://bitbucket.org/ignitionrobotics/ign-msgs"
-  url "https://bitbucket.org/ignitionrobotics/ign-msgs/get/cd27f8476eb9e4f7a41c1ab1d37dbadc69fd7f24.tar.gz"
-  version "0.9.9~20171031~cd27f84"
-  sha256 "51fe7d48c3be01305a7bc379119151f16b3b05e1c4b1bc6a418afb6383f8b550"
+  url "http://gazebosim.org/distributions/ign-msgs/releases/ignition-msgs-1.0.0~pre3.tar.bz2"
+  version "1.0.0~pre3"
+  sha256 "ee007c3a3c8afe445cddeb1ad4367b112e7ae0bd77192b423f12742385b70ec3"
+  version_scheme 1
 
   head "https://bitbucket.org/ignitionrobotics/ign-msgs", :branch => "default", :using => :hg
 
-  #bottle do
-  #  root_url "http://gazebosim.org/distributions/ign-msgs/releases"
-  #  sha256 "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF" => :high_sierra
-  #  sha256 "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF" => :sierra
-  #  sha256 "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF" => :el_capitan
-  #end
+  bottle do
+    root_url "http://gazebosim.org/distributions/ign-msgs/releases"
+    sha256 "ee6402686cb14b22d04ff886d11429859ead8c40ceacb9c6a4e1672a19f56a23" => :high_sierra
+    sha256 "77ae6cae651c708b4295b18758e09b1352f97cf81617cb58a481ae51c42c7125" => :sierra
+    sha256 "4b60d68941535921c7a1a0dd0409180d22790950a6d8fa07301814c19616726e" => :el_capitan
+  end
 
   depends_on "cmake" => :run
   depends_on "ignition-cmake0"
