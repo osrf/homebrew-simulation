91e3abe31e7c2b928d69daf4ebf4c04890e49a1e
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-12-02 18:59:31 +0100

    Fixes and new sdf6 formula

---
+++ sdformat6.rb
@@ -0,0 +1,65 @@
+class Sdformat5 < Formula
+  desc "Simulation Description Format"
+  homepage "http://sdformat.org"
+  url "http://gazebosim.org/distributions/sdformat/get/db654f37b4e76ca25a86b8ccac735e730eb5e783.tar.gz"
+  version "5.99.99~20181201~db654f3"
+  sha256 "e5946e84431cf7874cf422d5b5a9f34f42b31d82b5baea532d1e466011bd89e0"
+
+  head "https://bitbucket.org/osrf/sdformat", :branch => "default", :using => :hg
+
+  depends_on "cmake" => :build
+
+  depends_on "boost"
+  depends_on "doxygen"
+  depends_on "ignition-math4"
+  depends_on "pkg-config" => :run
+  depends_on "ros/deps/urdfdom" => :optional
+  depends_on "tinyxml"
+
+  conflicts_with "sdformat", :because => "Differing version of the same formula"
+  conflicts_with "sdformat3", :because => "Differing version of the same formula"
+  conflicts_with "sdformat4", :because => "Differing version of the same formula"
+  conflicts_with "sdformat5", :because => "Differing version of the same formula"
+
+  def install
+    ENV.m64
+
+    cmake_args = std_cmake_args
+    cmake_args << "-DUSE_EXTERNAL_URDF:BOOL=True" if build.with? "urdfdom"
+    cmake_args << ".."
+
+    mkdir "build" do
+      system "cmake", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <iostream>
+      #include "sdf/sdf.hh"
+      const std::string sdfString(
+        "<sdf version='1.5'>"
+        "  <model name='example'>"
+        "    <link name='link'>"
+        "      <sensor type='gps' name='mysensor' />"
+        "    </link>"
+        "  </model>"
+        "</sdf>");
+      int main() {
+        sdf::SDF modelSDF;
+        modelSDF.SetFromString(sdfString);
+        std::cout << modelSDF.ToString() << std::endl;
+      }
+    EOS
+    system "pkg-config", "sdformat"
+    cflags = `pkg-config --cflags sdformat`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lsdformat",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+  end
+end
