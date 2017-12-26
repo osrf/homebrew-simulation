308e7a7ca74c9de9e4f80540942095b71fd4662c
chapulina <burajiru.no.chapulina@gmail.com>
2017-12-26 10:09:39 -0800

    ign-gui depend on rendering0 and msgs1

--- ignition-gui0.rb
+++ ignition-gui0.rb
@@ -21,6 +21,8 @@ class IgnitionGui0 < Formula
   depends_on "qwt"
   depends_on "tinyxml2"
   depends_on "ignition-common0"
+  depends_on "ignition-rendering0"
+  depends_on "ignition-msgs1"
   depends_on "ignition-transport4"
 
   depends_on "pkg-config" => :run
