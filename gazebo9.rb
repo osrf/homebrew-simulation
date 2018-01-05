3bb3187cba0a39707b843df9c5847d58dc2c48ad
Steven Peters <scpeters@openrobotics.org>
2018-01-05 10:55:30 -0800

    gazebo9 formula, depend on fuel-tools

---
+++ gazebo9.rb
@@ -0,0 +1,72 @@
+class Gazebo9 < Formula
+  desc "Gazebo robot simulator"
+  homepage "http://gazebosim.org"
+  url "https://bitbucket.org/osrf/gazebo/get/025c9df56cfd.tar.gz"
+  sha256 "b8519071072f758d02ddded69a8d9a972a82f0cdc5f9120dec64153ef8294851"
+  version "8.99.99~20180105~025c9df56cfd"
+
+  head "https://bitbucket.org/osrf/gazebo", :branch => "default", :using => :hg
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "boost"
+  depends_on "doxygen"
+  depends_on "freeimage"
+  depends_on "ignition-fuel-tools"
+  depends_on "ignition-math3"
+  depends_on "ignition-msgs0"
+  depends_on "ignition-transport3"
+  depends_on "libtar"
+  depends_on "ogre1.9"
+  depends_on "protobuf"
+  depends_on "protobuf-c"
+  depends_on "qt"
+  depends_on "qwt"
+  depends_on "sdformat5"
+  depends_on "tbb"
+  depends_on "tinyxml"
+  depends_on "tinyxml2"
+
+  depends_on "ossp-uuid" => :linked
+  depends_on "zeromq" => :linked
+
+  depends_on "bullet" => :recommended
+  depends_on "dartsim/dart/dartsim6" => :optional
+  depends_on "ffmpeg" => :recommended
+  depends_on "gdal" => :optional
+  depends_on "gts" => :recommended
+  depends_on "player" => :optional
+  depends_on "simbody" => :recommended
+
+  conflicts_with "gazebo2", :because => "Differing version of the same formula"
+  conflicts_with "gazebo3", :because => "Differing version of the same formula"
+  conflicts_with "gazebo4", :because => "Differing version of the same formula"
+  conflicts_with "gazebo5", :because => "Differing version of the same formula"
+  conflicts_with "gazebo6", :because => "Differing version of the same formula"
+  conflicts_with "gazebo7", :because => "Differing version of the same formula"
+  conflicts_with "gazebo8", :because => "Differing version of the same formula"
+
+  patch do
+    # Fix build when homebrew python is installed
+    url "https://gist.githubusercontent.com/scpeters/9199370/raw/afe595587e38737c537124a3652db99de026c272/brew_python_fix.patch"
+    sha256 "c4774f64c490fa03236564312bd24a8630963762e25d98d072e747f0412df18e"
+  end
+
+  def install
+    ENV.m64
+
+    cmake_args = std_cmake_args
+    cmake_args << "-DQWT_WIN_INCLUDE_DIR=#{HOMEBREW_PREFIX}/lib/qwt.framework/Headers"
+    cmake_args << "-DQWT_WIN_LIBRARY_DIR=#{HOMEBREW_PREFIX}/lib/qwt.framework"
+
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "#{bin}/gz", "sdf"
+  end
+end
