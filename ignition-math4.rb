6984e9cf0ea3842982cf9bffc8e535e54c6023a9
Chris Lalancette <clalancette@gmail.com>
2018-01-09 13:36:48 -0500

    Ignition rndf use ign-cmake (#345)
    * Make a copy of ignition-rndf to ignition-rndf0.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Fix the dependencies for ignition-rndf0.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Have a single formula with Aliases.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Disable ignition-math4 conflict with ignition-math3.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Make the class name IgnitionRndf0.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Disable bottles and point to a temporary commit hash.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- ignition-math4.rb
+++ ignition-math4.rb
@@ -19,7 +19,6 @@ class IgnitionMath4 < Formula
   depends_on "ignition-cmake0"
 
   conflicts_with "ignition-math2", :because => "Symbols collision between the two libraries"
-  conflicts_with "ignition-math3", :because => "Symbols collision between the two libraries"
 
   def install
     system "cmake", ".", *std_cmake_args
