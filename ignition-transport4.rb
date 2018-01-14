f91eb1128c0a48abf94a7f5e1f350118573feac8
Steven Peters <scpeters@openrobotics.org>
2018-01-14 15:06:17 -0800

    ignition-transport4: new commit, build bottles

--- ignition-transport4.rb
+++ ignition-transport4.rb
@@ -1,19 +1,19 @@
 class IgnitionTransport4 < Formula
   desc "Transport middleware for robotics"
   homepage "https://ignitionrobotics.org"
-  url "https://bitbucket.org/ignitionrobotics/ign-transport/get/4fbebfb97acb.tar.gz"
-  version "3.9.9~20171212~4fbebfb97acb"
-  sha256 "1c20423edc743b365e3246f042e5d395268bca1a4a3464d25f19c7d734e719eb"
+  url "https://bitbucket.org/ignitionrobotics/ign-transport/get/f6f114bd5d57.tar.gz"
+  version "3.9.9~20180112~f6f114bd5d57"
+  sha256 "bbe7522988d2f6af009ae266ce7decb942094a8f7f72e997c3914bb027f2ef59"
 
   head "https://bitbucket.org/ignitionrobotics/ign-transport", :branch => "ign-transport4", :using => :hg
 
-  # bottle do
-  #   root_url "http://gazebosim.org/distributions/ign-transport/releases"
-  #   cellar :any
-  #   sha256 "3b106604af2e8ccfb665ee0d3e677b3a075ec380ee7d818b33892c2ba965bbc3" => :high_sierra
-  #   sha256 "c7a355b6aa97db3d8dae30362db6747173cc49977569e10442b827805cfc7649" => :sierra
-  #   sha256 "f142839194381a6da9ca6dbe1a3c7c83238dc93245b12924fde9fea791e1b829" => :el_capitan
-  # end
+  bottle do
+    root_url "http://gazebosim.org/distributions/ign-transport/releases"
+    cellar :any
+    sha256 "3b106604af2e8ccfb665ee0d3e677b3a075ec380ee7d818b33892c2ba965bbc3" => :high_sierra
+    sha256 "c7a355b6aa97db3d8dae30362db6747173cc49977569e10442b827805cfc7649" => :sierra
+    sha256 "f142839194381a6da9ca6dbe1a3c7c83238dc93245b12924fde9fea791e1b829" => :el_capitan
+  end
 
   depends_on "cmake" => :run
   depends_on "doxygen" => [:build, :optional]
