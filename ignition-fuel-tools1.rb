607868e11523a27c6a626cd807e6033d73f8ef0e
osrfbuild <osrfbuild@osrfoundation.org>
2018-01-19 09:37:56 +0100

    ignition-fuel_tools 1.0.0~pre1 (#364)
    * ignition-fuel_tools 1.0.0~pre1
    
    * test: fuel-tools -> fuel_tools
    
    * bump version_scheme
    
    * ignition-fuel-tools1 1.0.0~pre1 high_sierra bottle
    
    * ignition-fuel-tools1 1.0.0~pre1 el_capitan bottle
    
    * ignition-fuel-tools1 1.0.0~pre1 sierra bottle

--- ignition-fuel-tools1.rb
+++ ignition-fuel-tools1.rb
@@ -1,15 +1,16 @@
 class IgnitionFuelTools1 < Formula
   desc "Tools for using fuel API to download robot models"
   homepage "https://ignitionrobotics.org"
-  url "https://bitbucket.org/ignitionrobotics/ign-fuel-tools/get/dbae9593b26ed676eb0046b6304aa89f5aa67e72.tar.gz"
-  version "1.0.0~20180118~1a1641ce8e"
-  sha256 "1a1641ce8e93fdb73ed99ea008bd0f81d2a58dbcf7c6c3e487a1609223841c88"
+  url "http://gazebosim.org/distributions/ign-fuel-tools/releases/ignition-fuel_tools-1.0.0~pre1.tar.bz2"
+  version "1.0.0~pre1"
+  sha256 "f08924998d82c95b195d3bd72773a3288d978239671d7f1733ab9007deacfa8f"
+  version_scheme 1
 
   bottle do
     root_url "http://gazebosim.org/distributions/ign-fuel-tools/releases"
-    sha256 "797425b88adb94256f5c15b409c5c122f32413a67afa8a2af83add256d13fcaf" => :high_sierra
-    sha256 "651f8b58f2bdb43755dd5a62c53e6b883b3f5d399e7e28459bdb21b6161beea6" => :sierra
-    sha256 "7e13fe6b088be88127cdde433fd63cf299a4e06aa11fad068e7163353b4bfcaa" => :el_capitan
+    sha256 "ea7e192e2b8862a04ebfd39da28818c89ee29ba26825385fd7cae15dcf067ef5" => :high_sierra
+    sha256 "736016a36f7c0a8d843646e75c32004ee05f03aeb84760a249be13d5abfc4775" => :sierra
+    sha256 "6644ba0d10032c579cf9cf8d84476f7112e714b46b37eec3d0b276e810c4caf0" => :el_capitan
   end
 
   depends_on "cmake" => :run
@@ -29,7 +30,7 @@ class IgnitionFuelTools1 < Formula
 
   test do
     (testpath/"test.cpp").write <<-EOS
-      #include <ignition/fuel-tools.hh>
+      #include <ignition/fuel_tools.hh>
       int main() {
         ignition::fuel_tools::ServerConfig srv;
         return 0;
@@ -37,19 +38,19 @@ class IgnitionFuelTools1 < Formula
     EOS
     (testpath/"CMakeLists.txt").write <<-EOS
       cmake_minimum_required(VERSION 2.8 FATAL_ERROR)
-      find_package(ignition-fuel-tools1 QUIET REQUIRED)
-      include_directories(${IGNITION-FUEL-TOOLS_INCLUDE_DIRS})
-      link_directories(${IGNITION-FUEL-TOOLS_LIBRARY_DIRS})
+      find_package(ignition-fuel_tools1 QUIET REQUIRED)
+      include_directories(${IGNITION-FUEL_TOOLS_INCLUDE_DIRS})
+      link_directories(${IGNITION-FUEL_TOOLS_LIBRARY_DIRS})
       add_executable(test_cmake test.cpp)
-      target_link_libraries(test_cmake ${IGNITION-FUEL-TOOLS_LIBRARIES})
+      target_link_libraries(test_cmake ${IGNITION-FUEL_TOOLS_LIBRARIES})
     EOS
     # test building with pkg-config
-    system "pkg-config", "ignition-fuel-tools1"
-    cflags = `pkg-config --cflags ignition-fuel-tools1`.split(" ")
+    system "pkg-config", "ignition-fuel_tools1"
+    cflags = `pkg-config --cflags ignition-fuel_tools1`.split(" ")
     system ENV.cc, "test.cpp",
                    *cflags,
                    "-L#{lib}",
-                   "-lignition-fuel-tools1",
+                   "-lignition-fuel_tools1",
                    "-lc++",
                    "-o", "test"
     system "./test"
