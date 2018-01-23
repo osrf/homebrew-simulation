ebd5927feec03fde4fef15123f74611cfd08a5a0
Steven Peters <scpeters@openrobotics.org>
2018-01-23 11:21:34 -0800

    add bottles

--- gazebo9.rb
+++ gazebo9.rb
@@ -7,6 +7,13 @@ class Gazebo9 < Formula
 
   head "https://bitbucket.org/osrf/gazebo", :branch => "default", :using => :hg
 
+  bottle do
+    root_url "http://gazebosim.org/distributions/gazebo/releases"
+    sha256 "c2e9cdb9894c5f4b1c3413cdbebefcca1189d255f9cfd8550aa947b306531cff" => :high_sierra
+    sha256 "4cbd66c4d028115f55ded390ba472720c29139863dcbff3b735d40c21ac5b9a1" => :sierra
+    sha256 "207a41808a55897963e4f05a39ba6d7b6c639fd1fab4eb1c13a51bae16d36c29" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
 
