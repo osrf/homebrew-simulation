f45f4f8e47dec28e13d6f3bc69d2fd5f3312879a
osrfbuild <osrfbuild@osrfoundation.org>
2018-02-22 20:56:46 +0100

    ignition-tools 0.1.0 (#382)
    * ignition-tools 0.1.0
    
    * fix test
    
    * bottle for el_capitan_or_later
    
    * remove _or_later to fix bottle build
    
    * ignition-tools 0.1.0 el_capitan bottle
    
    * bottle el_capitan_or_later

--- ignition-tools.rb
+++ ignition-tools.rb
@@ -1,10 +1,15 @@
 class IgnitionTools < Formula
   desc "Entry point for ignition command-line tools"
   homepage "https://ignitionrobotics.org"
-  url "https://bitbucket.org/ignitionrobotics/ign-tools/get/381c0a968fa.tar.gz"
-  version "0.0.0-20160209-381c0a968fa"
-  sha256 "8ea662be885d84342c2522c41b6d24cd42adf8e1e9e4e21fdfd2c4fbbc62a170"
+  url "http://gazebosim.org/distributions/ign-tools/releases/ignition-tools-0.1.0.tar.bz2"
+  sha256 "67d41587cf9ab77a0e6586597d5b38e75e17b1ee122ca24414ec5ce9c6b89bc0"
   head "https://bitbucket.org/ignitionrobotics/ign-tools", :branch => "default", :using => :hg
+
+  bottle do
+    root_url "http://gazebosim.org/distributions/ign-tools/releases"
+    cellar :any
+    sha256 "9749ed1c0a0fae437ba4f7f2a4605c1defb7c78b3306d7f32b3e322e72d0f511" => :el_capitan_or_later
+  end
 
   depends_on "cmake" => :build
 
@@ -16,6 +21,18 @@ class IgnitionTools < Formula
   end
 
   test do
-    system "false"
+    mkdir testpath/"config"
+    (testpath/"config/test.yaml").write <<~EOS
+      --- # Test subcommand
+      format: 1.0.0
+      library_name: test
+      library_path: path
+      library_version: 2.0.0
+      commands:
+          - test  : Test utility
+      ---
+    EOS
+    ENV["IGN_CONFIG_PATH"] = testpath/"config/"
+    system "#{bin}/ign", "test", "--versions"
   end
 end
