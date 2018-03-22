87fcc84dcefd014dbfa5d9b133d08d8b868a5ae5
Steven Peters <scpeters@openrobotics.org>
2018-03-22 12:43:49 -0700

    ignition-cmake1: new formula

--- ignition-cmake1.rb
+++ ignition-cmake1.rb
@@ -1,16 +1,11 @@
-class IgnitionCmake0 < Formula
+class IgnitionCmake1 < Formula
   desc "CMake helper functions for building robotic applications"
   homepage "https://ignitionrobotics.org"
-  url "http://gazebosim.org/distributions/ign-cmake/releases/ignition-cmake-0.5.0.tar.bz2"
-  sha256 "658e616cc21015166f5b69e3b5ba7f8677e7a39b357ec47115608e323c7e6555"
+  url "https://bitbucket.org/ignitionrobotics/ign-cmake/get/c4a7896c462256e1a6d567480335e73173ef1285.tar.gz"
+  version "0.999.999~20180322~c4a7896"
+  sha256 "81f9b84fb353c48452a2e9b16672a616f1c5d0a33ae85f1cb8a578a660f19e18"
 
-  head "https://bitbucket.org/ignitionrobotics/ign-cmake", :branch => "ign-cmake0", :using => :hg
-
-  bottle do
-    root_url "http://gazebosim.org/distributions/ign-cmake/releases"
-    cellar :any_skip_relocation
-    sha256 "60367e2968a95d7ccd902aa8901216ee44c5bb04caac948f430a645352579485" => :el_capitan_or_later
-  end
+  head "https://bitbucket.org/ignitionrobotics/ign-cmake", :branch => "default", :using => :hg
 
   depends_on "cmake" => :run
 
@@ -24,10 +19,9 @@ class IgnitionCmake0 < Formula
   test do
     (testpath/"CMakeLists.txt").write <<-EOS
       cmake_minimum_required(VERSION 3.5.1 FATAL_ERROR)
-      find_package(ignition-cmake0 REQUIRED)
+      find_package(ignition-cmake1 REQUIRED)
       ign_configure_project(test 0.1.0)
       ign_configure_build(QUIT_IF_BUILD_ERRORS)
-      #ign_create_packages()
     EOS
     %w[doc include src test].each do |dir|
       mkdir dir do
