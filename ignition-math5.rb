1b1a669205cb8a7790de0910d9b06e564cabd7b7
osrfbuild <osrfbuild@osrfoundation.org>
2018-03-29 06:29:17 +0200

    ignition-math5 5.0.0~pre1 (#390)

--- ignition-math5.rb
+++ ignition-math5.rb
@@ -1,9 +1,9 @@
 class IgnitionMath5 < Formula
   desc "Math API for robotic applications"
   homepage "https://ignitionrobotics.org"
-  url "https://bitbucket.org/ignitionrobotics/ign-math/get/f14b8b7c1f74fe9962e0b1dc90b9c16af4f4e348.tar.gz"
-  version "4.99.99~20180313~f14b8b7"
-  sha256 "63b7be729858a6d72f0e2b52ca397f0200b360a9e4ca33003607058c25ad2109"
+  url "http://gazebosim.org/distributions/ign-math/releases/ignition-math5-5.0.0~pre1.tar.bz2"
+  version "5.0.0~pre1"
+  sha256 "e2fb9aa441992428e380e1de17ef00434164c54ea91fb62e251a3a39fc14483e"
 
   head "https://bitbucket.org/ignitionrobotics/ign-math", :branch => "default", :using => :hg
 
