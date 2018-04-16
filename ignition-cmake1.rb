f0a47b69dd5a45b8d876a5be91903755d71063bb
osrfbuild <osrfbuild@osrfoundation.org>
2018-04-16 22:43:55 +0200

    ignition-cmake1 1.0.0~pre2 (#400)
    * ignition-cmake1 1.0.0~pre2
    
    fix test
    don't call pkg-config ignition-cmake1
    enable bottles
    
    * ignition-cmake1 1.0.0~pre2 el_capitan_or_later bottle

--- ignition-cmake1.rb
+++ ignition-cmake1.rb
@@ -1,11 +1,17 @@
 class IgnitionCmake1 < Formula
   desc "CMake helper functions for building robotic applications"
   homepage "https://ignitionrobotics.org"
-  url "https://bitbucket.org/ignitionrobotics/ign-cmake/get/467f2033527e.tar.gz"
-  version "0.999.1999~20180328~467f203"
-  sha256 "d43497304aeab5520e607cbecf1bcdd121970661c265c45622889ae519a7b099"
+  url "http://gazebosim.org/distributions/ign-cmake/releases/ignition-cmake1-1.0.0~pre2.tar.bz2"
+  version "1.0.0~pre2"
+  sha256 "29d5746b8a47f5bbcc72c59358cd49ecbe525fa07b83d08edcfb231881ae37dd"
 
   head "https://bitbucket.org/ignitionrobotics/ign-cmake", :branch => "default", :using => :hg
+
+  bottle do
+    root_url "http://gazebosim.org/distributions/ign-cmake/releases"
+    cellar :any_skip_relocation
+    sha256 "cd2855452d5c2800defb22a5562d8474cd3fd0ec92654bc7aebe4fd91a67cca9" => :el_capitan_or_later
+  end
 
   depends_on "cmake"
   depends_on "pkg-config"
@@ -20,8 +26,9 @@ class IgnitionCmake1 < Formula
   test do
     (testpath/"CMakeLists.txt").write <<-EOS
       cmake_minimum_required(VERSION 3.5.1 FATAL_ERROR)
+      project(ignition-test VERSION 0.1.0)
       find_package(ignition-cmake1 REQUIRED)
-      ign_configure_project(test 0.1.0)
+      ign_configure_project()
       ign_configure_build(QUIT_IF_BUILD_ERRORS)
     EOS
     %w[doc include src test].each do |dir|
@@ -32,6 +39,5 @@ class IgnitionCmake1 < Formula
     mkdir "build" do
       system "cmake", ".."
     end
-    system "pkg-config", "ignition-cmake1"
   end
 end
