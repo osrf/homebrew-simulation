b988f3b0c4873cbaccb58f30f44d70c71b2f8186
Steven Peters <scpeters@openrobotics.org>
2018-04-24 18:09:43 -0700

    ignition-transport5: new formula

---
+++ ignition-transport5.rb
@@ -0,0 +1,59 @@
+class IgnitionTransport5 < Formula
+  desc "Transport middleware for robotics"
+  homepage "https://ignitionrobotics.org"
+  url "https://bitbucket.org/ignitionrobotics/ign-transport/get/e8cb475abe13d7f9fd14d2b354f45145f0b63875.tar.gz"
+  version "4.99.99~20180424~e8cb475"
+  sha256 "3f435f6a15592d91bfeb29895f36b1d61ccc12e67558a7ed868cc7f4d8378887"
+
+  head "https://bitbucket.org/ignitionrobotics/ign-transport", :branch => "default", :using => :hg
+
+  depends_on "doxygen" => [:build, :optional]
+
+  depends_on "cmake"
+  depends_on "cppzmq"
+  depends_on "ignition-cmake1"
+  depends_on "ignition-msgs2"
+  depends_on "ignition-tools"
+  depends_on "ossp-uuid"
+  depends_on "pkg-config"
+  depends_on "protobuf"
+  depends_on "protobuf-c" => :build
+  depends_on "sqlite"
+  depends_on "zeromq"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <iostream>
+      #include <ignition/transport.hh>
+      int main() {
+        ignition::transport::Node node;
+        return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 2.8 FATAL_ERROR)
+      find_package(ignition-transport5 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-transport5::ignition-transport5)
+    EOS
+    system "pkg-config", "ignition-transport5"
+    cflags = `pkg-config --cflags ignition-transport5`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lignition-transport5",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+  end
+end
