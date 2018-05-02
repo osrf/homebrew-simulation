377794eff54b9eaf8df492061f54a46b00d29aeb
osrfbuild <osrfbuild@osrfoundation.org>
2018-05-02 02:56:20 +0200

    ignition-cmake 0.6.0 (#409)
    * ignition-cmake 0.6.0
    
    * ignition-cmake0 0.6.0 el_capitan_or_later bottle

--- ignition-cmake0.rb
+++ ignition-cmake0.rb
@@ -1,15 +1,15 @@
 class IgnitionCmake0 < Formula
   desc "CMake helper functions for building robotic applications"
   homepage "https://ignitionrobotics.org"
-  url "http://gazebosim.org/distributions/ign-cmake/releases/ignition-cmake-0.5.0.tar.bz2"
-  sha256 "658e616cc21015166f5b69e3b5ba7f8677e7a39b357ec47115608e323c7e6555"
+  url "http://gazebosim.org/distributions/ign-cmake/releases/ignition-cmake-0.6.0.tar.bz2"
+  sha256 "0a4d2316f6820b1533cf79753ad676d5c04fa7f903d603224b8ea8e9caf42200"
 
   head "https://bitbucket.org/ignitionrobotics/ign-cmake", :branch => "ign-cmake0", :using => :hg
 
   bottle do
     root_url "http://gazebosim.org/distributions/ign-cmake/releases"
     cellar :any_skip_relocation
-    sha256 "60367e2968a95d7ccd902aa8901216ee44c5bb04caac948f430a645352579485" => :el_capitan_or_later
+    sha256 "007c3f42eb0aa6d37221df520537910995ada49cbc6c1746b8ff8854df07d3af" => :el_capitan_or_later
   end
 
   depends_on "cmake"
