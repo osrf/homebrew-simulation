f075e7b78ed9985f6ae679eac62df12dd1f467e8
osrfbuild <osrfbuild@osrfoundation.org>
2018-05-24 05:33:00 +0200

    ignition-common 1.1.1 (#418)
    * ignition-common 1.1.1
    
    * remove old patch
    
    * ignition-common1 1.1.1 el_capitan bottle
    
    * ignition-common1 1.1.1 sierra bottle
    
    * ignition-common1 1.1.1 high_sierra bottle

--- ignition-common1.rb
+++ ignition-common1.rb
@@ -1,17 +1,16 @@
 class IgnitionCommon1 < Formula
   desc "Common libraries for robotics applications"
   homepage "https://bitbucket.org/ignitionrobotics/ign-common"
-  url "http://gazebosim.org/distributions/ign-common/releases/ignition-common-1.1.0.tar.bz2"
-  sha256 "e05d39a2b48c49bbe92701db25cab790d3d5a7f5eac6aa6a9fc1edeadbd7e6ea"
-  revision 1
+  url "http://gazebosim.org/distributions/ign-common/releases/ignition-common-1.1.1.tar.bz2"
+  sha256 "2e8b65c9390bc78088865d95c0933c564b07b3b55b68c14e1c6d947ca8d9525a"
 
   head "https://bitbucket.org/ignitionrobotics/ign-common", :branch => "default", :using => :hg
 
   bottle do
     root_url "http://gazebosim.org/distributions/ign-common/releases"
-    sha256 "f7be30a707380232e4fc3c74847259e0a3b8c6aaa2cd814e88f52a13aa4da930" => :high_sierra
-    sha256 "514e08b1536386559b4f4b8815e97e62fe9724fd8b96ab53ded66ff2015c474f" => :sierra
-    sha256 "5af33ed88052a3f8442fff34b9bbc6e7b67d0277b991da16e56117ae5da221ce" => :el_capitan
+    sha256 "c2f06b275a5e82784417e72d9fe40be02731d285a6bd4a4beff29287282335b1" => :high_sierra
+    sha256 "95637ed2153b1a0dde52453bb2686197583d79cba68deccc37089052f9d02fd7" => :sierra
+    sha256 "68058186521d1882c55eaf2ce52b9de8f02287f49b68b2442e09c5a23775aa44" => :el_capitan
   end
 
   depends_on "cmake"
@@ -22,12 +21,6 @@ class IgnitionCommon1 < Formula
   depends_on "ossp-uuid"
   depends_on "pkg-config"
   depends_on "tinyxml2"
-
-  patch do
-    # Fix for ffmpeg4
-    url "https://bitbucket.org/ignitionrobotics/ign-common/commits/d937173602af5e6d5c22ced4a80bd0cf3f2f9fff/raw/"
-    sha256 "564ca08bcd547df579a42d4f94aeae0423723b21eb67a9c9d4be2c3025f7dcb4"
-  end
 
   def install
     system "cmake", ".", *std_cmake_args
