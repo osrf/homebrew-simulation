c25f6ad0e06fa901e653c04670d9d7b157e43ef2
Steven Peters <scpeters@openrobotics.org>
2018-06-14 15:11:50 -0700

    fix audit

--- sdformat8.rb
+++ sdformat8.rb
@@ -18,8 +18,8 @@ class Sdformat8 < Formula
   depends_on "doxygen"
   depends_on "ignition-math6"
   depends_on "pkg-config"
-  depends_on "ros/deps/urdfdom" => :optional
   depends_on "tinyxml"
+  depends_on "ros/deps/urdfdom" => :optional
 
   conflicts_with "sdformat", :because => "Differing version of the same formula"
   conflicts_with "sdformat3", :because => "Differing version of the same formula"
