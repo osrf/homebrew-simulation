8f94fc42ffad79965563b3a0df1f267b760cef1c
osrfbuild <osrfbuild@osrfoundation.org>
2018-06-16 00:13:08 +0200

    ignition-math5 5.0.0~pre2 (#431)
    * ignition-math5 5.0.0~pre2
    
    * add bottle block
    
    * ignition-math5 5.0.0~pre2 sierra bottle
    
    * ignition-math5 5.0.0~pre2 el_capitan bottle
    
    * ignition-math5 5.0.0~pre2 high_sierra bottle

--- ignition-math5.rb
+++ ignition-math5.rb
@@ -1,11 +1,19 @@
 class IgnitionMath5 < Formula
   desc "Math API for robotic applications"
   homepage "https://ignitionrobotics.org"
-  url "http://gazebosim.org/distributions/ign-math/releases/ignition-math5-5.0.0~pre1.tar.bz2"
-  version "5.0.0~pre1"
-  sha256 "e2fb9aa441992428e380e1de17ef00434164c54ea91fb62e251a3a39fc14483e"
+  url "http://gazebosim.org/distributions/ign-math/releases/ignition-math5-5.0.0~pre2.tar.bz2"
+  version "5.0.0~pre2"
+  sha256 "e4dda0631f79bf7f7fd2318265a08b7eb0f0826fa22213a82e437aa00c7f047f"
 
   head "https://bitbucket.org/ignitionrobotics/ign-math", :branch => "default", :using => :hg
+
+  bottle do
+    root_url "http://gazebosim.org/distributions/ign-math/releases"
+    cellar :any
+    sha256 "6a9dfb8d6b0b437e425ccf5e02986c55a7d3c30f5beaa60d966a541cba89da88" => :high_sierra
+    sha256 "5f5a8e37999d82493bf66a49602246e8c08a48020f71555c28adef974e2b63c4" => :sierra
+    sha256 "4ee3fc61a051bb38b086308e2c0adf492b09ff699464ab30ec8dae3d01f31b4e" => :el_capitan
+  end
 
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
