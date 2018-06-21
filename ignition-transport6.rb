191468cf64ee61b96ede6cf1860d092175628459
Nate Koenig <nkoenig@users.noreply.github.com>
2018-06-20 17:33:49 -0700

    ignition-transport6: new formula from gz11 branch (#438)
    * New formula for ign-transport6
    
    * ign-cmake2
    
    * update commit for filesystem fixes
    
    * add bottle block
    
    * ignition-transport6 5.999.999~20180620~e40d5e5 high_sierra bottle

---
+++ ignition-transport6.rb
@@ -0,0 +1,61 @@
+class IgnitionTransport6 < Formula
+  desc "Transport middleware for robotics"
+  homepage "https://ignitionrobotics.org"
+  url "https://bitbucket.org/ignitionrobotics/ign-transport/get/e40d5e54ccfb716bc975f75c312b3171da36c15e.tar.gz"
+  version "5.999.999~20180620~e40d5e5"
+  sha256 "dd2ddaed5d896229a66d34d681668a27690a07d6d8514e9a0b536c2959aad5de"
+
+  bottle do
+    root_url "http://gazebosim.org/distributions/ign-transport/releases"
+    sha256 "799874361c492b28f8ea72c14a32d7f01c1816966eb11853ceafa4586bee03f4" => :high_sierra
+  end
+
+  depends_on "doxygen" => [:build, :optional]
+  depends_on "protobuf-c" => :build
+
+  depends_on "cmake"
+  depends_on "cppzmq"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-msgs3"
+  depends_on "ignition-tools"
+  depends_on "ossp-uuid"
+  depends_on "pkg-config"
+  depends_on "protobuf"
+  depends_on "zeromq"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <iostream>
+      #include <ignition/transport.hh>
+      int main() {
+        ignition::transport::Node node;
+        return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.10 FATAL_ERROR)
+      find_package(ignition-transport6 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-transport6::ignition-transport6)
+    EOS
+    system "pkg-config", "ignition-transport6"
+    cflags = `pkg-config --cflags ignition-transport6`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lignition-transport6",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+  end
+end
