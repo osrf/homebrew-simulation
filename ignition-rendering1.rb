88a4d89c42473dc542aeb020d32ff11a53f55636
Steven Peters <scpeters@openrobotics.org>
2018-07-20 17:31:51 -0700

    disable rendering bottles

--- ignition-rendering1.rb
+++ ignition-rendering1.rb
@@ -5,12 +5,12 @@ class IgnitionRendering1 < Formula
   version "0.999.999~20180720~8cbb07b"
   sha256 "d40c67b5e273720a9ec6f7e42d3ab674d464a20f0fcd5e512a2997375c22c6b1"
 
-  bottle do
-    root_url "http://gazebosim.org/distributions/ign-rendering/releases"
-    sha256 "8b8c253114e3c6af1d6978e79b02c3c10895a5fe276a21570e370eaa4252491d" => :high_sierra
-    sha256 "520b171c51d0415f99ae7c8ecdd955249d166bad89edcad83879b85735c9a80f" => :sierra
-    sha256 "42acd100d3187950a56ec3fde87349feb78368d7c3552a9a1344f0c031511f72" => :el_capitan
-  end
+  # bottle do
+  #   root_url "http://gazebosim.org/distributions/ign-rendering/releases"
+  #   sha256 "8b8c253114e3c6af1d6978e79b02c3c10895a5fe276a21570e370eaa4252491d" => :high_sierra
+  #   sha256 "520b171c51d0415f99ae7c8ecdd955249d166bad89edcad83879b85735c9a80f" => :sierra
+  #   sha256 "42acd100d3187950a56ec3fde87349feb78368d7c3552a9a1344f0c031511f72" => :el_capitan
+  # end
 
   depends_on "cmake" => :build
 
