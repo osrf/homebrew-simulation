91071b9254f411d8cb75d1eb9c518f96ecd3b50f
Nate Koenig <natekoenig@gmail.com>
2018-09-11 11:59:11 -0700

    Updating url and sha

--- ignition-plugin0.rb
+++ ignition-plugin0.rb
@@ -1,9 +1,9 @@
 class IgnitionPlugin0 < Formula
   desc "Plugin libraries for robotics applications"
   homepage "https://bitbucket.org/ignitionrobotics/ign-plugin"
-  url "http://gazebosim.org/distributions/ign-plugin/releases/ignition-plugin-0.1.0~pre2.tar.bz2"
-  version "0.1.0~pre2"
-  sha256 "04efa0e1a5437ae800f770f997bf8ebcbb0f9a415d0565a66bebec7ae570ec9d"
+  url "https://bitbucket.org/ignitionrobotics/ign-plugin/get/0640dcb6f62fd2c58a720d2328203b0798c7e61d.tar.gz"
+  version "0.1.0~pre3"
+  sha256 "2ee4554d5f8dc120faa9e5d71974f663bc45356d7e04b2d943cc22c95169ad6b"
 
   bottle do
     root_url "http://gazebosim.org/distributions/ign-plugin/releases"
