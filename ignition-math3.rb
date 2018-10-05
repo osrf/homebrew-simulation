535b2cd2f03e84923a41b9bb2dad8ad277d89ee7
Steven Peters <scpeters@openrobotics.org>
2018-10-05 14:11:33 -0700

    ignition-math3: update 3.3.0 bottle.

--- ignition-math3.rb
+++ ignition-math3.rb
@@ -9,6 +9,7 @@ class IgnitionMath3 < Formula
   bottle do
     root_url "http://gazebosim.org/distributions/ign-math/releases"
     cellar :any
+    sha256 "958ed00cd402ba76a8095adb3aa804243b553b126b60882cd7162dc60d6098d1" => :mojave
     sha256 "79f35d471b53743a1a8ad5d40d64014ef732247312c8b8cb22ce83b40783e945" => :high_sierra
     sha256 "34c194270aa0e1dbf44a11e18313ce986ef7654745e1d01689a73b7a53507720" => :sierra
     sha256 "4f35a563ed082a2c3e72be81b574495eb4d430541bce5323526a4faf6356455c" => :el_capitan
