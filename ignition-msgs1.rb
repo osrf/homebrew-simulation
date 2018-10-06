b5f723805ec4d3147b00eae992ace61db5ec2df2
Steven Peters <scpeters@openrobotics.org>
2018-10-05 17:32:30 -0700

    ignition-msgs1: update 1.0.0_2 bottle.

--- ignition-msgs1.rb
+++ ignition-msgs1.rb
@@ -11,6 +11,7 @@ class IgnitionMsgs1 < Formula
   bottle do
     root_url "http://gazebosim.org/distributions/ign-msgs/releases"
     cellar :any
+    sha256 "a1d2c5cf6b818781917ea40760b1ea2b3c18eb19e08f44906c8481d80e543496" => :mojave
     sha256 "08ac01a6c9e57ffd603d91927b0abfeae240593f9a5703d7871878cf8a85ea2d" => :high_sierra
     sha256 "23c76c6bac0f29e0b660bea0fe2e9e9e94f447ac98df4ff95cad2422d2c063a9" => :sierra
     sha256 "ecd1acd2086d145059d831a094aedeb17a1a94cc8389c5bd321e2bd0fbaeed10" => :el_capitan
