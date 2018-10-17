b0bfa6cdd740397a9570e171b96d234b54cdade8
Steven Peters <scpeters@openrobotics.org>
2018-10-17 12:40:51 -0700

    sdformat7: new formula

---
+++ sdformat7.rb
@@ -0,0 +1,66 @@
+class Sdformat7 < Formula
+  desc "Simulation Description Format"
+  homepage "http://sdformat.org"
+  url "https://bitbucket.org/osrf/sdformat/get/2fed80e6bc44.tar.gz"
+  version "6.999.999~20180905~2fed80e"
+  sha256 "b9792f701be807a9f522b9b9b09c521340744ba1bd593d533765b1dc7bb40bb5"
+
+  head "https://bitbucket.org/osrf/sdformat", :branch => "default", :using => :hg
+
+  depends_on "cmake" => :build
+
+  depends_on "boost"
+  depends_on "doxygen"
+  depends_on "ignition-math5"
+  depends_on "pkg-config"
+  depends_on "tinyxml"
+  depends_on "urdfdom" => :optional
+
+  conflicts_with "sdformat", :because => "Differing version of the same formula"
+  conflicts_with "sdformat3", :because => "Differing version of the same formula"
+  conflicts_with "sdformat4", :because => "Differing version of the same formula"
+  conflicts_with "sdformat5", :because => "Differing version of the same formula"
+  conflicts_with "sdformat6", :because => "Differing version of the same formula"
+
+  def install
+    ENV.m64
+
+    cmake_args = std_cmake_args
+    cmake_args << "-DUSE_EXTERNAL_URDF:BOOL=True" if build.with? "urdfdom"
+    cmake_args << ".."
+
+    mkdir "build" do
+      system "cmake", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <iostream>
+      #include "sdf/sdf.hh"
+      const std::string sdfString(
+        "<sdf version='1.5'>"
+        "  <model name='example'>"
+        "    <link name='link'>"
+        "      <sensor type='gps' name='mysensor' />"
+        "    </link>"
+        "  </model>"
+        "</sdf>");
+      int main() {
+        sdf::SDF modelSDF;
+        modelSDF.SetFromString(sdfString);
+        std::cout << modelSDF.ToString() << std::endl;
+      }
+    EOS
+    system "pkg-config", "sdformat"
+    cflags = `pkg-config --cflags sdformat`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lsdformat",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+  end
+end
