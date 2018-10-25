bd74adc17dc6a891ccba13e54e5f0930a7c4ac20
Steven Peters <scpeters@openrobotics.org>
2018-10-25 14:38:52 -0700

    requires at least high sierra, remove bottle block

--- ignition-rendering1.rb
+++ ignition-rendering1.rb
@@ -5,19 +5,13 @@ class IgnitionRendering1 < Formula
   version "1.0.0~pre3"
   sha256 "201e382313ac9fdf9fcce21b4a6e12bde184c47426b398436062be80a0520399"
 
-  # bottle do
-  #   root_url "http://gazebosim.org/distributions/ign-rendering/releases"
-  #   sha256 "8b8c253114e3c6af1d6978e79b02c3c10895a5fe276a21570e370eaa4252491d" => :high_sierra
-  #   sha256 "520b171c51d0415f99ae7c8ecdd955249d166bad89edcad83879b85735c9a80f" => :sierra
-  #   sha256 "42acd100d3187950a56ec3fde87349feb78368d7c3552a9a1344f0c031511f72" => :el_capitan
-  # end
-
   depends_on "cmake" => :build
 
   depends_on "freeimage"
   depends_on "ignition-cmake2"
   depends_on "ignition-common3"
   depends_on "ignition-math6"
+  depends_on :macos => :high_sierra # c++17
   depends_on "ogre1.9"
   depends_on "pkg-config"
 
