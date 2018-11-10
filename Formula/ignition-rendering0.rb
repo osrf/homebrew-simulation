9196578439a4994fe2cd06343ebb07ec62301b33
Steven Peters <scpeters@openrobotics.org>
2018-11-09 16:37:54 -0800

    tinyxml2@6.2.0: build bottle (#527)
    * tinyxml2@6.2.0: build bottle
    
    * ignition-rendering0: avoid rendering1 conflicts
    
    * fix test
    
    * tinyxml2@6.2.0: update 6.2.0 bottle.

--- Formula/ignition-rendering0.rb
+++ Formula/ignition-rendering0.rb
@@ -4,15 +4,15 @@ class IgnitionRendering0 < Formula
   url "https://bitbucket.org/ignitionrobotics/ign-rendering/get/5b69fe76e650c3d9c087edf50cee10bec0a22e05.tar.gz"
   version "0.0.0~20180823~5b69fe7"
   sha256 "7334dcffffa5674d7905dc6168d7c12fc82279f2f1736ef7ce86664071c43f5c"
-  revision 1
+  revision 2
 
   head "https://bitbucket.org/ignitionrobotics/ign-rendering", :branch => "default", :using => :hg
 
   bottle do
     root_url "http://gazebosim.org/distributions/bottles-simulation"
-    sha256 "5bfb8645b610be2a8f92068927c6eb14bce166e295eb7186edabed2f9a93bf8f" => :mojave
-    sha256 "7492d8f05b6727ca7e38a0df2367668a442bea1e6873000ec27cc6cecae1eb4b" => :high_sierra
-    sha256 "ee7299b00e976c2e9ed82e60f77e8ad9f1eac20d51360cf8714b74d2a25664fa" => :sierra
+    sha256 "86f34785e2785fbfaa12f67e6b5486c16068848c82c5119495beb83d096e9d2e" => :mojave
+    sha256 "135c3ab87141bc8ffdf8fd1a56a4caff7602049a0454f7e5f99019282d4c9845" => :high_sierra
+    sha256 "ce2459a4b043a786c3a559f98436815c59e0b26886aedfbc1a91977406387913" => :sierra
   end
 
   depends_on "cmake" => :build
@@ -23,6 +23,12 @@ class IgnitionRendering0 < Formula
   depends_on "ignition-math5"
   depends_on "ogre1.9"
   depends_on "pkg-config"
+
+  patch do
+    # Don't conflict with ignition-rendering1
+    url "https://bitbucket.org/ignitionrobotics/ign-rendering/commits/8aaa4b8188cae5cd5082e46a05abde6bfcc7fbde/raw/"
+    sha256 "87ca51e370faab94d2ff92bc3fde3d236fe5f264a582a5740e047d85818280a8"
+  end
 
   def install
     system "cmake", ".", *std_cmake_args
