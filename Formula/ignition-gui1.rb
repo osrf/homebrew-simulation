989cca1d05447b0f2fbf9154d23671b2d4db3168
Steven Peters <scpeters@openrobotics.org>
2018-11-17 08:50:32 -0800

    ignition-gui1: use newer commit and build bottle (#531)
    * ignition-gui1: use newer commit and build bottle
    
    * ignition-gui1: depend on tinyxml2@6.2.0
    
    * ignition-gui1: fix test
    
    * ignition-gui1: update 1.0.0~pre2~1~ fae7d4b bottle.

--- Formula/ignition-gui1.rb
+++ Formula/ignition-gui1.rb
@@ -1,11 +1,17 @@
 class IgnitionGui1 < Formula
   desc "Common libraries for robotics applications. GUI Library"
   homepage "https://bitbucket.org/ignitionrobotics/ign-gui"
-  url "http://gazebosim.org/distributions/ign-gui/releases/ignition-gui-1.0.0~pre2.tar.bz2"
-  version "1.0.0~pre2"
-  sha256 "9e822c41f2e631922f31d997599193306f2aad53c94efe60cb8b863301425bb3"
+  url "https://bitbucket.org/ignitionrobotics/ign-gui/get/fae7d4bb536acce627ce971308029ad49cb5fe2a.tar.gz"
+  version "1.0.0~pre2~1~fae7d4b"
+  sha256 "5c7fc684b83b8ac8db930930353e20abf37f6492d60f730ca3978c5774dd2446"
 
   head "https://bitbucket.org/ignitionrobotics/ign-gui", :branch => "gz11", :using => :hg
+
+  bottle do
+    root_url "http://gazebosim.org/distributions/bottles-simulation"
+    sha256 "2489a47d8f8adca2a0b07d1e6989fa2487baa4fa2f7ca3cb71599b620f9b9811" => :mojave
+    sha256 "512f47c998e823b3811eef3ee40d91627591f0c9a069572724c44fcddd748303" => :high_sierra
+  end
 
   depends_on "cmake" => :build
   depends_on "ignition-cmake2"
@@ -18,7 +24,7 @@ class IgnitionGui1 < Formula
   depends_on "pkg-config"
   depends_on "qt"
   depends_on "qwt"
-  depends_on "tinyxml2"
+  depends_on "tinyxml2@6.2.0"
 
   def install
     ENV.m64
@@ -36,33 +42,50 @@ class IgnitionGui1 < Formula
   test do
     (testpath/"test.cpp").write <<-EOS
     #include <iostream>
-    #include <ignition/gui/qt.h>
-    #include <ignition/gui/Iface.hh>
 
-    using namespace ignition;
-    using namespace gui;
+    #ifndef Q_MOC_RUN
+      #include <ignition/gui/qt.h>
+      #include <ignition/gui/Application.hh>
+      #include <ignition/gui/MainWindow.hh>
+    #endif
 
     //////////////////////////////////////////////////
     int main(int _argc, char **_argv)
     {
       std::cout << "Hello, GUI!" << std::endl;
+
       // Increase verboosity so we see all messages
-      setVerbosity(4);
-      // Initialize app
-      initApp();
-      // Create main window
-      createMainWindow();
+      ignition::common::Console::SetVerbosity(4);
+
+      // Create app
+      ignition::gui::Application app(_argc, _argv);
+
+      // Load plugins / config
+      if (!app.LoadPlugin("Publisher"))
+      {
+        return 1;
+      }
+
       // Customize main window
-      auto win = mainWindow();
-      win->setWindowTitle("Hello Window!");
-      // After window is closed
-      stop();
+      auto win = app.findChild<ignition::gui::MainWindow *>()->QuickWindow();
+      win->setProperty("title", "Hello Window!");
+
+      // Run window
+      // app.exec();
+
       std::cout << "After run" << std::endl;
 
       return 0;
     }
     EOS
-    ENV.append_path "PKG_CONFIG_PATH", "#{Formula["qt"].opt_lib}/pkgconfig"
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
+      find_package(ignition-gui1 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-gui1::ignition-gui1)
+    EOS
+    ENV.append_path "PKG_CONFIG_PATH", Formula["qt"].opt_lib/"pkgconfig"
+    ENV.append_path "PKG_CONFIG_PATH", Formula["tinyxml2@6.2.0"].opt_lib/"pkgconfig"
     system "pkg-config", "ignition-gui1"
     cflags   = `pkg-config --cflags ignition-gui1`.split(" ")
     ldflags  = `pkg-config --libs ignition-gui1`.split(" ")
@@ -72,5 +95,14 @@ class IgnitionGui1 < Formula
                    "-lc++",
                    "-o", "test"
     system "./test"
+    # test building with cmake
+    ENV.append_path "CMAKE_PREFIX_PATH", Formula["qt"].opt_prefix
+    mkdir "build" do
+      ENV.delete("MACOSX_DEPLOYMENT_TARGET")
+      ENV.delete("SDKROOT")
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
   end
 end
