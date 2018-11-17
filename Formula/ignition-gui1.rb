affa7b94400694f8a827834af9d404ea3980dcf8
Steven Peters <scpeters@openrobotics.org>
2018-11-17 00:17:45 -0800

    ignition-gui1: fix test

--- Formula/ignition-gui1.rb
+++ Formula/ignition-gui1.rb
@@ -36,33 +36,50 @@ class IgnitionGui1 < Formula
   test do
     (testpath/"test.cpp").write <<-EOS
     #include <iostream>
-    #include <ignition/gui/qt.h>
-    #include <ignition/gui/Iface.hh>
 
-    using namespace ignition;
-    using namespace gui;
+    #ifndef Q_MOC_RUN
+      #include <ignition/gui/qt.h>
+      #include <ignition/gui/Application.hh>
+      #include <ignition/gui/MainWindow.hh>
+    #endif
 
     //////////////////////////////////////////////////
     int main(int _argc, char **_argv)
     {
       std::cout << "Hello, GUI!" << std::endl;
+
       // Increase verboosity so we see all messages
-      setVerbosity(4);
-      // Initialize app
-      initApp();
-      // Create main window
-      createMainWindow();
+      ignition::common::Console::SetVerbosity(4);
+
+      // Create app
+      ignition::gui::Application app(_argc, _argv);
+
+      // Load plugins / config
+      if (!app.LoadPlugin("Publisher"))
+      {
+        return 1;
+      }
+
       // Customize main window
-      auto win = mainWindow();
-      win->setWindowTitle("Hello Window!");
-      // After window is closed
-      stop();
+      auto win = app.findChild<ignition::gui::MainWindow *>()->QuickWindow();
+      win->setProperty("title", "Hello Window!");
+
+      // Run window
+      // app.exec();
+
       std::cout << "After run" << std::endl;
 
       return 0;
     }
     EOS
-    ENV.append_path "PKG_CONFIG_PATH", "#{Formula["qt"].opt_lib}/pkgconfig"
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
+      find_package(ignition-gui1 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-gui1::ignition-gui1)
+    EOS
+    ENV.append_path "PKG_CONFIG_PATH", Formula["qt"].opt_lib/"pkgconfig"
+    ENV.append_path "PKG_CONFIG_PATH", Formula["tinyxml2@6.2.0"].opt_lib/"pkgconfig"
     system "pkg-config", "ignition-gui1"
     cflags   = `pkg-config --cflags ignition-gui1`.split(" ")
     ldflags  = `pkg-config --libs ignition-gui1`.split(" ")
@@ -72,5 +89,14 @@ class IgnitionGui1 < Formula
                    "-lc++",
                    "-o", "test"
     system "./test"
+    # test building with cmake
+    ENV.append_path "CMAKE_PREFIX_PATH", Formula["qt"].opt_prefix
+    mkdir "build" do
+      ENV.delete("MACOSX_DEPLOYMENT_TARGET")
+      ENV.delete("SDKROOT")
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
   end
 end
