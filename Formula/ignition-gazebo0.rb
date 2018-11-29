b850aae57ddf961e8d86d47f33a8a1fc0edfd61e
Steven Peters <scpeters@openrobotics.org>
2018-11-28 16:55:08 -0800

    ignition-gazebo0: new formula (#537)

---
+++ Formula/ignition-gazebo0.rb
@@ -0,0 +1,94 @@
+class IgnitionGazebo0 < Formula
+  desc "Gazebo robot simulator"
+  homepage "https://bitbucket.org/ignitionrobotics/ign-gazebo"
+  url "https://bitbucket.org/ignitionrobotics/ign-gazebo/get/67bf2a59036476d4ee1575095bd889006bae58ac.tar.gz"
+  version "0.1.0~pre1~1~67bf2a5"
+  sha256 "9a81a40e055e3eb5c10205d9725edd1a27df997696a675c02e44c1ae3676a331"
+
+  head "https://bitbucket.org/ignitionrobotics/ign-gazebo", :branch => "default", :using => :hg
+
+  depends_on "cmake" => :build
+  depends_on "gflags"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common3"
+  depends_on "ignition-gui1"
+  depends_on "ignition-math6"
+  depends_on "ignition-msgs3"
+  depends_on "ignition-physics1"
+  depends_on "ignition-plugin1"
+  depends_on "ignition-rendering1"
+  depends_on "ignition-transport6"
+  depends_on :macos => :mojave # c++17
+  depends_on "pkg-config"
+  depends_on "sdformat8"
+
+  def install
+    ENV.m64
+
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+    #include <ignition/gazebo/Entity.hh>
+    #include <ignition/gazebo/EntityComponentManager.hh>
+    #include <ignition/gazebo/components/World.hh>
+    #include <ignition/gazebo/components/Name.hh>
+
+    using namespace ignition;
+    using namespace gazebo;
+
+    //////////////////////////////////////////////////
+    int main(int argc, char **argv)
+    {
+      // Warm start. Initial allocation of resources can throw off calculations.
+      {
+        // Create the entity component manager
+        EntityComponentManager mgr;
+
+        // Create the matching entities
+        for (int i = 0; i < 100; ++i)
+        {
+          EntityId entity = mgr.CreateEntity();
+          mgr.CreateComponent(entity, components::World());
+          mgr.CreateComponent(entity, components::Name("world_name"));
+        }
+
+        mgr.Each<components::World, components::Name>(
+            [&](const EntityId &, const components::World *,
+              const components::Name *)->bool {return true;});
+      }
+
+      return 0;
+    }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
+      find_package(ignition-gazebo QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-gazebo::core)
+    EOS
+    ENV.append_path "PKG_CONFIG_PATH", Formula["qt"].opt_lib/"pkgconfig"
+    system "pkg-config", "ignition-gazebo"
+    cflags   = `pkg-config --cflags ignition-gazebo`.split(" ")
+    ldflags  = `pkg-config --libs ignition-gazebo`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   *ldflags,
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    ENV.append_path "CMAKE_PREFIX_PATH", Formula["qt"].opt_prefix
+    mkdir "build" do
+      ENV.delete("MACOSX_DEPLOYMENT_TARGET")
+      ENV.delete("SDKROOT")
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+  end
+end
