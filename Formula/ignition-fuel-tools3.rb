1df5fbebe7f1a6fe53703a2e04cad7e0ed1a1bfe
Steven Peters <scpeters@openrobotics.org>
2019-01-03 16:18:21 -0800

    add placeholder url, fix test

--- Formula/ignition-fuel-tools3.rb
+++ Formula/ignition-fuel-tools3.rb
@@ -1,14 +1,9 @@
 class IgnitionFuelTools3 < Formula
   desc "Tools for using fuel API to download robot models"
   homepage "https://ignitionrobotics.org"
-  url "http://gazebosim.org/distributions/ign-fuel-tools/releases/ignition-fuel-tools3-3.0.0~pre1.tar.bz2"
-  version "3.0.0~pre1"
-  sha256 "TODO"
-
-  bottle do
-    root_url "http://gazebosim.org/distributions/bottles-simulation"
-    sha256 "TODO" => :mojave
-  end
+  url "https://bitbucket.org/ignitionrobotics/ign-fuel-tools/get/a161b73abf2478f20f3e749224166856a08cbaa7.tar.gz"
+  version "2.999.999~pre1"
+  sha256 "bc8d36d83b0324ab17c2ec6715ddaaa104da7bff827b5cb333fcb991d6a8e623"
 
   depends_on "cmake"
   depends_on "ignition-cmake2"
@@ -35,19 +30,19 @@ class IgnitionFuelTools3 < Formula
     EOS
     (testpath/"CMakeLists.txt").write <<-EOS
       cmake_minimum_required(VERSION 2.8 FATAL_ERROR)
-      find_package(ignition-fuel_tools1 QUIET REQUIRED)
+      find_package(ignition-fuel_tools3 QUIET REQUIRED)
       include_directories(${IGNITION-FUEL_TOOLS_INCLUDE_DIRS})
       link_directories(${IGNITION-FUEL_TOOLS_LIBRARY_DIRS})
       add_executable(test_cmake test.cpp)
-      target_link_libraries(test_cmake ignition-fuel_tools1::ignition-fuel_tools1)
+      target_link_libraries(test_cmake ignition-fuel_tools3::ignition-fuel_tools3)
     EOS
     # test building with pkg-config
-    system "pkg-config", "ignition-fuel_tools1"
-    cflags = `pkg-config --cflags ignition-fuel_tools1`.split(" ")
+    system "pkg-config", "ignition-fuel_tools3"
+    cflags = `pkg-config --cflags ignition-fuel_tools3`.split(" ")
     system ENV.cc, "test.cpp",
                    *cflags,
                    "-L#{lib}",
-                   "-lignition-fuel_tools1",
+                   "-lignition-fuel_tools3",
                    "-lc++",
                    "-o", "test"
     system "./test"
