02fb35abc743c5679814dbcd6b970f17a6b90c6e
chapulina <louise@openrobotics.org>
2019-02-07 19:54:23 -0800

    sdformat8: update commit (#583)
    * Update commit: sdf8 and msgs3
    
    * move msgs to separate PR, bump sdf
    
    * : update  bottle.

--- Formula/sdformat8.rb
+++ Formula/sdformat8.rb
@@ -1,13 +1,13 @@
 class Sdformat8 < Formula
   desc "Simulation Description Format"
   homepage "http://sdformat.org"
-  url "https://bitbucket.org/osrf/sdformat/get/6ae595df61ad1327cebbbce.tar.gz"
-  version "8.0.0~pre4~1~6ae595d"
-  sha256 "839edf3e4cee565c9642e9ffc23f041356ae992fdfc7f828c2e5571d999f81a4"
+  url "https://bitbucket.org/osrf/sdformat/get/301cecc99c227bad0b783477a2c4f7004b99961e.tar.gz"
+  version "8.0.0~pre4~2~301cecc9"
+  sha256 "e89fc530de658de15252b931c7305f9e6fb61465a23f326ebd82acd0eebd9d58"
 
   bottle do
     root_url "http://gazebosim.org/distributions/bottles-simulation"
-    sha256 "3541343b94d9a0cc4e0efe5dd45bad1dcb2f724d942dc45482a272009752d7cb" => :mojave
+    sha256 "aacb076ffd8d91c2a3ea840661aa1c67e164b4145c4eea39d9dcb2cf5cbdd10b" => :mojave
   end
 
   depends_on "cmake" => :build
