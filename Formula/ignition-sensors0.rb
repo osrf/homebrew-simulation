55e63cfc035b8bd75411abfedd29699c31956d8b
chapulina <louise@openrobotics.org>
2019-02-13 10:06:38 -0800

    ignition-sensors: update commit (#589)
    * Update ign-sensors commit
    
    * ignition-sensors: update updated commit bottle.

--- Formula/ignition-sensors0.rb
+++ Formula/ignition-sensors0.rb
@@ -1,15 +1,15 @@
 class IgnitionSensors0 < Formula
   desc "Sensors library for robotics applications"
   homepage "https://bitbucket.org/ignitionrobotics/ign-sensors"
-  url "https://bitbucket.org/ignitionrobotics/ign-sensors/get/6f3b5f091187dfa6385ba1f1449c2472b7f6916b.tar.gz"
-  version "0.1.0~pre1~1~6f3b5f0"
-  sha256 "4ee68a62e6355fd4e7465449d67e9adfa11a1d793d2c62644453e6d0b96512d3"
+  url "https://bitbucket.org/ignitionrobotics/ign-sensors/get/c4a9947412e6015e8a7aa0791710da8bd9d5d84a.tar.gz"
+  version "0.1.0~pre2~1~c4a9947"
+  sha256 "2bc1a5229537765738d73b6f1f803fce0b56e74796a430f8bf3340448443c5a0"
 
   head "https://bitbucket.org/ignitionrobotics/ign-sensors", :branch => "default", :using => :hg
 
   bottle do
     root_url "http://gazebosim.org/distributions/bottles-simulation"
-    sha256 "f000b97a9c526ea2022fa9a7e9c9a2bc5313e0d1e5ddd0507e5ce4b595a9c8f4" => :mojave
+    sha256 "3066265228d314095860f483a21da3086596c3b55c2dba858092095a95ad724c" => :mojave
   end
 
   depends_on "cmake" => :build
