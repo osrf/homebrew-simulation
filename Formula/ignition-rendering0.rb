3ea9865324d186822683a986725bb0eebef31f54
osrfbuild <osrfbuild@osrfoundation.org>
2019-03-06 07:36:39 +0100

    ignition-rendering0 0.1.0 (#622)
    * ignition-rendering0: update 0.1.0
    
    * ignition-rendering0: update 0.1.0 bottle.

--- Formula/ignition-rendering0.rb
+++ Formula/ignition-rendering0.rb
@@ -1,18 +1,16 @@
 class IgnitionRendering0 < Formula
   desc "Rendering library for robotics applications"
   homepage "https://bitbucket.org/ignitionrobotics/ign-rendering"
-  url "https://bitbucket.org/ignitionrobotics/ign-rendering/get/5b69fe76e650c3d9c087edf50cee10bec0a22e05.tar.gz"
-  version "0.0.0~20180823~5b69fe7"
-  sha256 "7334dcffffa5674d7905dc6168d7c12fc82279f2f1736ef7ce86664071c43f5c"
-  revision 2
+  url "http://gazebosim.org/distributions/ign-rendering/releases/ignition-rendering0-0.1.0.tar.bz2"
+  sha256 "7d0d3e71b0e18f1ac1a8a0313747fc754cc301cfd12c249845b298c049f6ded3"
 
   head "https://bitbucket.org/ignitionrobotics/ign-rendering", :branch => "default", :using => :hg
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 "86f34785e2785fbfaa12f67e6b5486c16068848c82c5119495beb83d096e9d2e" => :mojave
-    sha256 "135c3ab87141bc8ffdf8fd1a56a4caff7602049a0454f7e5f99019282d4c9845" => :high_sierra
-    sha256 "ce2459a4b043a786c3a559f98436815c59e0b26886aedfbc1a91977406387913" => :sierra
+    sha256 "ad402c4978497b188cd95019d41da6ef90075f18d5e08434a8b441b339d7c7af" => :mojave
+    sha256 "001e9175fcdd530d02e38a1218abab777d08008b184d75fff70bcbb5b4edecac" => :high_sierra
+    sha256 "4325e4ccccc6ecd828228673073aefad5f787ec7a3e43c730e7d9fdf70bb429e" => :sierra
   end
 
   depends_on "cmake" => :build
