0def4367420f6befa3f8a85257e9ba4eff8cd1b9
Steven Peters <scpeters@openrobotics.org>
2019-03-24 17:45:21 -0700

    ignition-rendering1: better test for ogre plugin, bump revision (#651)
    * ignition-rendering1: test ogre plugin
    
    * ignition-rendering1: revision for boost
    
    * ignition-rendering1: update revision for boost bottle.

--- Formula/ignition-rendering1.rb
+++ Formula/ignition-rendering1.rb
@@ -3,11 +3,12 @@ class IgnitionRendering1 < Formula
   homepage "https://bitbucket.org/ignitionrobotics/ign-rendering"
   url "https://osrf-distributions.s3.amazonaws.com/ign-rendering/releases/ignition-rendering-1.0.0.tar.bz2"
   sha256 "73dc1ce94c281ddd21796ebf363e1cd03acf15709cda959926d3187462221bfe"
+  revision 1
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 "cafade1cb99232ab1e187b986b0489f17121a6879aff7fde762a7417207d4689" => :mojave
-    sha256 "3f065edd6db6be46c5225377f5a393e00e0877a8f1be6261aa218734aee92c05" => :high_sierra
+    sha256 "2cd474e3ff77d19e393f1f6c73a004e0ef0eb46c911de8b1bc576c7174d999d2" => :mojave
+    sha256 "6fa7646f34ba9b813d768f969f22cdec58d3d5a3a0a0a864e086bb41d20a8104" => :high_sierra
   end
 
   depends_on "cmake" => :build
@@ -28,14 +29,22 @@ class IgnitionRendering1 < Formula
 
   test do
     (testpath/"test.cpp").write <<-EOS
-      #include <ignition/rendering/PixelFormat.hh>
+      #include <ignition/rendering/RenderEngine.hh>
+      #include <ignition/rendering/RenderingIface.hh>
       int main(int _argc, char** _argv)
       {
-        ignition::rendering::PixelFormat pf = ignition::rendering::PF_UNKNOWN;
-        return ignition::rendering::PixelUtil::IsValid(pf);
+        ignition::rendering::RenderEngine *engine =
+            ignition::rendering::engine("ogre");
+        return engine == nullptr;
       }
     EOS
-    ENV.append_path "PKG_CONFIG_PATH", "#{Formula["qt"].opt_lib}/pkgconfig"
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.10.2 FATAL_ERROR)
+      find_package(ignition-rendering1 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-rendering1::ignition-rendering1)
+    EOS
+    # test building with pkg-config
     system "pkg-config", "ignition-rendering1"
     cflags   = `pkg-config --cflags ignition-rendering1`.split(" ")
     ldflags  = `pkg-config --libs ignition-rendering1`.split(" ")
@@ -45,5 +54,13 @@ class IgnitionRendering1 < Formula
                    "-lc++",
                    "-o", "test"
     system "./test"
+    # test building with cmake
+    mkdir "build" do
+      ENV.delete("MACOSX_DEPLOYMENT_TARGET")
+      ENV.delete("SDKROOT")
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
   end
 end
