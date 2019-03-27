aaf24a2ae49b1710571857471101c451bcc9f877
osrfbuild <osrfbuild@osrfoundation.org>
2019-03-27 06:48:45 +0100

    gazebo7 7.15.0 (#657)
    * gazebo7: update 7.15.0
    
    * remove old patches
    
    * gazebo7: update 7.15.0 bottle.

--- Formula/gazebo7.rb
+++ Formula/gazebo7.rb
@@ -1,17 +1,16 @@
 class Gazebo7 < Formula
   desc "Gazebo robot simulator"
   homepage "http://gazebosim.org"
-  url "https://osrf-distributions.s3.amazonaws.com/gazebo/releases/gazebo-7.14.0.tar.bz2"
-  sha256 "c1e48a2304d8186a3df4fb763262d189a8afe6b1ab82c30e614e336f3cb6034f"
-  revision 7
+  url "https://osrf-distributions.s3.amazonaws.com/gazebo/releases/gazebo-7.15.0.tar.bz2"
+  sha256 "a648d6850c91b94b1b744b37d5a0b1663942e8b5d0f80d800dbc873feac67889"
 
   head "https://bitbucket.org/osrf/gazebo", :branch => "gazebo7", :using => :hg
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 "8adac7ff6bf9d5e95e9444eac570c5b805b881233493aa99ed062117351a36a5" => :mojave
-    sha256 "09fd4865ab733362f198f7e04654814124e833ccc171655d427ac23895fe46b9" => :high_sierra
-    sha256 "3aed34518eeecffe4f13ecabdec064e52c3447fd225e70da0a1b712add465d0b" => :sierra
+    sha256 "18ae1b239ae65073510d71a070f3a914b5c5311f1c03bbca6ff1c22d7341a7db" => :mojave
+    sha256 "5b55937d8ba81504fac1f979cca733e4ff42fab2a1e4c0121167561d0a496b00" => :high_sierra
+    sha256 "2a9744fbeccc6e2f2ffaf508d6306c93452b7e5d54d6ad668dd9fde6e8211009" => :sierra
   end
 
   depends_on "cmake" => :build
@@ -47,24 +46,6 @@ class Gazebo7 < Formula
   conflicts_with "gazebo5", :because => "Differing version of the same formula"
   conflicts_with "gazebo6", :because => "Differing version of the same formula"
   conflicts_with "gazebo8", :because => "Differing version of the same formula"
-
-  patch do
-    # Fix for compatibility with boost 1.69
-    url "https://bitbucket.org/osrf/gazebo/commits/88f30af10b49c10ac5598640f40dccc0e3ba6e8a/raw/"
-    sha256 "8fb4d61ce97d1a3437e4e4ef76d61e9768fad72e555b4303c4c90a326e9346dd"
-  end
-
-  patch do
-    # Fix for compatibility with boost 1.69
-    url "https://bitbucket.org/osrf/gazebo/commits/d702208a1c5013dea3bfa986ae5a201ad4bc8cf3/raw/"
-    sha256 "f6aef9035c24d7e2a1457a2ca1c2b37485dcb3b970b58beca9c7b8c28109ffa4"
-  end
-
-  patch do
-    # Fix for compatibility with boost 1.68
-    url "https://bitbucket.org/osrf/gazebo/commits/cc53e4cdd34875dbb99048137f1d27541d12b3d0/raw/"
-    sha256 "f382a668ba2c6a318f3d4b5f616a11ad098973d941cd73a5493f0b1788ae8a42"
-  end
 
   patch do
     # Fix build when homebrew python is installed
