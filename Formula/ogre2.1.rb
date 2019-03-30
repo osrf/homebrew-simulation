ba3735c23bb6aff4267e3032d717644b5c916aea
Steven Peters <scpeters@openrobotics.org>
2019-03-30 00:43:41 -0700

    ogre2.1: new commit, fix build for xcode 10+ (#654)
    * ogre2.1: newer commit, patch for GL3+, XCode 10
    
    * ogre2.1: ensure samples aren't installed, new test
    
    * ogre2.1: update fix mojave bottle.

--- Formula/ogre2.1.rb
+++ Formula/ogre2.1.rb
@@ -1,22 +1,27 @@
 class Ogre21 < Formula
   desc "Scene-oriented 3D engine written in c++"
   homepage "https://www.ogre3d.org/"
-  url "https://bitbucket.org/sinbad/ogre/get/06a386fa64e79a7204a90faf53da1735743f6c2e.tar.bz2"
-  version "2.0.9999~20180616~06a386f"
-  sha256 "d2e28bfcfbb1277355047c1d8bcd141b05b83af52d277725168e4281eac92a6d"
+  url "https://bitbucket.org/sinbad/ogre/get/bf5de3029fd5a88d103ff99d7b2f94a5f8396ac9.tar.bz2"
+  version "2.0.9999~20190325~bf5de30"
+  sha256 "3949377daf5485847eeb78e420c3de4d8ed522bab8ac4c053c3deb4daa4bf4bd"
+
+  head "https://bitbucket.org/sinbad/ogre", :branch => "v2-1", :using => :hg
 
   bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/ogre/releases"
-    sha256 "088deeb420ba6d058962eca59a58967f54280b197669c5dc59fa3cb06071417a" => :high_sierra
-    sha256 "ade07317c6ca515204be1ef64b4d8c87e60732e6d426d100447587a611235c4c" => :sierra
-    sha256 "115c6af607d0844bc3d51f65933a0394dc23c40a5c3453a1c37abdb7e74b81cb" => :el_capitan
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    cellar :any
+    sha256 "8a2e7dfba343138df66831a7e81d61431da6e42c7eccbbc392535106ca77eb3c" => :mojave
+    sha256 "9f01ccd917196f78ad06198fa69fea2698ae4161bb7b61b4fa21b3b56d50e6ce" => :high_sierra
+    sha256 "ffde1dc5510e2c624f808cdf6f06e4d41f808a3db9234a9c8e2bfff3614e6597" => :sierra
   end
 
   depends_on "cmake" => :build
+  depends_on "pkg-config" => :test
   depends_on "doxygen"
   depends_on "freeimage"
   depends_on "freetype"
   depends_on "libzzip"
+  depends_on "rapidjson"
   depends_on "tbb"
   depends_on :x11
 
@@ -29,8 +34,11 @@ class Ogre21 < Formula
     sha256 "99d76e023cd5740da66c76ced40ce85e7da7b811ea99d9015d1293fc454badc0"
   end
 
-  # workaround for test since OgreMeshTool can't find plugins_tools.cfg otherwise
-  patch :DATA
+  patch do
+    # fix GL3+ compilation with Xcode 10
+    url "https://bitbucket.org/scpeters/ogre-1/commits/14b5dc7fc2d8e1281140d027e1effb4d8a317895/raw"
+    sha256 "41c678d3021feab844c5731c0cc2aa7007b731cfde5e084bc87d3a1eba9fa581"
+  end
 
   def install
     ENV.m64
@@ -41,8 +49,12 @@ class Ogre21 < Formula
       "-DOGRE_BUILD_DOCS:BOOL=FALSE",
       "-DOGRE_INSTALL_DOCS:BOOL=FALSE",
       "-DOGRE_BUILD_SAMPLES:BOOL=FALSE",
+      "-DOGRE_BUILD_SAMPLES2:BOOL=FALSE",
+      "-DOGRE_INSTALL_SAMPLES:BOOL=FALSE",
       "-DOGRE_INSTALL_SAMPLES_SOURCE:BOOL=FALSE",
     ]
+    # use the following to disable GL3Plus render engine which won't work when OpenGL is removed
+    # cmake_args << "-DOGRE_BUILD_RENDERSYSTEM_GL3PLUS:BOOL=OFF" if MacOS::Xcode.version >= "10"
     cmake_args.concat std_cmake_args
 
     mkdir "build" do
@@ -53,64 +65,27 @@ class Ogre21 < Formula
     # Put these cmake files where Debian puts them
     (share/"OGRE/cmake/modules").install Dir[prefix/"CMake/*.cmake"]
     rmdir prefix/"CMake"
-
-    # Put these cfg files in share instead of bin
-    (share/"OGRE/cfg").install Dir[prefix/"bin/*.cfg"]
   end
 
   test do
-    (testpath/"test.mesh.xml").write <<-EOS
-      <mesh>
-        <submeshes>
-          <submesh material="BaseWhite" usesharedvertices="false" use32bitindexes="false" operationtype="triangle_list">
-            <faces count="1">
-              <face v1="0" v2="1" v3="2" />
-            </faces>
-            <geometry vertexcount="3">
-              <vertexbuffer positions="true" normals="false" texture_coords="0">
-                <vertex>
-                  <position x="-50" y="-50" z="50" />
-                </vertex>
-                <vertex>
-                  <position x="-50" y="-50" z="-50" />
-                </vertex>
-                <vertex>
-                  <position x="50" y="-50" z="-50" />
-                </vertex>
-              </vertexbuffer>
-            </geometry>
-          </submesh>
-        </submeshes>
-        <submeshnames>
-          <submeshname name="submesh0" index="0" />
-        </submeshnames>
-      </mesh>
+    (testpath/"test.cpp").write <<-EOS
+      #include <Ogre.h>
+      int main()
+      {
+        Ogre::Root *root = new Ogre::Root("", "", "");
+        delete root;
+        return 0;
+      }
     EOS
-    system "#{bin}/OgreMeshTool", "#{testpath}/test.mesh.xml"
-    system "du", "-h", "#{testpath}/test.mesh"
+    system "pkg-config", "OGRE"
+    cflags = `pkg-config --cflags OGRE`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-std=c++11",
+                   "-L#{lib}",
+                   "-lOgreMain",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
   end
 end
-
-__END__
-diff -r d8213f4fb1db Tools/MeshTool/src/main.cpp
---- a/Tools/MeshTool/src/main.cpp       Thu Jun 14 19:05:19 2018 -0300
-+++ b/Tools/MeshTool/src/main.cpp       Mon Jun 18 16:43:28 2018 -0700
-@@ -1125,7 +1125,7 @@
-     SetCurrentDirectoryW( pathName.c_str() );
- #else
-     //This ought to work in Unix, but I didn't test it, otherwise try setenv()
--    //chdir( fullAppPath.GetPath().mb_str() );
-+    chdir( "/usr/local/share/OGRE/cfg" );
- #endif
-     //Most Ogre materials assume floating point to use radix point, not comma.
-     //Prevent awfull number truncation in non-US systems
-@@ -1139,7 +1139,7 @@
-     SetCurrentDirectoryW( gWorkingDir );
- #else
-     //This ought to work in Unix, but I didn't test it, otherwise try setenv()
--    //chdir( fullAppPath.GetPath().mb_str() );
-+    chdir( "/usr/local/share/OGRE/cfg" );
- #endif
- }
- 
-
