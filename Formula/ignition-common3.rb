453cf9a1ccec479b1ef942d740f51b0fbbde6692
Steven Peters <scpeters@openrobotics.org>
2019-04-18 18:02:48 -0700

    fix regular expression so it won't match itself

--- Formula/ignition-common3.rb
+++ Formula/ignition-common3.rb
@@ -74,7 +74,8 @@ class IgnitionCommon3 < Formula
     # check for Xcode frameworks in bottle
     # ! requires system with single argument, which uses standard shell
     # put in variable to avoid audit complaint
-    cmd_not_grep_xcode = "! grep -rnI Applications/Xcode #{prefix}"
+    # enclose / in [] so the following line won't match itself
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
     system cmd_not_grep_xcode
   end
 end
