1634509c861c57de362fad022cee218f957d7f80
osrfbuild <osrfbuild@osrfoundation.org>
2019-05-21 21:37:55 +0200

    ignition-tools 0.2.0 (#735)
    * ignition-tools: update 0.2.0
    
    * ignition-tools: update 0.2.0 bottle.

--- Formula/ignition-tools.rb
+++ Formula/ignition-tools.rb
@@ -1,15 +1,16 @@
 class IgnitionTools < Formula
   desc "Entry point for ignition command-line tools"
   homepage "https://ignitionrobotics.org"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-tools/releases/ignition-tools-0.2.0~pre1.tar.bz2"
-  version "0.2.0~pre1"
-  sha256 "ff18c8ed38e3c15a03344a3c54a67b2b74b3cd9115a3f84c79b12262ea4d51d4"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-tools/releases/ignition-tools-0.2.0.tar.bz2"
+  sha256 "13e74891ff72ed6775e00cf5592444632f88fa22bfd3ba510c347631039aa0f1"
   head "https://bitbucket.org/ignitionrobotics/ign-tools", :branch => "default", :using => :hg
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
     cellar :any_skip_relocation
-    sha256 "12810515ae12967113d0fa3c49db707eb815d6d9605c75a06ed7dea5dd4dd0f2" => :sierra
+    sha256 "07f3e030b5ed95a33db038465998ffb03eacf4d6c4d7d65678f6a40e9b26d3e2" => :mojave
+    sha256 "092a961c84d20a9a7e2d031b2222a037060543af217b0103e785adecc3eed4d5" => :high_sierra
+    sha256 "aa2dfff86d9ee0d04525d697df09cfd87ae02fefcf4b3c85835d6b98bbd3f5cc" => :sierra
   end
 
   depends_on "cmake" => :build
