a94822e07347edc7ac6863ea5b5cd1101673169c
osrfbuild <osrfbuild@osrfoundation.org>
2019-05-22 20:16:34 +0200

    ignition-sensors2 2.1.0~pre1 (#741)
    * ignition-sensors2: update 2.1.0~pre1
    
    * ignition-sensors2: update 2.1.0~pre1 bottle.

--- Formula/ignition-sensors2.rb
+++ Formula/ignition-sensors2.rb
@@ -1,14 +1,15 @@
 class IgnitionSensors2 < Formula
   desc "Sensors library for robotics applications"
   homepage "https://bitbucket.org/ignitionrobotics/ign-sensors"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-sensors/releases/ignition-sensors2-2.0.0.tar.bz2"
-  sha256 "620d4cc1ffbbe4b328b6dfab3ea779398dce5bab9f1918bbdf3f9fde3d3e8fde"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-sensors/releases/ignition-sensors2-2.1.0~pre1.tar.bz2"
+  version "2.1.0~pre1"
+  sha256 "3888d41268338fc0a455ee694a8b2c72aba737d5ccfa8565a9e3138c84b65752"
 
   head "https://bitbucket.org/ignitionrobotics/ign-sensors", :branch => "default", :using => :hg
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 "b2e81f5b77670382a6ba69eb9c2503ba7805ebeeff3a0859d541db44ed527a91" => :mojave
+    sha256 "de12d0308dd0b598d544fe5b83dfda4ab00c01bf1b79374b92373ae07a1ef694" => :mojave
   end
 
   depends_on "cmake" => [:build, :test]
