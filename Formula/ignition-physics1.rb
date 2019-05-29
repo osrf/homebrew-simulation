a99bbee0cbda591450552c02ffd226918cca1eef
osrfbuild <osrfbuild@osrfoundation.org>
2019-05-29 02:41:26 +0200

    ignition-physics 1.2.0~pre1 (#748)
    * ignition-physics: update 1.2.0~pre1
    
    * ignition-physics: update 1.2.0~pre1 bottle.

--- Formula/ignition-physics1.rb
+++ Formula/ignition-physics1.rb
@@ -1,13 +1,14 @@
 class IgnitionPhysics1 < Formula
   desc "Physics library for robotics applications"
   homepage "https://bitbucket.org/ignitionrobotics/ign-physics"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-physics/releases/ignition-physics-1.1.0.tar.bz2"
-  sha256 "ee555181e2b8a630b0a03572ade7f3c27c86abaa10bf5e59e31da9d37fd30a6a"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-physics/releases/ignition-physics-1.2.0~pre1.tar.bz2"
+  version "1.2.0~pre1"
+  sha256 "230049da06c5f367559b73e2d2a82b0e1a2c5215fa3fdf6f5bdc1b0e80141d68"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
     cellar :any
-    sha256 "9d74a9a28c535079b80c9ad78142ac458985999b1b63bf05dc4baaf81e89c9f2" => :mojave
+    sha256 "b69320e9667d79b51dcb617ca8fad751925209774667ff14478a32296200a2e5" => :mojave
   end
 
   depends_on "cmake" => :build
