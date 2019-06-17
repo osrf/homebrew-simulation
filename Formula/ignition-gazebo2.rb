bae6164c7fff698ae1f43d02b12949a14b73da73
osrfbuild <osrfbuild@osrfoundation.org>
2019-06-17 22:07:34 +0200

    ignition-gazebo2 2.1.0~pre2 (#761)
    * ignition-gazebo2: update 2.1.0~pre2
    
    * ignition-gazebo2: update 2.1.0~pre2 bottle.

--- Formula/ignition-gazebo2.rb
+++ Formula/ignition-gazebo2.rb
@@ -1,15 +1,15 @@
 class IgnitionGazebo2 < Formula
   desc "Gazebo robot simulator"
   homepage "https://bitbucket.org/ignitionrobotics/ign-gazebo"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-gazebo/releases/ignition-gazebo2-2.1.0~pre1.tar.bz2"
-  version "2.1.0~pre1"
-  sha256 "8f59081f37aa7730084173186a087960d4f7122d05256bbbd445c0108758fd17"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-gazebo/releases/ignition-gazebo2-2.1.0~pre2.tar.bz2"
+  version "2.1.0~pre2"
+  sha256 "afdc99da77f5dc16274b1996c8a74aacd357748dc50b28b5002251bad35f870b"
 
   head "https://bitbucket.org/ignitionrobotics/ign-gazebo", :branch => "default", :using => :hg
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 "6701884e995b121d761dcc9888f5093b22453f2ab1121fe59046381c90d6e478" => :mojave
+    sha256 "520f45674ffbdacdc4a49a3423d1b12de64cc07c9c2a7703d270b7b070435206" => :mojave
   end
 
   depends_on "cmake" => :build
