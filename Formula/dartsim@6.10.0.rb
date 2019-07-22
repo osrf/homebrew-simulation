cde33637f81a6ec56d3c04827077415ea6b0ff2a
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-07-22 18:55:58 +0200

    Add a new formula for dartsim 6.10.0

---
+++ Formula/dartsim@6.10.0.rb
@@ -0,0 +1,53 @@
+class Dartsim < Formula
+  desc "Dynamic Animation and Robotics Toolkit"
+  homepage "https://dartsim.github.io/"
+  # osrc custom nightly built from commit fdde7e7894ebc36bae8811f7a63e5b1c899bb4af
+  url "https://github.com/dartsim/dart/archive/fdde7e7894ebc36bae8811f7a63e5b1c899bb4af.zip"
+  sha256 "5aa9069ac4cada47d026f93a25fadd8461ce2de302b2a91699ab10a095205347"
+  revision 1
+
+  bottle do
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "assimp"
+  depends_on "boost"
+  depends_on "bullet"
+  depends_on "eigen"
+  depends_on "fcl"
+  depends_on "flann"
+  depends_on "ipopt"
+  depends_on "libccd"
+  depends_on "nlopt"
+  depends_on "ode"
+  depends_on "open-scene-graph"
+  depends_on "tinyxml2"
+  depends_on "urdfdom"
+
+  def install
+    ENV.cxx11
+
+    # Force to link to system GLUT (see: https://cmake.org/Bug/view.php?id=16045)
+    system "cmake", ".", "-DGLUT_glut_LIBRARY=/System/Library/Frameworks/GLUT.framework",
+                         *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <dart/dart.hpp>
+      int main() {
+        auto world = std::make_shared<dart::simulation::World>();
+        assert(world != nullptr);
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-I#{Formula["eigen"].include}/eigen3",
+                    "-I#{include}", "-L#{lib}", "-ldart",
+                    "-L#{Formula["assimp"].opt_lib}", "-lassimp",
+                    "-L#{Formula["boost"].opt_lib}", "-lboost_system",
+                    "-std=c++14", "-o", "test"
+    system "./test"
+  end
+end
