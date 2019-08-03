83ff05bac840ec56be548df764ccfc4cf22ab1a4
Louise Poubel <louise@openrobotics.org>
2019-08-02 17:30:22 -0700

    ignition-sensors3: depend on ignition-rendering3
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

---
+++ Formula/ignition-sensors3.rb
@@ -0,0 +1,70 @@
+class IgnitionSensors3 < Formula
+  desc "Sensors library for robotics applications"
+  homepage "https://bitbucket.org/ignitionrobotics/ign-sensors"
+  url "https://bitbucket.org/ignitionrobotics/ign-sensors/get/cd22b21d6130f6e9e236fb6bb3a641105cf1c236.tar.bz2"
+  version "2.999.999~20190802~cd22b21"
+  sha256 "6ed3398be82082a374bdc6d702b2e025530c65267a888f49d0da110d3c8380ae"
+
+  head "https://bitbucket.org/ignitionrobotics/ign-sensors", :branch => "default", :using => :hg
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 "fee9bfd612fbfe78e6befbd0bc7e3c42d6af56258212e2c43243f5e3c90e70c9" => :mojave
+  end
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "pkg-config" => [:build, :test]
+
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common3"
+  depends_on "ignition-math6"
+  depends_on "ignition-msgs4"
+  depends_on "ignition-rendering3"
+  depends_on "ignition-transport7"
+  depends_on "sdformat8"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <ignition/sensors.hh>
+
+      int main()
+      {
+        ignition::sensors::Noise noise(ignition::sensors::NoiseType::NONE);
+
+        return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.10.2 FATAL_ERROR)
+      find_package(ignition-sensors3 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-sensors3::ignition-sensors3)
+    EOS
+    # test building with pkg-config
+    system "pkg-config", "ignition-sensors3"
+    cflags   = `pkg-config --cflags ignition-sensors3`.split(" ")
+    ldflags  = `pkg-config --libs ignition-sensors3`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   *ldflags,
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    mkdir "build" do
+      ENV.delete("MACOSX_DEPLOYMENT_TARGET")
+      ENV.delete("SDKROOT")
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
