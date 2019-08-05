95fe426a122f08ec16c501c9abb99497476b5153
chapulina <louise@openrobotics.org>
2019-08-05 12:46:15 -0700

    ignition-gui3 depend on ignition-rendering3 (#802)
    * ignition-gui3 depend on ignition-rendering3
    
    Signed-off-by: Louise Poubel <louise@openrobotics.org>
    
    * ignition-gui3: update depend on ign-rendering3 bottle.

---
+++ Formula/ignition-gui3.rb
@@ -0,0 +1,111 @@
+class IgnitionGui3 < Formula
+  desc "Common libraries for robotics applications. GUI Library"
+  homepage "https://bitbucket.org/ignitionrobotics/ign-gui"
+  url "https://bitbucket.org/ignitionrobotics/ign-gui/get/4fc18bf44e8cf6575c33f0627f8b7496264df598.tar.bz2"
+  version "2.999.999~20190802~4fc18b"
+  sha256 "12876b21ac96c5627015c16397384e908a2d612d3b67de86feb122e2f23c7ee2"
+
+  head "https://bitbucket.org/ignitionrobotics/ign-gui", :branch => "default", :using => :hg
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 "1ea0d682577f8a96cce32c5a2625e7fad0c57f135145152a97adea3f53ffcb33" => :mojave
+    sha256 "b3ad60dd609556129e132887b10351488c48e5013321a64532b7934c5dfe1566" => :high_sierra
+  end
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common3"
+  depends_on "ignition-msgs4"
+  depends_on "ignition-plugin1"
+  depends_on "ignition-rendering3"
+  depends_on "ignition-transport7"
+  depends_on :macos => :high_sierra # c++17
+  depends_on "qt"
+  depends_on "qwt"
+  depends_on "tinyxml2"
+
+  def install
+    ENV.m64
+
+    cmake_args = std_cmake_args
+    cmake_args << "-DQWT_WIN_INCLUDE_DIR=#{HOMEBREW_PREFIX}/lib/qwt.framework/Headers"
+    cmake_args << "-DQWT_WIN_LIBRARY_DIR=#{HOMEBREW_PREFIX}/lib/qwt.framework"
+
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+    #include <iostream>
+
+    #ifndef Q_MOC_RUN
+      #include <ignition/gui/qt.h>
+      #include <ignition/gui/Application.hh>
+      #include <ignition/gui/MainWindow.hh>
+    #endif
+
+    //////////////////////////////////////////////////
+    int main(int _argc, char **_argv)
+    {
+      std::cout << "Hello, GUI!" << std::endl;
+
+      // Increase verboosity so we see all messages
+      ignition::common::Console::SetVerbosity(4);
+
+      // Create app
+      ignition::gui::Application app(_argc, _argv);
+
+      // Load plugins / config
+      if (!app.LoadPlugin("Publisher"))
+      {
+        return 1;
+      }
+
+      // Customize main window
+      auto win = app.findChild<ignition::gui::MainWindow *>()->QuickWindow();
+      win->setProperty("title", "Hello Window!");
+
+      // Run window
+      // app.exec();
+
+      std::cout << "After run" << std::endl;
+
+      return 0;
+    }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
+      find_package(ignition-gui3 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-gui3::ignition-gui3)
+    EOS
+    ENV.append_path "PKG_CONFIG_PATH", Formula["qt"].opt_lib/"pkgconfig"
+    system "pkg-config", "ignition-gui3"
+    cflags   = `pkg-config --cflags ignition-gui3`.split(" ")
+    ldflags  = `pkg-config --libs ignition-gui3`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   *ldflags,
+                   "-lc++",
+                   "-o", "test"
+    ENV["IGN_PARTITION"] = rand((1 << 32) - 1).to_s
+    system "./test"
+    # test building with cmake
+    ENV.append_path "CMAKE_PREFIX_PATH", Formula["qt"].opt_prefix
+    mkdir "build" do
+      ENV.delete("MACOSX_DEPLOYMENT_TARGET")
+      ENV.delete("SDKROOT")
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
