f193acfc3421841b9d3c2da2ca6ca8a04d48916a
chapulina <louise@openrobotics.org>
2019-08-05 10:43:57 -0700

    ign-rendering3 formula (#804)
    * ign-rendering3 formula
    
    Signed-off-by: Louise Poubel <louise@openrobotics.org>
    
    * add file
    
    Signed-off-by: Louise Poubel <louise@openrobotics.org>
    
    * fix test
    
    * ignition-rendering3: update 2.999.999 bottle.

---
+++ Formula/ignition-rendering3.rb
@@ -0,0 +1,72 @@
+class IgnitionRendering3 < Formula
+  desc "Rendering library for robotics applications"
+  homepage "https://bitbucket.org/ignitionrobotics/ign-rendering"
+  url "https://bitbucket.org/ignitionrobotics/ign-rendering/get/7af2213e442e4017b39172434012982b4bb64037.tar.bz2"
+  version "2.999.999~20190802~7af221"
+  sha256 "eb9ecf67bb1e41c01b1a5ced55db0cf459026f921184ea44fe9bab66a0f44bf6"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 "0d34aa5b2b59752028ba6aecd714da42d6f9d98cd182c90de024f083d4c814ed" => :mojave
+    sha256 "2a906dfff3e4dbe051974b6861e128d0f244d3ac9fbde9ce82caeff99db5b370" => :high_sierra
+  end
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "pkg-config" => [:build, :test]
+
+  depends_on "freeimage"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common3"
+  depends_on "ignition-math6"
+  depends_on "ignition-plugin1"
+  depends_on :macos => :high_sierra # c++17
+  depends_on "ogre1.9"
+  depends_on "ogre2.1"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    azure = !ENV["HOMEBREW_AZURE_PIPELINES"].nil?
+    travis = !ENV["HOMEBREW_TRAVIS_CI"].nil?
+    (testpath/"test.cpp").write <<-EOS
+      #include <ignition/rendering/RenderEngine.hh>
+      #include <ignition/rendering/RenderingIface.hh>
+      int main(int _argc, char** _argv)
+      {
+        ignition::rendering::RenderEngine *engine =
+            ignition::rendering::engine("ogre");
+        return engine == nullptr;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.10.2 FATAL_ERROR)
+      find_package(ignition-rendering3 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-rendering3::ignition-rendering3)
+    EOS
+    # test building with pkg-config
+    system "pkg-config", "ignition-rendering3"
+    cflags   = `pkg-config --cflags ignition-rendering3`.split(" ")
+    ldflags  = `pkg-config --libs ignition-rendering3`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   *ldflags,
+                   "-lc++",
+                   "-o", "test"
+    system "./test" unless azure || travis
+    # test building with cmake
+    mkdir "build" do
+      ENV.delete("MACOSX_DEPLOYMENT_TARGET")
+      ENV.delete("SDKROOT")
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake" unless azure || travis
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
