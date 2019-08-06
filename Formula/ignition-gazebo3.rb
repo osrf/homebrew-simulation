f303dd8cc3bcf11c00eeb658a102a36c39642aa1
chapulina <louise@openrobotics.org>
2019-08-06 11:39:33 -0700

    ignition-gazebo3: depend on rendering3, sensors3 and gui3 (#803)
    * ignition-gazebo3: depend rendering3 sensors3 gui3
    
    Signed-off-by: Louise Poubel <louise@openrobotics.org>
    
    * ignition-gazebo3: update depend on ign-rendering3 bottle.

---
+++ Formula/ignition-gazebo3.rb
@@ -0,0 +1,109 @@
+class IgnitionGazebo3 < Formula
+  desc "Ignition Gazebo robot simulator"
+  homepage "https://bitbucket.org/ignitionrobotics/ign-gazebo"
+  url "https://bitbucket.org/ignitionrobotics/ign-gazebo/get/cc18930145e52014e7ea18352dcbc13c55dd2b12.tar.bz2"
+  version "2.999.999~20190802~cc18930"
+  sha256 "45dd25a9634ff6fe2cd267f7f60746f5b4deebbf98d67c4b447eed324424c0b4"
+
+  head "https://bitbucket.org/ignitionrobotics/ign-gazebo", :branch => "default", :using => :hg
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 "02debb32aa7c5ba244c1d946c372dd3b1f344aa8fb179c9a2b987114da3d58e9" => :mojave
+  end
+
+  depends_on "cmake" => :build
+  depends_on "gflags"
+  depends_on "google-benchmark"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common3"
+  depends_on "ignition-fuel-tools3"
+  depends_on "ignition-gui3"
+  depends_on "ignition-math6"
+  depends_on "ignition-msgs4"
+  depends_on "ignition-physics1"
+  depends_on "ignition-plugin1"
+  depends_on "ignition-rendering3"
+  depends_on "ignition-sensors3"
+  depends_on "ignition-transport7"
+  depends_on :macos => :mojave # c++17
+  depends_on "pkg-config"
+  depends_on "sdformat8"
+
+  conflicts_with "ignition-gazebo1", :because => "Both install bin/ign-gazebo symlinks"
+  conflicts_with "ignition-gazebo2", :because => "Both install bin/ign-gazebo symlinks"
+
+  def install
+    ENV.m64
+
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+    #include <cstdint>
+    #include <ignition/gazebo/Entity.hh>
+    #include <ignition/gazebo/EntityComponentManager.hh>
+    #include <ignition/gazebo/components/World.hh>
+    #include <ignition/gazebo/components/Name.hh>
+
+    using namespace ignition;
+    using namespace gazebo;
+
+    //////////////////////////////////////////////////
+    int main(int argc, char **argv)
+    {
+      // Warm start. Initial allocation of resources can throw off calculations.
+      {
+        // Create the entity component manager
+        EntityComponentManager mgr;
+
+        // Create the matching entities
+        for (int i = 0; i < 100; ++i)
+        {
+          Entity entity = mgr.CreateEntity();
+          mgr.CreateComponent(entity, components::World());
+          mgr.CreateComponent(entity, components::Name("world_name"));
+        }
+
+        mgr.Each<components::World, components::Name>(
+            [&](const Entity &, const components::World *,
+              const components::Name *)->bool {return true;});
+      }
+
+      return 0;
+    }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
+      find_package(ignition-gazebo3 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-gazebo3::core)
+    EOS
+    ENV.append_path "PKG_CONFIG_PATH", Formula["qt"].opt_lib/"pkgconfig"
+    system "pkg-config", "ignition-gazebo3"
+    cflags   = `pkg-config --cflags ignition-gazebo3`.split(" ")
+    ldflags  = `pkg-config --libs ignition-gazebo3`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   *ldflags,
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    ENV.append_path "CMAKE_PREFIX_PATH", Formula["qt"].opt_prefix
+    mkdir "build" do
+      ENV.delete("MACOSX_DEPLOYMENT_TARGET")
+      ENV.delete("SDKROOT")
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
