925c8b4c57788008b2b26e75002153ad6783675f
Steven Peters <scpeters@openrobotics.org>
2019-08-09 17:01:31 -0700

    sdformat7: update commit and use ignition-tools (#666)
    * sdformat7: update commit and use ignition-tools
    
    * sdformat7: update depend on ign-tools bottle.

--- Formula/sdformat7.rb
+++ Formula/sdformat7.rb
@@ -1,18 +1,17 @@
 class Sdformat7 < Formula
   desc "Simulation Description Format"
   homepage "http://sdformat.org"
-  url "https://bitbucket.org/osrf/sdformat/get/2fed80e6bc44.tar.gz"
-  version "6.999.999~20180905~2fed80e"
-  sha256 "b9792f701be807a9f522b9b9b09c521340744ba1bd593d533765b1dc7bb40bb5"
-  revision 2
+  url "https://bitbucket.org/osrf/sdformat/get/f6b1f8e573d2af8abc40f63f3deb363de2472280.tar.gz"
+  version "6.999.999~20190808~f6b1f8e"
+  sha256 "cc58b11d5f7af8db0f293bef7cffdf86609ea233806ccfe065caeb38776ec954"
 
-  head "https://bitbucket.org/osrf/sdformat", :branch => "default", :using => :hg
+  head "https://bitbucket.org/osrf/sdformat", :branch => "sdf7", :using => :hg
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 "ba0798dc96e5c7044c4a9c1e3de7558fa141c3442d185307fb58292ca374b908" => :mojave
-    sha256 "4b2b10f850c00393d2772964fd895856ad7f8edcbd57747f69c95927be31fb78" => :high_sierra
-    sha256 "c6bc10aeeda21616645773d176188157f408561033e46f6ad4d20cd23c80da92" => :sierra
+    sha256 "88c59fe865c964a20675dbcbb1837ff2294dfaa5eba8f14eeb8be319eecd4e81" => :mojave
+    sha256 "b8f2b76bad1c9e70b6485b6303ddb0b03883465895174dd45e6aa7b8352e2560" => :high_sierra
+    sha256 "7196965b2e2771a43d71217372194836e41cbf9768bbbc4eed32de09a91f9135" => :sierra
   end
 
   depends_on "cmake" => :build
@@ -20,6 +19,7 @@ class Sdformat7 < Formula
   depends_on "boost"
   depends_on "doxygen"
   depends_on "ignition-math5"
+  depends_on "ignition-tools"
   depends_on "pkg-config"
   depends_on "tinyxml"
   depends_on "urdfdom" => :optional
