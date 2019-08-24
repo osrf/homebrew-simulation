42e5a2786ee083a38361a4ebe945e48bc74b061e
Steven Peters <scpeters@openrobotics.org>
2019-08-24 01:34:24 -0700

    ignition-msgs3: revision bump

--- Formula/ignition-msgs3.rb
+++ Formula/ignition-msgs3.rb
@@ -3,6 +3,7 @@ class IgnitionMsgs3 < Formula
   homepage "https://bitbucket.org/ignitionrobotics/ign-msgs"
   url "https://osrf-distributions.s3.amazonaws.com/ign-msgs/releases/ignition-msgs3-3.2.0.tar.bz2"
   sha256 "5754ee472e83dc0e46111f66123dbc8b635570603f4bcbaf0f71be0efaff1d11"
+  revision 1
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
