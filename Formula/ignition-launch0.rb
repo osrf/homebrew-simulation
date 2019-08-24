54949094b681f31b4474e80d12abc0eb2676620f
Steven Peters <scpeters@openrobotics.org>
2019-08-24 02:37:12 -0700

    ignition-launch0: revision bump

--- Formula/ignition-launch0.rb
+++ Formula/ignition-launch0.rb
@@ -3,6 +3,7 @@ class IgnitionLaunch0 < Formula
   homepage "https://bitbucket.org/ignitionrobotics/ign-launch"
   url "https://osrf-distributions.s3.amazonaws.com/ign-launch/releases/ignition-launch-0.2.0.tar.bz2"
   sha256 "5becd254b5e47668e41b2da676bae16c119a856ed2ea0a0de661a555fcd5f685"
+  revision 1
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
