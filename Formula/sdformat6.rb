02c030ab8f6ad693df983335111b64f5089cd3b0
Steven Peters <scpeters@openrobotics.org>
2019-09-06 11:43:12 -0700

    sdformat6: depend on ignition-tools (#810)

--- Formula/sdformat6.rb
+++ Formula/sdformat6.rb
@@ -19,6 +19,7 @@ class Sdformat6 < Formula
   depends_on "boost"
   depends_on "doxygen"
   depends_on "ignition-math4"
+  depends_on "ignition-tools"
   depends_on "pkg-config"
   depends_on "tinyxml"
   depends_on "urdfdom" => :optional
