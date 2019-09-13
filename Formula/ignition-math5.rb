abd3c03a8db9fe81ca6eb9b2d851b64badc38cc1
osrfbuild <osrfbuild@osrfoundation.org>
2019-09-13 02:18:44 +0200

    ignition-math5 5.1.0 (#841)
    * ignition-math5: update 5.1.0
    
    * ignition-math5: update 5.1.0 bottle.

--- Formula/ignition-math5.rb
+++ Formula/ignition-math5.rb
@@ -1,8 +1,8 @@
 class IgnitionMath5 < Formula
   desc "Math API for robotic applications"
   homepage "https://ignitionrobotics.org"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-math/releases/ignition-math5-5.0.0.tar.bz2"
-  sha256 "2536f20aebfa617dbf4a552638b25b084774f6b65f92d9ed3c304af5e45e3db2"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-math/releases/ignition-math5-5.1.0.tar.bz2"
+  sha256 "f28aa11f8f292e70cc8a0427452fa4548390408ec9b6df88d5b221dde0d42d1c"
   version_scheme 1
 
   head "https://bitbucket.org/ignitionrobotics/ign-math", :branch => "default", :using => :hg
@@ -10,9 +10,9 @@ class IgnitionMath5 < Formula
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
     cellar :any
-    sha256 "1fa8dfc5c6453d869131f3ff103909ca8364fde2a1ddbd4e1b1c5a2a5bb8a52e" => :mojave
-    sha256 "e064841eb6fcf150e84fc7ceb8de27cbc404816d9031df7a658f7535da8da011" => :high_sierra
-    sha256 "504f37f5ff7ad64e31cf1538a76c7e689ede271c6eddee0a6a779ee581e62875" => :sierra
+    sha256 "08a8d27d961fba37b980e3ecdd87d7416429807402ddaf38d3d616808d578371" => :mojave
+    sha256 "43dc2a329a12fed49066bc08d0fd788ffa346b936c7d683f8b936e86b3404b88" => :high_sierra
+    sha256 "606c4dfb580402e4cfb7ac1a5d7ccd99a7eeded41c974df618e067c1e51fd423" => :sierra
   end
 
   depends_on "cmake" => :build
