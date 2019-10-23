c1082fad7d40326f80288bff94a76e978029f54d
Steven Peters <scpeters@openrobotics.org>
2019-10-23 10:52:06 -0700

    fix audit, remove bottle block

--- Formula/ignition-launch2.rb
+++ Formula/ignition-launch2.rb
@@ -2,15 +2,10 @@ class IgnitionLaunch2 < Formula
   desc "Launch libraries for robotics applications"
   homepage "https://bitbucket.org/ignitionrobotics/ign-launch"
   url "https://bitbucket.org/ignitionrobotics/ign-launch/get/444674a894fe14ba86c99a4198fde68c5a8669e6.tar.bz2"
-  sha256 "00b5a1120ea86f32780fd087a5829151b491a06b093ed0eddcc4c94d5056c9ad"
   version "1.999.999~20190802~444674"
+  sha256 "00b5a1120ea86f32780fd087a5829151b491a06b093ed0eddcc4c94d5056c9ad"
 
   head "https://bitbucket.org/ignitionrobotics/ign-launch", :branch => "default", :using => :hg
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 "5404eef495cc29930be9555e116288f0444d86184c62596f6a27afa6e6040eb4" => :mojave
-  end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
