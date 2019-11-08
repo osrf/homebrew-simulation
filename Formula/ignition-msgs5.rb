264f675b6300bb6c353ad8098bd9b6971f80a171
Steven Peters <scpeters@openrobotics.org>
2019-11-08 02:06:16 -0800

    ignition-msgs5: new formula (#866)
    * ignition-msgs5: new formula
    
    * ignition-msgs5: update initial version bottle.

---
+++ Formula/ignition-msgs5.rb
@@ -0,0 +1,69 @@
+class IgnitionMsgs5 < Formula
+  desc "Middleware protobuf messages for robotics"
+  homepage "https://bitbucket.org/ignitionrobotics/ign-msgs"
+  url "https://bitbucket.org/ignitionrobotics/ign-msgs/get/e4fa43cfd3748499bbbfe989098622ea31614c89.tar.gz"
+  version "4.999.999~1~20191105~e4fa43cf"
+  sha256 "717acfe9808b67e267d6bd518ebda5d57f1b17325acd0bf1e0f2fd82eabc9dee"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    cellar :any
+    sha256 "166ed025b7cb85acd3f25d15dbbf0382d0391ccaa5215c5630c716f2a0b30255" => :mojave
+    sha256 "160524b6618617be18cc59cfebbf5e2a8132b4c127469e697ae1b616fcc30551" => :high_sierra
+  end
+
+  depends_on "protobuf-c" => :build
+
+  depends_on "cmake"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-math6"
+  depends_on "ignition-tools"
+  depends_on :macos => :high_sierra # c++17
+  depends_on "pkg-config"
+  depends_on "protobuf"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <ignition/msgs.hh>
+      int main() {
+        ignition::msgs::UInt32;
+        return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.10 FATAL_ERROR)
+      find_package(ignition-msgs5 QUIET REQUIRED)
+      set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${IGNITION-MSGS_CXX_FLAGS}")
+      include_directories(${IGNITION-MSGS_INCLUDE_DIRS})
+      link_directories(${IGNITION-MSGS_LIBRARY_DIRS})
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-msgs5::ignition-msgs5)
+    EOS
+    # test building with pkg-config
+    system "pkg-config", "ignition-msgs5"
+    cflags = `pkg-config --cflags ignition-msgs5`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lignition-msgs5",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    mkdir "build" do
+      ENV.delete("MACOSX_DEPLOYMENT_TARGET")
+      ENV.delete("SDKROOT")
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
