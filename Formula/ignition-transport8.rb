628c8f49594b3d4e69b23e91ede16659458e7ebb
Louise Poubel <louise@openrobotics.org>
2019-11-11 17:50:59 -0800

    ignition-transport8: depend on ign-msgs5
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

---
+++ Formula/ignition-transport8.rb
@@ -0,0 +1,66 @@
+class IgnitionTransport8 < Formula
+  desc "Transport middleware for robotics"
+  homepage "https://ignitionrobotics.org"
+  url "https://bitbucket.org/ignitionrobotics/ign-transport/get/1a7d53eb2b44e220fe281cd86d77860fbbf1d7cc.tar.bz2"
+  version "7.999.999~20191111~1a7d53"
+  sha256 "0c9aba36e0e583ebb99c9c1c19aa959f357ba2dcde93489088b52605dad61450"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 "d3b9bfccece5dd53793edbe9ac040fe64e916c08fecee3c60f2d2a688895ecde" => :mojave
+  end
+
+  depends_on "doxygen" => [:build, :optional]
+  depends_on "protobuf-c" => :build
+
+  depends_on "cmake"
+  depends_on "cppzmq"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-msgs5"
+  depends_on "ignition-tools"
+  depends_on :macos => :high_sierra # c++17
+  depends_on "ossp-uuid"
+  depends_on "pkg-config"
+  depends_on "protobuf"
+  depends_on "zeromq"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <iostream>
+      #include <ignition/transport.hh>
+      int main() {
+        ignition::transport::NodeOptions options;
+        return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.10 FATAL_ERROR)
+      find_package(ignition-transport8 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-transport8::ignition-transport8)
+    EOS
+    system "pkg-config", "ignition-transport8"
+    cflags = `pkg-config --cflags ignition-transport8`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lignition-transport8",
+                   "-lc++",
+                   "-o", "test"
+    ENV["IGN_PARTITION"] = rand((1 << 32) - 1).to_s
+    system "./test"
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
