6ee03b4891bc8811cc6cec78f77d1d76721fca1d
Steven Peters <scpeters@openrobotics.org>
2019-11-15 23:51:15 -0800

    sdformat9: initial version

---
+++ Formula/sdformat9.rb
@@ -0,0 +1,75 @@
+class Sdformat9 < Formula
+  desc "Simulation Description Format"
+  homepage "http://sdformat.org"
+  url "https://bitbucket.org/osrf/sdformat/get/608453d771bcf5cd51ffcd17cf0224d142fc4c26.tar.bz2"
+  version "8.999.999~20191115~1~608453d"
+  sha256 "04dabd45896f0425b6deb7829f0f4e2c0d2509fe5f1b9e8152e9c3fc33d802a2"
+
+  depends_on "cmake" => :build
+
+  depends_on "doxygen"
+  depends_on "ignition-math6"
+  depends_on :macos => :mojave # c++17
+  depends_on "pkg-config"
+  depends_on "tinyxml"
+  depends_on "urdfdom" => :optional
+
+  def install
+    ENV.m64
+
+    cmake_args = std_cmake_args
+    cmake_args << "-DUSE_EXTERNAL_URDF:BOOL=True" if build.with? "urdfdom"
+    cmake_args << ".."
+
+    mkdir "build" do
+      system "cmake", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <iostream>
+      #include "sdf/sdf.hh"
+      const std::string sdfString(
+        "<sdf version='1.5'>"
+        "  <model name='example'>"
+        "    <link name='link'>"
+        "      <sensor type='gps' name='mysensor' />"
+        "    </link>"
+        "  </model>"
+        "</sdf>");
+      int main() {
+        sdf::SDF modelSDF;
+        modelSDF.SetFromString(sdfString);
+        std::cout << modelSDF.ToString() << std::endl;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
+      find_package(sdformat9 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ${SDFormat_LIBRARIES})
+    EOS
+    system "pkg-config", "sdformat9"
+    cflags = `pkg-config --cflags sdformat9`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lsdformat9",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    mkdir "build" do
+      ENV.delete("MACOSX_DEPLOYMENT_TARGET")
+      ENV.delete("SDKROOT")
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
