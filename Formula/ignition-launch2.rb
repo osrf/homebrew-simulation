62a5ea23683854ab21e06aa4425a742e63700a77
chapulina <louise@openrobotics.org>
2019-11-17 18:04:02 -0800

    ignition-launch2: depend on msgs5 and transport8 (#875)
    * ignition-launch2: depend on msgs5 and transport8
    
    Signed-off-by: Louise Poubel <louise@openrobotics.org>
    
    * ignition-launch2: update use msgs5/transport8 bottle.

--- Formula/ignition-launch2.rb
+++ Formula/ignition-launch2.rb
@@ -1,11 +1,16 @@
 class IgnitionLaunch2 < Formula
   desc "Launch libraries for robotics applications"
   homepage "https://bitbucket.org/ignitionrobotics/ign-launch"
-  url "https://bitbucket.org/ignitionrobotics/ign-launch/get/444674a894fe14ba86c99a4198fde68c5a8669e6.tar.bz2"
-  version "1.999.999~20190802~444674"
-  sha256 "00b5a1120ea86f32780fd087a5829151b491a06b093ed0eddcc4c94d5056c9ad"
+  url "https://bitbucket.org/ignitionrobotics/ign-launch/get/eab55552d2985fb15c95a2fb88b604e23d2c382b.tar.bz2"
+  version "1.999.999~20191114~eab555"
+  sha256 "a41deaca6e97b40c3590c01b14d381d95861992daca573293be72e49b6b327f4"
 
   head "https://bitbucket.org/ignitionrobotics/ign-launch", :branch => "default", :using => :hg
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 "e1ccec118661305b1c828126ce3c828fe58f33542537abd3e6774d82af4cd9ff" => :mojave
+  end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
@@ -14,10 +19,10 @@ class IgnitionLaunch2 < Formula
   depends_on "ignition-common3"
   depends_on "ignition-gazebo3"
   depends_on "ignition-gui3"
-  depends_on "ignition-msgs4"
+  depends_on "ignition-msgs5"
   depends_on "ignition-plugin1"
   depends_on "ignition-tools"
-  depends_on "ignition-transport7"
+  depends_on "ignition-transport8"
   depends_on "qt"
   depends_on "sdformat8"
   depends_on "tinyxml2"
