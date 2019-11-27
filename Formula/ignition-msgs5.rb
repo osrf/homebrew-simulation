d03bc0cce236d669ec0481c1c2841217f15dfe27
osrfbuild <osrfbuild@osrfoundation.org>
2019-11-27 21:36:38 +0100

    ignition-msgs5 5.0.0~pre1 (#890)
    * ignition-msgs5: update 5.0.0~pre1
    
    * ignition-msgs5: update 5.0.0~pre1 bottle.

--- Formula/ignition-msgs5.rb
+++ Formula/ignition-msgs5.rb
@@ -1,15 +1,15 @@
 class IgnitionMsgs5 < Formula
   desc "Middleware protobuf messages for robotics"
   homepage "https://bitbucket.org/ignitionrobotics/ign-msgs"
-  url "https://bitbucket.org/ignitionrobotics/ign-msgs/get/e4fa43cfd3748499bbbfe989098622ea31614c89.tar.gz"
-  version "4.999.999~1~20191105~e4fa43cf"
-  sha256 "717acfe9808b67e267d6bd518ebda5d57f1b17325acd0bf1e0f2fd82eabc9dee"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-msgs/releases/ignition-msgs5-5.0.0~pre1.tar.bz2"
+  version "5.0.0~pre1"
+  sha256 "2c7608275a096f9fce01f88aaf5c72dd50516722fbced09631252cd8c7bafbbc"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
     cellar :any
-    sha256 "166ed025b7cb85acd3f25d15dbbf0382d0391ccaa5215c5630c716f2a0b30255" => :mojave
-    sha256 "160524b6618617be18cc59cfebbf5e2a8132b4c127469e697ae1b616fcc30551" => :high_sierra
+    sha256 "0db3c96b12d99e5f26c6667340bd43754f5a14a77af4c3e1f612362da5a2c13e" => :mojave
+    sha256 "89dea99a8a3956d5572387d89a0cec44a9a11e1941708dd8e1f220c3f64a7f26" => :high_sierra
   end
 
   depends_on "protobuf-c" => :build
