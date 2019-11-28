c9d36f12ed4575eecb5899f5b6f117edea8bbe53
Steven Peters <scpeters@openrobotics.org>
2019-11-28 14:16:56 -0800

    ignition-blueprint: revision bump for protobuf 3.11

--- Formula/ignition-blueprint.rb
+++ Formula/ignition-blueprint.rb
@@ -3,6 +3,7 @@ class IgnitionBlueprint < Formula
   homepage "https://bitbucket.org/ignitionrobotics/ign-blueprint"
   url "https://osrf-distributions.s3.amazonaws.com/ign-blueprint/releases/ignition-blueprint-1.0.0.tar.bz2"
   sha256 "a55860fa37bfb0c357ca86aaa31cd5de42e5f8f9022bced3e827808785e83041"
+  revision 1
 
   head "https://bitbucket.org/ignitionrobotics/ign-blueprint", :branch => "default", :using => :hg
 
