ad6922fc6d2de3c7c0d80f8d5139a6bfb5d0e827
osrfbuild <osrfbuild@osrfoundation.org>
2019-11-29 05:15:44 +0100

    ignition-launch 1.4.0 (#888)
    * ignition-launch: update 1.4.0
    
    * ignition-launch: update 1.4.0 bottle.

--- Formula/ignition-launch1.rb
+++ Formula/ignition-launch1.rb
@@ -1,12 +1,12 @@
 class IgnitionLaunch1 < Formula
   desc "Launch libraries for robotics applications"
   homepage "https://bitbucket.org/ignitionrobotics/ign-launch"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-launch/releases/ignition-launch-1.3.0.tar.bz2"
-  sha256 "d7f70538695822ae288a16db2d290b86724a8b2e4f1f7f126addee6c7c166a4f"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-launch/releases/ignition-launch-1.4.0.tar.bz2"
+  sha256 "324aee0b8a961ed08b07e71afbab875dc3be31d96785dcbd4fe9c2e6760bcb3f"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 "f2e00de4b48e22d985fb8dcfcd28cd77d809334841d7de6ffa5a239408894e33" => :mojave
+    sha256 "ff73acba60d88389256236ee66f239fb44313cb87a598dfa3eae259abbbd0d8a" => :mojave
   end
 
   depends_on "cmake" => :build
