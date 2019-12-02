4058ade17dcf7844d013e195527b3753dfc08d95
Nate Koenig <natekoenig@gmail.com>
2019-12-02 15:23:43 -0800

    ignition-fuel-tools4 initial version

---
+++ Formula/ignition-fuel-tools4.rb
@@ -0,0 +1,70 @@
+class IgnitionFuelTools4 < Formula
+  desc "Tools for using fuel API to download robot models"
+  homepage "https://ignitionrobotics.org"
+  url "https://bitbucket.org/ignitionrobotics/ign-fuel-tools/get/c6bdc21470d0cbd717e9b299fe9ea9558ef91690.tar.bz2"
+  version "3.999.999~20191202~2c696f6"
+  sha256 "2c696f656aefb46165cb49010061f1bd45f949d0ba104e56a44b2f828e4cac9a"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    cellar :any
+    sha256 "619cdf54925e7dcd0a469ea0dcd3dc58e13086dfd39f08d696f621372819d5c9" => :mojave
+    sha256 "f8999baf0b43b03a0d8714b4db91fc8fb4df2e88197cfd63d4765a4b83a78d32" => :high_sierra
+  end
+
+  depends_on "cmake"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common3"
+  depends_on "ignition-msgs5"
+  depends_on "jsoncpp"
+  depends_on "libyaml"
+  depends_on "libzip"
+  depends_on :macos => :high_sierra # c++17
+  depends_on "pkg-config"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <ignition/fuel_tools.hh>
+      int main() {
+        ignition::fuel_tools::ServerConfig srv;
+        return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 2.8 FATAL_ERROR)
+      find_package(ignition-fuel_tools4 QUIET REQUIRED)
+      include_directories(${IGNITION-FUEL_TOOLS_INCLUDE_DIRS})
+      link_directories(${IGNITION-FUEL_TOOLS_LIBRARY_DIRS})
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-fuel_tools4::ignition-fuel_tools4)
+    EOS
+    # test building with pkg-config
+    system "pkg-config", "ignition-fuel_tools4"
+    cflags = `pkg-config --cflags ignition-fuel_tools4`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lignition-fuel_tools4",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    mkdir "build" do
+      ENV.delete("MACOSX_DEPLOYMENT_TARGET")
+      ENV.delete("SDKROOT")
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
