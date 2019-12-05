7bc581eddfd050095d0c6722aa08c70f3720e940
osrfbuild <osrfbuild@osrfoundation.org>
2019-12-05 19:44:21 +0100

    sdformat8 8.6.1 (#913)
    * sdformat8: update 8.6.1
    
    * sdformat8: update 8.6.1 bottle.

--- Formula/sdformat8.rb
+++ Formula/sdformat8.rb
@@ -1,12 +1,12 @@
 class Sdformat8 < Formula
   desc "Simulation Description Format"
   homepage "http://sdformat.org"
-  url "https://osrf-distributions.s3.amazonaws.com/sdformat/releases/sdformat-8.6.0.tar.bz2"
-  sha256 "454574dd3257d77c27c5c12a9c67729ef10a45dc46e54192a84e6129564982a3"
+  url "https://osrf-distributions.s3.amazonaws.com/sdformat/releases/sdformat-8.6.1.tar.bz2"
+  sha256 "34b6ef98896f01e6da87b3523574af9470c8069f3d5883cb5754983f9eb92625"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 "067de90b1d145920925a0e84c78722c95f7621efea2b4e6d3ddba020fc8ac2a8" => :mojave
+    sha256 "b7c9b0960d11bcd7b5c770dadcb8cc548d4961dfe6de6e6fe5ccd2f8236dc909" => :mojave
   end
 
   depends_on "cmake" => :build
