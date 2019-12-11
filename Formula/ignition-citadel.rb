dd54e3f1e8fbadd1c239921cd1593bf96c717c54
Steven Peters <scpeters@openrobotics.org>
2019-12-11 05:26:50 -0800

    ignition-citadel: fix version, bump version_scheme

--- Formula/ignition-citadel.rb
+++ Formula/ignition-citadel.rb
@@ -2,7 +2,9 @@ class IgnitionCitadel < Formula
   desc "Ignition Citadel collection"
   homepage "https://bitbucket.org/ignitionrobotics/ign-citadel"
   url "https://osrf-distributions.s3.amazonaws.com/ign-citadel/releases/ignition-citadel-1.0.0~pre1.tar.bz2"
+  version "1.0.0~pre1"
   sha256 "18d64051a42073e68c16cda327046b596742b5102caf70d51f496e8721241fbc"
+  version_scheme 1
 
   head "https://bitbucket.org/ignitionrobotics/ign-citadel", :branch => "default", :using => :hg
 
