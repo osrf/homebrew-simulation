b7fade53816f2144877a4bd44dfa8ae6990047bb
osrfbuild <osrfbuild@osrfoundation.org>
2019-12-18 02:41:14 +0100

    ignition-transport7 7.2.1 (#938)
    * ignition-transport7: update 7.2.1
    
    * ignition-transport7: update 7.2.1 bottle.

--- Formula/ignition-transport7.rb
+++ Formula/ignition-transport7.rb
@@ -1,13 +1,13 @@
 class IgnitionTransport7 < Formula
   desc "Transport middleware for robotics"
   homepage "https://ignitionrobotics.org"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-transport/releases/ignition-transport7-7.2.0.tar.bz2"
-  sha256 "05102904e32720ad75bfbb29d56f0bf24cae44ed625b128cb6b19f2746a6d404"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-transport/releases/ignition-transport7-7.2.1.tar.bz2"
+  sha256 "f6ab8426918a2d2cedc577fd5a7483fab3799223f3aaf41f296c6e816fe64eea"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 "284f3eab9af7afb998e1ceab1933eb0bca5104dcfe2848050ed9d5fe2b37b1ac" => :mojave
-    sha256 "e54568e4a80ab1059149044e1289eaf2f83777ad3f1fddf053a871eb5d4d67ed" => :high_sierra
+    sha256 "319c704c81ec10634ce9efea34451be8aa656d67ff578e8ae18f00b3041ba036" => :mojave
+    sha256 "ad46d79f8d7de84bea5cb2d703df0005273f78ac72dc5b4462acc131bdb17f13" => :high_sierra
   end
 
   depends_on "doxygen" => [:build, :optional]
