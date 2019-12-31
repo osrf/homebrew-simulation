c532f45c41b917791115e6d0f57d10a24a82f3c2
Steven Peters <scpeters@openrobotics.org>
2019-12-31 15:53:41 -0800

    gazebo11: initial version

---
+++ Formula/gazebo11.rb
@@ -0,0 +1,106 @@
+class Gazebo11 < Formula
+  desc "Gazebo robot simulator"
+  homepage "http://gazebosim.org"
+  url "https://bitbucket.org/osrf/gazebo/get/4c0591897f59a777fe5c0bf89d1bede4c7f66f21.tar.bz2"
+  version "10.999.999~20191227~4c05918"
+  sha256 "86940b13c1a2a6f2373f82c544d750ad2e32bcd6578168064a480db3b5f9f90c"
+
+  head "https://bitbucket.org/osrf/gazebo", :branch => "default", :using => :hg
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "boost"
+  depends_on "doxygen"
+  depends_on "freeimage"
+  depends_on "graphviz"
+  depends_on "ignition-fuel-tools1"
+  depends_on "ignition-math4"
+  depends_on "ignition-msgs1"
+  depends_on "ignition-transport4"
+  depends_on "libtar"
+  depends_on "ogre1.9"
+  depends_on "ossp-uuid" => :linked
+  depends_on "protobuf"
+  depends_on "protobuf-c"
+  depends_on "qt"
+  depends_on "qwt"
+  depends_on "sdformat6"
+  depends_on "tbb"
+  depends_on "tinyxml"
+  depends_on "tinyxml2"
+  depends_on "zeromq" => :linked
+
+  depends_on "bullet" => :recommended
+  depends_on "dartsim" => :recommended
+  depends_on "ffmpeg" => :recommended
+  depends_on "gts" => :recommended
+  depends_on "simbody" => :recommended
+  depends_on "gdal" => :optional
+  depends_on "player" => :optional
+
+  conflicts_with "gazebo2", :because => "Differing version of the same formula"
+  conflicts_with "gazebo3", :because => "Differing version of the same formula"
+  conflicts_with "gazebo4", :because => "Differing version of the same formula"
+  conflicts_with "gazebo5", :because => "Differing version of the same formula"
+  conflicts_with "gazebo6", :because => "Differing version of the same formula"
+  conflicts_with "gazebo7", :because => "Differing version of the same formula"
+  conflicts_with "gazebo8", :because => "Differing version of the same formula"
+  conflicts_with "gazebo9", :because => "Differing version of the same formula"
+  conflicts_with "gazebo10", :because => "Differing version of the same formula"
+
+  def install
+    ENV.m64
+
+    cmake_args = std_cmake_args
+    cmake_args << "-DQWT_WIN_INCLUDE_DIR=#{HOMEBREW_PREFIX}/lib/qwt.framework/Headers"
+    cmake_args << "-DQWT_WIN_LIBRARY_DIR=#{HOMEBREW_PREFIX}/lib/qwt.framework"
+
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    # this used to show boost linking errors, but not anymore
+    system "#{bin}/gz", "sdf"
+    # running this sample code seg-faults from boost filesystem
+    # if a bottle rebuild is needed
+    (testpath/"test.cpp").write <<-EOS
+      #include <gazebo/common/CommonIface.hh>
+      int main() {
+        gazebo::common::copyDir(".", "./tmp");
+        return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 2.8 FATAL_ERROR)
+      find_package(gazebo QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      include_directories(${GAZEBO_INCLUDE_DIRS})
+      target_link_libraries(test_cmake ${GAZEBO_LIBRARIES})
+    EOS
+    system "pkg-config", "gazebo"
+    # cflags = `pkg-config --cflags gazebo`.split(" ")
+    # libs = `pkg-config --libs gazebo`.split(" ")
+    # boost libs not properly generated in pkg-config file
+    # disable test for now
+    # see https://github.com/osrf/homebrew-simulation/issues/850
+    # system ENV.cc, "test.cpp",
+    #                *cflags,
+    #                "-L#{lib}",
+    #                *libs,
+    #                "-lc++",
+    #                "-o", "test"
+    # system "./test"
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
