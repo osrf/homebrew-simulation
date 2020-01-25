7a72d5454c4d09f9bfe3927e52b5d1608f95d88f
osrfbuild <osrfbuild@osrfoundation.org>
2020-01-25 17:16:52 +0100

    sdformat9 9.1.0~pre2 (#954)
    * sdformat9: update 9.1.0~pre2
    
    * sdformat9: update 9.1.0~pre2 bottle.

--- Formula/sdformat9.rb
+++ Formula/sdformat9.rb
@@ -1,13 +1,13 @@
 class Sdformat9 < Formula
   desc "Simulation Description Format"
   homepage "http://sdformat.org"
-  url "https://osrf-distributions.s3.amazonaws.com/sdformat/releases/sdformat-9.1.0~pre1.tar.bz2"
-  version "9.1.0~pre1"
-  sha256 "d8d47e9a4c04d56ded4dd7b3f3e4deac2033f8f049c86723570a14a48fb99b32"
+  url "https://osrf-distributions.s3.amazonaws.com/sdformat/releases/sdformat-9.1.0~pre2.tar.bz2"
+  version "9.1.0~pre2"
+  sha256 "373555b996bbc99e48b20abbd20475bf57e855409c45cb904db5d6314cda4878"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 "237f8c7e17e257637e4429ba553c8a5170c4914ffae66413d37101eb28e32f36" => :mojave
+    sha256 "fd31a09cee98d7edf4ddfdc47833b1bd23c2af6d160265f2bad23e129a399c5f" => :mojave
   end
 
   depends_on "cmake" => :build
