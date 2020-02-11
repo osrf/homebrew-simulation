30bb97fa905b65d56e7a516fe3051b8f6d263660
Steven Peters <scpeters@openrobotics.org>
2020-02-11 09:26:11 -0800

    gazebo11: revision bump for fcl

--- Formula/gazebo11.rb
+++ Formula/gazebo11.rb
@@ -3,6 +3,7 @@ class Gazebo11 < Formula
   homepage "http://gazebosim.org"
   url "https://osrf-distributions.s3.amazonaws.com/gazebo/releases/gazebo-11.0.0.tar.bz2"
   sha256 "3c823e3ed6fbf79a23663eade5f33460c5334dde3b99e376dcb69fa2561886f5"
+  revision 1
 
   head "https://bitbucket.org/osrf/gazebo", :branch => "default", :using => :hg
 
