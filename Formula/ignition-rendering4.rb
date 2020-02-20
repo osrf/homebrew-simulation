2e18251c3432ad9c4ab916cb57831dfff6d833bd
iche033 <ichen@osrfoundation.org>
2020-02-20 00:14:11 -0800

    ignition-sensors4 and ignition-rendering4: new formulas (#967)
    * : update  bottle.
    
    Co-authored-by: Steven Peters <scpeters@openrobotics.org>
    Co-authored-by: osrfbuild <osrfbuild@osrfoundation.org>

---
+++ Formula/ignition-rendering4.rb
@@ -0,0 +1,71 @@
+class IgnitionRendering4 < Formula
+  desc "Rendering library for robotics applications"
+  homepage "https://bitbucket.org/ignitionrobotics/ign-rendering"
+  url "https://bitbucket.org/ignitionrobotics/ign-rendering/get/e034fda8fefb2eff5d463013e38d2a6475eecc2a.tar.gz"
+  version "3.999.999~0~20200214~e034fda"
+  sha256 "a060260f5168c644191cca9e75a1af99500c1dddd0a1435f2753d382d820dfe1"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 "13ee7444ef222b9821651fce04f7bfff2e2811de9240e95b4b79aa11523241a9" => :mojave
+  end
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "pkg-config" => [:build, :test]
+
+  depends_on "freeimage"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common3"
+  depends_on "ignition-math6"
+  depends_on "ignition-plugin1"
+  depends_on :macos => :mojave # c++17
+  depends_on "ogre1.9"
+  depends_on "ogre2.1"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    azure = !ENV["HOMEBREW_AZURE_PIPELINES"].nil?
+    travis = !ENV["HOMEBREW_TRAVIS_CI"].nil?
+    (testpath/"test.cpp").write <<-EOS
+      #include <ignition/rendering/RenderEngine.hh>
+      #include <ignition/rendering/RenderingIface.hh>
+      int main(int _argc, char** _argv)
+      {
+        ignition::rendering::RenderEngine *engine =
+            ignition::rendering::engine("ogre");
+        return engine == nullptr;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.10.2 FATAL_ERROR)
+      find_package(ignition-rendering4 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-rendering4::ignition-rendering4)
+    EOS
+    # test building with pkg-config
+    system "pkg-config", "ignition-rendering4"
+    cflags   = `pkg-config --cflags ignition-rendering4`.split(" ")
+    ldflags  = `pkg-config --libs ignition-rendering4`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   *ldflags,
+                   "-lc++",
+                   "-o", "test"
+    system "./test" unless azure || travis
+    # test building with cmake
+    mkdir "build" do
+      ENV.delete("MACOSX_DEPLOYMENT_TARGET")
+      ENV.delete("SDKROOT")
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake" unless azure || travis
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
---
+++ Formula/ignition-sensors4.rb
@@ -0,0 +1,70 @@
+class IgnitionSensors4 < Formula
+  desc "Sensors library for robotics applications"
+  homepage "https://bitbucket.org/ignitionrobotics/ign-sensors"
+  url "https://bitbucket.org/ignitionrobotics/ign-sensors/get/e77b3a1e2abcf4b4ac1fad2158cf89c59792e6ec.tar.gz"
+  version "3.999.999~0~20200214~e77b3a1"
+  sha256 "5761a7b63acbcda868b042814a1e91cc9e0962a429f40e77c2bff918cd3e8bb3"
+
+  head "https://bitbucket.org/ignitionrobotics/ign-sensors", :branch => "default", :using => :hg
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 "e199155d71d9b114d16c28f0a928bf8295c1761a1a92e0d885a3535de302bc0a" => :mojave
+  end
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "pkg-config" => [:build, :test]
+
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common3"
+  depends_on "ignition-math6"
+  depends_on "ignition-msgs5"
+  depends_on "ignition-rendering4"
+  depends_on "ignition-transport8"
+  depends_on "sdformat9"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <ignition/sensors/Noise.hh>
+
+      int main()
+      {
+        ignition::sensors::Noise noise(ignition::sensors::NoiseType::NONE);
+
+        return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.10.2 FATAL_ERROR)
+      find_package(ignition-sensors4 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-sensors4::ignition-sensors4)
+    EOS
+    # test building with pkg-config
+    system "pkg-config", "ignition-sensors4"
+    cflags   = `pkg-config --cflags ignition-sensors4`.split(" ")
+    ldflags  = `pkg-config --libs ignition-sensors4`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   *ldflags,
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    mkdir "build" do
+      ENV.delete("MACOSX_DEPLOYMENT_TARGET")
+      ENV.delete("SDKROOT")
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
