e821a8855a87c182ba3f3c62399dca4654c207b3
Ian Chen <ichen@osrfoundation.org>
2020-02-24 17:06:34 -0800

    add ign-gazebo4 formula

---
+++ Formula/ignition-gazebo4.rb
@@ -0,0 +1,101 @@
+class IgnitionGazebo4 < Formula
+  desc "Ignition Gazebo robot simulator"
+  homepage "https://bitbucket.org/ignitionrobotics/ign-gazebo"
+  url "https://bitbucket.org/ignitionrobotics/ign-gazebo/get/78c43e71a5a079588fd18c84efab54578267e7ec.tar.bz2"
+  version "3.999.999~0~20200224~78c43e7"
+  sha256 "ce663e5bbc06a13653088d3c0f8a21a47642be6aa3d59804b7d05669cf7f02d8"
+
+  head "https://bitbucket.org/ignitionrobotics/ign-gazebo", :branch => "default", :using => :hg
+
+  depends_on "cmake" => :build
+  depends_on "gflags"
+  depends_on "google-benchmark"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common3"
+  depends_on "ignition-fuel-tools4"
+  depends_on "ignition-gui4"
+  depends_on "ignition-math6"
+  depends_on "ignition-msgs5"
+  depends_on "ignition-physics2"
+  depends_on "ignition-plugin1"
+  depends_on "ignition-rendering4"
+  depends_on "ignition-sensors4"
+  depends_on "ignition-transport8"
+  depends_on :macos => :mojave # c++17
+  depends_on "pkg-config"
+  depends_on "sdformat9"
+
+  def install
+    ENV.m64
+
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+    #include <cstdint>
+    #include <ignition/gazebo/Entity.hh>
+    #include <ignition/gazebo/EntityComponentManager.hh>
+    #include <ignition/gazebo/components/World.hh>
+    #include <ignition/gazebo/components/Name.hh>
+
+    using namespace ignition;
+    using namespace gazebo;
+
+    //////////////////////////////////////////////////
+    int main(int argc, char **argv)
+    {
+      // Warm start. Initial allocation of resources can throw off calculations.
+      {
+        // Create the entity component manager
+        EntityComponentManager mgr;
+
+        // Create the matching entities
+        for (int i = 0; i < 100; ++i)
+        {
+          Entity entity = mgr.CreateEntity();
+          mgr.CreateComponent(entity, components::World());
+          mgr.CreateComponent(entity, components::Name("world_name"));
+        }
+
+        mgr.Each<components::World, components::Name>(
+            [&](const Entity &, const components::World *,
+              const components::Name *)->bool {return true;});
+      }
+
+      return 0;
+    }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
+      find_package(ignition-gazebo4 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-gazebo4::core)
+    EOS
+    ENV.append_path "PKG_CONFIG_PATH", Formula["qt"].opt_lib/"pkgconfig"
+    system "pkg-config", "ignition-gazebo4"
+    cflags   = `pkg-config --cflags ignition-gazebo4`.split(" ")
+    ldflags  = `pkg-config --libs ignition-gazebo4`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   *ldflags,
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    ENV.append_path "CMAKE_PREFIX_PATH", Formula["qt"].opt_prefix
+    mkdir "build" do
+      ENV.delete("MACOSX_DEPLOYMENT_TARGET")
+      ENV.delete("SDKROOT")
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
