6feeb124f4deb7d81e7b1aa3c2d88df32d1bf3db
Steven Peters <scpeters@openrobotics.org>
2020-03-14 10:08:40 -0700

    ignition-gazebo4: revision bump for tinyxml2

--- Formula/ignition-gazebo4.rb
+++ Formula/ignition-gazebo4.rb
@@ -4,6 +4,7 @@ class IgnitionGazebo4 < Formula
   url "https://bitbucket.org/ignitionrobotics/ign-gazebo/get/78c43e71a5a079588fd18c84efab54578267e7ec.tar.bz2"
   version "3.999.999~0~20200224~78c43e7"
   sha256 "ce663e5bbc06a13653088d3c0f8a21a47642be6aa3d59804b7d05669cf7f02d8"
+  revision 1
 
   head "https://bitbucket.org/ignitionrobotics/ign-gazebo", :branch => "default", :using => :hg
 
