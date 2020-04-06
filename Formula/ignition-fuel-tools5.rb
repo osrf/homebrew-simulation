b6e7bbd5f26e943a53628d65c788a6b4a69666c9
Louise Poubel <louise@openrobotics.org>
2020-04-06 14:18:39 -0700

    ignition-msgs6, ignition-fuel-tools5: Dome dependencies
    Signed-off-by: Louise Poubel <louise@openrobotics.org>
    
    no bottle, add version and head
    
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

---
+++ Formula/ignition-fuel-tools5.rb
@@ -0,0 +1,63 @@
+class IgnitionFuelTools5 < Formula
+  desc "Tools for using Fuel API to download robot models"
+  homepage "https://ignitionrobotics.org"
+  url "https://bitbucket.org/ignitionrobotics/ign-fuel-tools/get/5b7d9e7be787619a3b125b0675912e1a21403fc6.tar.bz2"
+  sha256 "8f8a25b56265dd3853383100f9b73b9af22fdb06980c8d917dfb0316afb16229"
+  version "4.999.999~0~20200407~5b7d9e"
+
+  head "https://bitbucket.org/ignitionrobotics/ign-fuel-tools", :branch => "default", :using => :hg
+
+  depends_on "cmake"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common3"
+  depends_on "ignition-msgs6"
+  depends_on "jsoncpp"
+  depends_on "libyaml"
+  depends_on "libzip"
+  depends_on :macos => :high_sierra # c++17
+  depends_on "pkg-config"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <ignition/fuel_tools.hh>
+      int main() {
+        ignition::fuel_tools::ServerConfig srv;
+        return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 2.8 FATAL_ERROR)
+      find_package(ignition-fuel_tools5 QUIET REQUIRED)
+      include_directories(${IGNITION-FUEL_TOOLS_INCLUDE_DIRS})
+      link_directories(${IGNITION-FUEL_TOOLS_LIBRARY_DIRS})
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-fuel_tools5::ignition-fuel_tools5)
+    EOS
+    # test building with pkg-config
+    system "pkg-config", "ignition-fuel_tools5"
+    cflags = `pkg-config --cflags ignition-fuel_tools5`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lignition-fuel_tools5",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
