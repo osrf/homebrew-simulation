7309a65cdf74bd04e084c4a559a61c789aafc4b9
iche033 <ichen@osrfoundation.org>
2020-04-08 15:22:28 -0700

    ign-rendering4 update (#996)
    * update ign-rendering4
    
    * ignition-rendering4: update update bottle.
    
    Co-authored-by: OSRF Build Bot <osrfbuild@osrfoundation.org>

--- Formula/ignition-rendering4.rb
+++ Formula/ignition-rendering4.rb
@@ -1,13 +1,13 @@
 class IgnitionRendering4 < Formula
   desc "Rendering library for robotics applications"
   homepage "https://bitbucket.org/ignitionrobotics/ign-rendering"
-  url "https://bitbucket.org/ignitionrobotics/ign-rendering/get/edb7205171642059f8ff2f0b7f6d0ec894ce9e4e.tar.gz"
-  version "3.999.999~0~20200310~edb7205"
-  sha256 "283c875029eaf97a6ef1d7d21aae64e8f82fcea0899f9f5099c9ab2747f66c64"
+  url "https://bitbucket.org/ignitionrobotics/ign-rendering/get/f0dc5f4ce27697dbc02e2fe74a1a0fb3b5c091d8.tar.gz"
+  version "3.999.999~0~20200310~f0dc5f4"
+  sha256 "4f5eae2d1de279ae16c20607c8dcc0ef1dab057e4b29cda68dccfbe07161a71f"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 "0a1a73a6f33fdea03a4b0816ce7c53688345650f5fc52063fa9e466817ec7615" => :mojave
+    sha256 "9a399f361d1c4605e4ee5d322440420dfb9fd7a0daf1cec1ebbe69d7a5eb2e13" => :mojave
   end
 
   depends_on "cmake" => [:build, :test]
