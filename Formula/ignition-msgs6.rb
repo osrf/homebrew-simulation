74cb1a184bd084c241c96a003adb99595e87fb6e
chapulina <louise@openrobotics.org>
2020-04-13 10:57:48 -0700

    ignition-msgs6, ignition-fuel-tools5: Dome dependencies (#994)
    Signed-off-by: Louise Poubel <louise@openrobotics.org>
    
    no bottle, add version and head
    
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

---
+++ Formula/ignition-msgs6.rb
@@ -0,0 +1,62 @@
+class IgnitionMsgs6 < Formula
+  desc "Middleware protobuf messages for robotics"
+  homepage "https://bitbucket.org/ignitionrobotics/ign-msgs"
+  url "https://bitbucket.org/ignitionrobotics/ign-msgs/get/47d93769c80c7da72f03c3dd8ba5a36fcd6ab076.tar.bz2"
+  sha256 "8b9422786573a4f40e2aa63741fa964336a3e1138c8dee6e8c98847ff4ea1fb1"
+  version "5.999.999~0~20200407~47d937"
+
+  head "https://bitbucket.org/ignitionrobotics/ign-msgs", :branch => "default", :using => :hg
+
+  depends_on "protobuf-c" => :build
+  depends_on "cmake"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-math6"
+  depends_on "ignition-tools"
+  depends_on :macos => :high_sierra # c++17
+  depends_on "pkg-config"
+  depends_on "protobuf"
+  depends_on "tinyxml2"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <ignition/msgs.hh>
+      int main() {
+        ignition::msgs::UInt32;
+        return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.10 FATAL_ERROR)
+      find_package(ignition-msgs6 QUIET REQUIRED)
+      set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${IGNITION-MSGS_CXX_FLAGS}")
+      include_directories(${IGNITION-MSGS_INCLUDE_DIRS})
+      link_directories(${IGNITION-MSGS_LIBRARY_DIRS})
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-msgs6::ignition-msgs6)
+    EOS
+    # test building with pkg-config
+    system "pkg-config", "ignition-msgs6"
+    cflags = `pkg-config --cflags ignition-msgs6`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lignition-msgs6",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
