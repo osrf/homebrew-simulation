360885be9b83dae709bfd4f7e3ec4061afdf92b8
Steve Peters <scpeters@openrobotics.org>
2020-06-04 01:08:13 -0700

    ignition-rndf: github tarball
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/ignition-rndf0.rb
+++ Formula/ignition-rndf0.rb
@@ -1,8 +1,8 @@
 class IgnitionRndf0 < Formula
   desc "Ignition RNDF is a library for parsing RNDF road network files"
   homepage "https://ignitionrobotics.org"
-  url "https://bitbucket.org/ignitionrobotics/ign-rndf/get/7b8b1d2afa680424e4f53dbc7beafcdb88db50e0.tar.gz"
-  sha256 "12f78a8534d812bc6248021d78961b1458715ec6f84edc2cce2a1c5fe01fc416"
+  url "https://github.com/ignitionrobotics/ign-rndf/archive/45652bceabce1ea1630b41623f56560f10674343.tar.gz"
+  sha256 "ae108308d8a7b4dddfd3a5d23eb3d8a844e1760bf01cb3066cdd45570cf6c26f"
 
   head "https://github.com/ignitionrobotics/ign-rndf", :branch => "master"
 
