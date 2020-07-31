cc13d3b8c21b1ba26615fcd84a2ae3e020265f1f
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-07-31 23:54:22 +0200

    Add ign-physics3 formula for Dome (#1084)
    * Add ign-physics3 formula for Dome

---
+++ Formula/ignition-physics3.rb
@@ -0,0 +1,64 @@
+class IgnitionPhysics3 < Formula
+  desc "Physics library for robotics applications"
+  homepage "https://github.com/ignitionrobotics/ign-physics"
+  url "https://github.com/ignitionrobotics/ign-physics/archive/061965f69be7077c2ec3732ef4ef6b07f1a8cf06.tar.gz"
+  sha256 "fac94bdc0fb61b0bd6fe398ebce9b6a2525a421e134747772ee0fb7e633e7233"
+  version "2.999.999~0~20200731~061965f"
+  license "Apache-2.0"
+  revision 1
+
+  depends_on "cmake" => :build
+
+  depends_on "bullet"
+  depends_on "dartsim@6.10.0"
+  depends_on "google-benchmark"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common3"
+  depends_on "ignition-math6"
+  depends_on "ignition-plugin1"
+  depends_on macos: :mojave # c++17
+  depends_on "pkg-config"
+  depends_on "sdformat10"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include "ignition/plugin/Loader.hh"
+      #include "ignition/physics/ConstructEmpty.hh"
+      #include "ignition/physics/RequestEngine.hh"
+      int main()
+      {
+        ignition::plugin::Loader loader;
+        loader.LoadLib("#{opt_lib}/libignition-physics2-dartsim-plugin.dylib");
+        ignition::plugin::PluginPtr dartsim =
+            loader.Instantiate("ignition::physics::dartsim::Plugin");
+        using featureList = ignition::physics::FeatureList<
+            ignition::physics::ConstructEmptyWorldFeature>;
+        auto engine =
+            ignition::physics::RequestEngine3d<featureList>::From(dartsim);
+        return engine == nullptr;
+      }
+    EOS
+    system "pkg-config", "ignition-physics2"
+    cflags   = `pkg-config --cflags ignition-physics2`.split(" ")
+    ldflags  = `pkg-config --libs ignition-physics2`.split(" ")
+    system "pkg-config", "ignition-plugin1-loader"
+    loader_cflags   = `pkg-config --cflags ignition-plugin1-loader`.split(" ")
+    loader_ldflags  = `pkg-config --libs ignition-plugin1-loader`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   *ldflags,
+                   *loader_cflags,
+                   *loader_ldflags,
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
