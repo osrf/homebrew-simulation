7089c44b44cfe616aa654add78c29f3891314a33
Steve Peters <scpeters@openrobotics.org>
2020-09-16 14:17:11 -0700

    ignition-cmake2: revision bump to rebuild bottles (#1122)
    * update  bottle.
    
    Co-authored-by: OSRF Build Bot <osrfbuild@osrfoundation.org>

--- Formula/ignition-cmake2.rb
+++ Formula/ignition-cmake2.rb
@@ -4,6 +4,14 @@ class IgnitionCmake2 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-cmake/releases/ignition-cmake2-2.5.0.tar.bz2"
   sha256 "b5ea81835ea398b378edb818083f9dfc08441fadb721e37fc722d7faa9bd63b2"
   license "Apache-2.0"
+  revision 1
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    cellar :any_skip_relocation
+    sha256 "81d46dfd407bf74609a2f305d07ff1e7d12087620f2618259489d4bbbec043d8" => :mojave
+    sha256 "42dcfa0997f00db2ea5ed3f5799cd75f85c4df0705a8a2de708b2669a1ace055" => :high_sierra
+  end
 
   depends_on "cmake"
   depends_on "pkg-config"
