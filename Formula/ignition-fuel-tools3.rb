81478ee4e8caa70cbe1daabfb464c8bf823398c2
Steve Peters <scpeters@openrobotics.org>
2020-09-18 09:45:42 -0700

    Use -DBUILD_TESTING=Off

--- Formula/ignition-fuel-tools3.rb
+++ Formula/ignition-fuel-tools3.rb
@@ -16,8 +16,11 @@ class IgnitionFuelTools3 < Formula
   depends_on "pkg-config"
 
   def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_TESTING=Off"
+
     mkdir "build" do
-      system "cmake", "..", *std_cmake_args
+      system "cmake", "..", *cmake_args
       system "make", "install"
     end
   end
