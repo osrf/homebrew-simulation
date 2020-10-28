4587b69c6bb9d06be4e9c35c717410fc51a65d8b
Louise Poubel <louise@openrobotics.org>
2020-10-28 16:03:25 -0700

    Bump in edifice: ign-rendering5
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

---
+++ Formula/ignition-rendering5.rb
@@ -0,0 +1,68 @@
+class IgnitionRendering5 < Formula
+  desc "Rendering library for robotics applications"
+  homepage "https://github.com/ignitionrobotics/ign-rendering"
+  url "https://github.com/ignitionrobotics/ign-rendering/archive/d18dd1c6540b2aaa581a3f48ce96022d7ba19bda.tar.gz"
+  sha256 "4c97c5eb65b3c939db06176b5d1b3728af69ce17a0bb7ba522fcadcbac6b5c2b"
+  license "Apache-2.0"
+
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "pkg-config" => [:build, :test]
+
+  depends_on "freeimage"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common3"
+  depends_on "ignition-math6"
+  depends_on "ignition-plugin1"
+  depends_on macos: :mojave # c++17
+  depends_on "ogre1.9"
+  depends_on "ogre2.1"
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_TESTING=Off"
+    system "cmake", ".", *cmake_args
+    system "make", "install"
+  end
+
+  test do
+    azure = ENV["HOMEBREW_AZURE_PIPELINES"].present?
+    github_actions = ENV["HOMEBREW_GITHUB_ACTIONS"].present?
+    travis = ENV["HOMEBREW_TRAVIS_CI"].present?
+    (testpath/"test.cpp").write <<-EOS
+      #include <ignition/rendering/RenderEngine.hh>
+      #include <ignition/rendering/RenderingIface.hh>
+      int main(int _argc, char** _argv)
+      {
+        ignition::rendering::RenderEngine *engine =
+            ignition::rendering::engine("ogre");
+        return engine == nullptr;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.10.2 FATAL_ERROR)
+      find_package(ignition-rendering5 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-rendering5::ignition-rendering5)
+    EOS
+    # test building with pkg-config
+    system "pkg-config", "ignition-rendering5"
+    cflags   = `pkg-config --cflags ignition-rendering5`.split(" ")
+    ldflags  = `pkg-config --libs ignition-rendering5`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   *ldflags,
+                   "-lc++",
+                   "-o", "test"
+    system "./test" unless azure || github_actions || travis
+    # test building with cmake
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake" unless azure || github_actions || travis
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
