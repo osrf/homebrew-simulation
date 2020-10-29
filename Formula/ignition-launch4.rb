09174b066dbcf89c7746267b384560cc4ebe213a
Louise Poubel <louise@openrobotics.org>
2020-10-28 18:45:12 -0700

    Bump in edifice: ign-launch5
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

---
+++ Formula/ignition-launch4.rb
@@ -0,0 +1,37 @@
+class IgnitionLaunch4 < Formula
+  desc "Launch libraries for robotics applications"
+  homepage "https://github.com/ignitionrobotics/ign-launch"
+  url "https://github.com/ignitionrobotics/ign-launch/archive/29823c79fa313c67ee50ec6589a656e60f2667a1.tar.gz"
+  version "3.999.999~0~20201028~29823c"
+  sha256 "c2a28509cad17b26bca726c7ae030585c6f9df34c7cdc8481874821fcbbd94ad"
+  license "Apache-2.0"
+
+  head "https://github.com/ignitionrobotics/ign-launch", branch: "main"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common3"
+  depends_on "ignition-gazebo5"
+  depends_on "ignition-gui5"
+  depends_on "ignition-msgs6"
+  depends_on "ignition-plugin1"
+  depends_on "ignition-tools"
+  depends_on "ignition-transport9"
+  depends_on "qt"
+  depends_on "tinyxml2"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    ENV["IGN_CONFIG_PATH"] = "#{opt_share}/ignition"
+    system "ign", "launch", "--versions"
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
