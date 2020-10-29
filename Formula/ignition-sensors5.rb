f60d503210e25bf0c9ec029e368d77896a6c936a
Louise Poubel <louise@openrobotics.org>
2020-10-28 18:46:40 -0700

    Bump in edifice: ign-sensors5
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

---
+++ Formula/ignition-sensors5.rb
@@ -0,0 +1,67 @@
+class IgnitionSensors5 < Formula
+  desc "Sensors library for robotics applications"
+  homepage "https://github.com/ignitionrobotics/ign-sensors"
+  url "https://github.com/ignitionrobotics/ign-sensors/archive/c02cd052f263373d630eb8e1a2714d9965cb48d2.tar.gz"
+  version "4.999.999~0~20201028~c02cd0"
+  sha256 "fdd953c50aa814f52ec4c769c74c83755ec5056c9b53cd4a899bbd12004e5481"
+  license "Apache-2.0"
+
+  head "https://github.com/ignitionrobotics/ign-sensors", branch: "main"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "pkg-config" => [:build, :test]
+
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common3"
+  depends_on "ignition-math6"
+  depends_on "ignition-msgs6"
+  depends_on "ignition-rendering5"
+  depends_on "ignition-transport9"
+  depends_on "sdformat10"
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_TESTING=OFF"
+
+    system "cmake", ".", *cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <ignition/sensors/Noise.hh>
+
+      int main()
+      {
+        ignition::sensors::Noise noise(ignition::sensors::NoiseType::NONE);
+
+        return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.10.2 FATAL_ERROR)
+      find_package(ignition-sensors5 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-sensors5::ignition-sensors5)
+    EOS
+    # test building with pkg-config
+    system "pkg-config", "ignition-sensors5"
+    cflags   = `pkg-config --cflags ignition-sensors5`.split(" ")
+    ldflags  = `pkg-config --libs ignition-sensors5`.split(" ")
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   *ldflags,
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
