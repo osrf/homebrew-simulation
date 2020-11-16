35f76dfe47a67ed1e4d52064724165cb9df35ee6
osrfbuild <osrfbuild@osrfoundation.org>
2020-11-16 20:52:09 +0100

    ignition-launch2 2.2.0 (#1202)
    * update  bottle.

--- Formula/ignition-launch2.rb
+++ Formula/ignition-launch2.rb
@@ -1,16 +1,15 @@
 class IgnitionLaunch2 < Formula
   desc "Launch libraries for robotics applications"
   homepage "https://github.com/ignitionrobotics/ign-launch"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-launch/releases/ignition-launch2-2.1.0.tar.bz2"
-  sha256 "1ff5ed5e5f3216f40eacfcb01f206f989b279393f4ee65a36885c603d01bff29"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-launch/releases/ignition-launch2-2.2.0.tar.bz2"
+  sha256 "078b32dfe3fecaf95073b17f44be5afc3badc8e706f97a900ee89147a97b38a3"
   license "Apache-2.0"
-  revision 1
 
   head "https://github.com/ignitionrobotics/ign-launch", branch: "ign-launch2"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 "6525f64d29d6f64847d45e6a25f9f3eccd7a6d9fd06fb867731bc75d4cd68301" => :mojave
+    sha256 "ae84a68112f0a609768c332ef1c01ddbaf9e88d5d9dcc8c5fe7551ad5b3385a8" => :mojave
   end
 
   depends_on "cmake" => :build
