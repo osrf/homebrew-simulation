35fa514387d00f80fccd82a9986618e3f1c1965e
Steve Peters <scpeters@openrobotics.org>
2020-11-23 11:49:54 -0800

    gazebo11: revision bump for protobuf 3.14

--- Formula/gazebo11.rb
+++ Formula/gazebo11.rb
@@ -4,7 +4,7 @@ class Gazebo11 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gazebo/releases/gazebo-11.2.0.tar.bz2"
   sha256 "05ca11573e9d3451aae9ae282dada9f822a4d8a473642b499e4f7947c8397cf3"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/osrf/gazebo", branch: "gazebo11"
 
