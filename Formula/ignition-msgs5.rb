763bef6ed7b083fcc1097bbc5ad515d4fe41dda9
Steve Peters <scpeters@openrobotics.org>
2020-11-23 10:44:37 -0800

    ignition-msgs5: revision bump for protobuf 3.14

--- Formula/ignition-msgs5.rb
+++ Formula/ignition-msgs5.rb
@@ -4,6 +4,7 @@ class IgnitionMsgs5 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-msgs/releases/ignition-msgs5-5.4.0.tar.bz2"
   sha256 "e30f7f5d2e4aace7e72adfd3042e7cc786390b377a49c7a44b31a816e4656dd0"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
