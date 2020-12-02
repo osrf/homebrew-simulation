10f7802e5486c2ef1c76321af18b2f479888020d
osrfbuild <osrfbuild@osrfoundation.org>
2020-12-02 20:29:18 +0100

    ignition-fuel-tools4 4.3.0 (#1216)
    * ignition-launch2: revision bump for protobuf
    * update  bottle.
    
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/ignition-launch2.rb
+++ Formula/ignition-launch2.rb
@@ -4,12 +4,13 @@ class IgnitionLaunch2 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-launch/releases/ignition-launch2-2.2.0.tar.bz2"
   sha256 "078b32dfe3fecaf95073b17f44be5afc3badc8e706f97a900ee89147a97b38a3"
   license "Apache-2.0"
+  revision 1
 
   head "https://github.com/ignitionrobotics/ign-launch", branch: "ign-launch2"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 "ae84a68112f0a609768c332ef1c01ddbaf9e88d5d9dcc8c5fe7551ad5b3385a8" => :mojave
+    sha256 "b318a5d3b976fe8bee3833b52b7de016166a71d4f663041e0166b9b56162c79b" => :mojave
   end
 
   depends_on "cmake" => :build
