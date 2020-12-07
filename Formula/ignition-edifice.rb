8be07eb1c396e573efcebae40431d828e02a75f2
Steve Peters <scpeters@openrobotics.org>
2020-12-07 01:07:03 -0800

    ignition-edifice: test with vcs validate
    also bump revision

--- Formula/ignition-edifice.rb
+++ Formula/ignition-edifice.rb
@@ -1,9 +1,12 @@
 class IgnitionEdifice < Formula
+  include Language::Python::Virtualenv
+
   desc "Collection of gazebo simulation software"
   homepage "https://github.com/ignitionrobotics/ign-edifice"
   url "https://github.com/ignitionrobotics/ign-edifice/archive/4d48857070561af742efd68b8d4b13b9903572a8.tar.gz"
   sha256 "ad648480b1abe1a6bc08c9ece5d422cf21aad4eb27c619fe7519e3673e5b35d5"
   license "Apache-2.0"
+  revision 1
 
   head "https://github.com/ignitionrobotics/ign-edifice", branch: "main"
 
@@ -26,6 +29,11 @@ class IgnitionEdifice < Formula
   depends_on "pkg-config"
   depends_on "sdformat10"
 
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/64/c2/b80047c7ac2478f9501676c988a5411ed5572f35d1beff9cae07d321512c/PyYAML-5.3.1.tar.gz"
+    sha256 "b8eac752c5e14d3eca0e6dd9199cd627518cb5ec06add0de9d32baeee6fe645d"
+  end
+
   def install
     ENV.m64
 
@@ -33,11 +41,14 @@ class IgnitionEdifice < Formula
       system "cmake", "..", *std_cmake_args
       system "make", "install"
     end
+
+    venv = virtualenv_create(libexec, Formula["python@3.9"].opt_bin/"python3")
+    %w{PyYAML, vcstool}.each do |pkg|
+      venv.pip_install pkg
+    end
   end
 
-  # Failing test in Mojave
-  # test do
-  # TODO: improve the testing
-  #  system "#{bin}/ign", "gazebo", "--help"
-  # end
+  test do
+    system libexec/"bin/vcs", "validate", "--input", share/"ignition/ignition-citadel/gazebodistro/collection-citadel.yaml"
+  end
 end
