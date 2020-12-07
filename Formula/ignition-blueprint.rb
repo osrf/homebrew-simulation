1fba9143d7efcb45d0045f58330e5f9e9e0e3d7d
Steve Peters <scpeters@openrobotics.org>
2020-12-07 11:32:08 -0800

    ignition-blueprint: use latest commit

--- Formula/ignition-blueprint.rb
+++ Formula/ignition-blueprint.rb
@@ -4,8 +4,9 @@ class IgnitionBlueprint < Formula
   desc "Collection of gazebo simulation software"
   homepage "https://github.com/ignitionrobotics/ign-blueprint"
   url "https://osrf-distributions.s3.amazonaws.com/ign-blueprint/releases/ignition-blueprint-1.0.0.tar.bz2"
-  sha256 "a55860fa37bfb0c357ca86aaa31cd5de42e5f8f9022bced3e827808785e83041"
-  revision 4
+  url "https://github.com/ignitionrobotics/ign-blueprint/archive/a3f44f4ab45633e807b1a0402d6952b95c6b2d6f.tar.gz"
+  version "1.0.1~0~20201207~a3f44f"
+  sha256 "190fe8c427adfe300da62b772ef9fe1bf118f38acf81efd6cdb7672bbc67c304"
 
   head "https://github.com/ignitionrobotics/ign-blueprint", branch: "main"
 
