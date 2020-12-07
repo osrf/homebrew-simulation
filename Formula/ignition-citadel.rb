ced8ff62f7db9c92defdbc4f9c9e8059bf07a205
Steve Peters <scpeters@openrobotics.org>
2020-12-07 00:38:36 -0800

    ignition-citadel: test with vcs validate
    also bump revision

--- Formula/ignition-citadel.rb
+++ Formula/ignition-citadel.rb
@@ -1,9 +1,12 @@
 class IgnitionCitadel < Formula
+  include Language::Python::Virtualenv
+
   desc "Collection of gazebo simulation software"
   homepage "https://github.com/ignitionrobotics/ign-citadel"
   url "https://osrf-distributions.s3.amazonaws.com/ign-citadel/releases/ignition-citadel-1.0.1.tar.bz2"
   sha256 "1436ebb1b2497abb8f75599f2d8f2b79ec4c29b1d4d4ae6a264cae2f066e5702"
   license "Apache-2.0"
+  revision 1
   version_scheme 1
 
   head "https://github.com/ignitionrobotics/ign-citadel", branch: "main"
@@ -31,7 +34,13 @@ class IgnitionCitadel < Formula
   depends_on "ignition-transport8"
   depends_on macos: :mojave # c++17
   depends_on "pkg-config"
+  depends_on "python@3.9"
   depends_on "sdformat9"
+
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/64/c2/b80047c7ac2478f9501676c988a5411ed5572f35d1beff9cae07d321512c/PyYAML-5.3.1.tar.gz"
+    sha256 "b8eac752c5e14d3eca0e6dd9199cd627518cb5ec06add0de9d32baeee6fe645d"
+  end
 
   def install
     ENV.m64
@@ -40,11 +49,14 @@ class IgnitionCitadel < Formula
       system "cmake", "..", *std_cmake_args
       system "make", "install"
     end
+
+    venv = virtualenv_create(libexec, Formula["python@3.9"].opt_bin/"python3")
+    %w{PyYAML, vcstool}.each do |pkg|
+      venv.pip_install pkg
+    end
   end
 
-  # Failing test in Mojave
-  # test do
-  # TODO: improve the testing
-  #  system "#{bin}/ign", "gazebo", "--help"
-  # end
+  test do
+    system libexec/"bin/vcs", "validate", "--input", share/"ignition/ignition-citadel/gazebodistro/collection-citadel.yaml"
+  end
 end
