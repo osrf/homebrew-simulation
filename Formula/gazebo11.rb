6b4e47b94fdcb92987f5560ca1b4decac82208a4
Steve Peters <scpeters@openrobotics.org>
2021-01-03 22:14:53 -0800

    revert depend on qt5

--- Formula/gazebo11.rb
+++ Formula/gazebo11.rb
@@ -25,7 +25,7 @@ class Gazebo11 < Formula
   depends_on "ossp-uuid" => :linked
   depends_on "protobuf"
   depends_on "protobuf-c"
-  depends_on "qt5"
+  depends_on "qt"
   depends_on "qwt"
   depends_on "sdformat9"
   depends_on "tbb"
