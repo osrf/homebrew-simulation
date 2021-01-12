fdbf8cccc0d782dec5dbe3463fb82c4aa67b9805
Louise Poubel <louise@openrobotics.org>
2021-01-12 15:04:11 -0800

    Bump in edifice: ign-edifice
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- Formula/ignition-edifice.rb
+++ Formula/ignition-edifice.rb
@@ -3,19 +3,13 @@ class IgnitionEdifice < Formula
 
   desc "Collection of gazebo simulation software"
   homepage "https://github.com/ignitionrobotics/ign-edifice"
-  url "https://github.com/ignitionrobotics/ign-edifice/archive/be4570f0b4757445ff77496cb30d4ceb940e8baf.tar.gz"
-  version "0.999.999~0~20210111~be4570"
-  sha256 "2da4c054c562fb3703ada530dc04fb92705a73df30e2d2c39d4dd5e01e8800e9"
+  url "https://github.com/ignitionrobotics/ign-edifice/archive/7a87f6d356962a363bbe5d8e64565a283ca9e9a1.tar.gz"
+  version "0.999.999~0~20210112~7a87f6"
+  sha256 "91bb623765123f41082dbd75d4777b84f32974f11c88d9c1aa98e1ac7f060d6f"
   license "Apache-2.0"
   version_scheme 1
 
   head "https://github.com/ignitionrobotics/ign-edifice", branch: "main"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    cellar :any
-    sha256 "2ac65f9a60fcf5a284f1d4fceae0dc1217eb80eb5922c30a9e5a61dd01f3e94a" => :mojave
-  end
 
   depends_on "cmake" => :build
   depends_on "ignition-cmake2"
@@ -26,7 +20,7 @@ class IgnitionEdifice < Formula
   depends_on "ignition-launch4"
   depends_on "ignition-math6"
   depends_on "ignition-msgs7"
-  depends_on "ignition-physics3"
+  depends_on "ignition-physics4"
   depends_on "ignition-plugin1"
   depends_on "ignition-rendering5"
   depends_on "ignition-sensors5"
@@ -34,7 +28,7 @@ class IgnitionEdifice < Formula
   depends_on "ignition-transport10"
   depends_on macos: :mojave # c++17
   depends_on "pkg-config"
-  depends_on "sdformat10"
+  depends_on "sdformat11"
 
   resource "PyYAML" do
     url "https://files.pythonhosted.org/packages/64/c2/b80047c7ac2478f9501676c988a5411ed5572f35d1beff9cae07d321512c/PyYAML-5.3.1.tar.gz"
