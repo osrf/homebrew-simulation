b6e082cdc0f3a802965e97e90869461989870334
Louise Poubel <louise@openrobotics.org>
2021-01-12 17:50:33 -0800

    sdformat11: new formula (#1283)
    * update  bottle.
    
    Signed-off-by: Louise Poubel <louise@openrobotics.org>
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>
    Co-authored-by: OSRF Build Bot <osrfbuild@osrfoundation.org>

---
+++ Formula/sdformat11.rb
@@ -0,0 +1,73 @@
+class Sdformat11 < Formula
+  desc "Simulation Description Format"
+  homepage "http://sdformat.org"
+  url "https://github.com/osrf/sdformat/archive/ed198a063cfabd5be3b0d8de4b3787eea7a1f68a.tar.gz"
+  version "10.999.999~0~20210112~ed198a"
+  sha256 "e138bb92521b3e1b7d547d665d3ae16b68029cbf5a70f2a7219334875a97cad0"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 "3dd2fd033372b4c6ec00c9304a70bcacdb59f445b21e8c408b9da0de4ab21b12" => :mojave
+  end
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "pkg-config" => [:build, :test]
+
+  depends_on "doxygen"
+  depends_on "ignition-math6"
+  depends_on macos: :mojave # c++17
+  depends_on "tinyxml2"
+  depends_on "urdfdom"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <iostream>
+      #include "sdf/sdf.hh"
+      const std::string sdfString(
+        "<sdf version='1.5'>"
+        "  <model name='example'>"
+        "    <link name='link'>"
+        "      <sensor type='gps' name='mysensor' />"
+        "    </link>"
+        "  </model>"
+        "</sdf>");
+      int main() {
+        sdf::SDF modelSDF;
+        modelSDF.SetFromString(sdfString);
+        std::cout << modelSDF.ToString() << std::endl;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
+      find_package(sdformat11 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ${SDFormat_LIBRARIES})
+    EOS
+    system "pkg-config", "sdformat11"
+    cflags = `pkg-config --cflags sdformat11`.split
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lsdformat11",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
