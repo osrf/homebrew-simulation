6c67c746d9183e75a1b50003b724613c3c61df37
Louise Poubel <louise@openrobotics.org>
2021-01-20 16:44:49 -0800

    ignition-transport10: revision bump (#1294)
    Signed-off-by: Louise Poubel <louise@openrobotics.org>
    
    * update  bottle.
    
    Co-authored-by: OSRF Build Bot <osrfbuild@osrfoundation.org>

--- Formula/ignition-transport10.rb
+++ Formula/ignition-transport10.rb
@@ -1,9 +1,9 @@
 class IgnitionTransport10 < Formula
   desc "Transport middleware for robotics"
   homepage "https://ignitionrobotics.org"
-  url "https://github.com/ignitionrobotics/ign-transport/archive/705c29446af147bec411763d3d2bb7d347b570d7.tar.gz"
-  version "9.999.999~0~20210111~705c29"
-  sha256 "d3c811f36919b0505396cf2573844afe8c8fae824d3d8380c3fc79895eb1eb76"
+  url "https://github.com/ignitionrobotics/ign-transport/archive/4a4bc19c399248d4685c7ed964db5582973d6f1c.tar.gz"
+  version "9.999.999~0~20210120~4a4bc1"
+  sha256 "ec7e7e34224f43b69c5354e2ef6601f12f687825e19cb642783fdf90122e275a"
   license "Apache-2.0"
   version_scheme 1
 
@@ -11,7 +11,7 @@ class IgnitionTransport10 < Formula
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 "19f69cf2f4877b2165360c204c1927f5eaeec73e995658b31379cfd1f44e6bf9" => :mojave
+    sha256 "4f5755f629417941df79099edc268149aeef5b1238f96eb3ca92a658bba0d542" => :mojave
   end
 
   depends_on "doxygen" => [:build, :optional]
