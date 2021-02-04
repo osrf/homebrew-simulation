dfa24d159be0588ed31c67263395d1309650c91a
osrfbuild <osrfbuild@osrfoundation.org>
2021-02-04 22:30:36 +0100

    ignition-rendering4 4.3.1 (#1322)
    * update  bottle.
    
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/ignition-rendering4.rb
+++ Formula/ignition-rendering4.rb
@@ -1,14 +1,14 @@
 class IgnitionRendering4 < Formula
   desc "Rendering library for robotics applications"
   homepage "https://github.com/ignitionrobotics/ign-rendering"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-rendering/releases/ignition-rendering4-4.3.0.tar.bz2"
-  sha256 "0b43af53e5c4257a16c2c003797c5285a60df380029bad65845253168a7146ae"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-rendering/releases/ignition-rendering4-4.3.1.tar.bz2"
+  sha256 "0257a219bf6110ef2c07b35cd506c5243a848a1d902b8b7dd649c3d977b8af60"
   license "Apache-2.0"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 catalina: "08550cc7a98ab14914b481b8518a326063a3a3885489585dddd556895c9bce04"
-    sha256 mojave:   "acb1b110276d0634d96caa6251f9109719f7a22aa525dcf1834e5cc730211daf"
+    sha256 catalina: "1b823892a4292ec8cca8e2e57279c6bcc2d5865bd70a777d005dcb24e33fbfe7"
+    sha256 mojave:   "0dc27c4d2d16dd3ead24ae562a425c9931d389963f50ddcbe7599d390ba4a5a6"
   end
 
   depends_on "cmake" => [:build, :test]
