6a172a798d459efe0344e0c89490bbbf370b085d
Steve Peters <scpeters@openrobotics.org>
2021-03-02 21:17:26 -0800

    ignition-transport8: revision bump for protobuf

--- Formula/ignition-transport8.rb
+++ Formula/ignition-transport8.rb
@@ -4,6 +4,7 @@ class IgnitionTransport8 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-transport/releases/ignition-transport8-8.2.0.tar.bz2"
   sha256 "cff732a797ea08fdb4834331bea14ae50d1cbdd63efae613a475bc42f32e29b3"
   license "Apache-2.0"
+  revision 1
 
   depends_on "doxygen" => [:build, :optional]
   depends_on "protobuf-c" => :build
