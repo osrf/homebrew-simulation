fa82c8045fb6395fdb231fc8d9aa8a25feab11ad
Steve Peters <scpeters@openrobotics.org>
2021-03-05 09:39:15 -0800

    gazebo9: revision bump for qt@5

--- Formula/gazebo9.rb
+++ Formula/gazebo9.rb
@@ -4,7 +4,7 @@ class Gazebo9 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gazebo/releases/gazebo-9.16.0.tar.bz2"
   sha256 "8cf2805f254a32896002d4b6a24578c9dc1c5628123a5c30b7c6cb443461d15b"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/osrf/gazebo.git", branch: "gazebo9"
 
