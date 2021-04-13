284b063fa09bc96e033f94836a334da4dc0af275
Louise Poubel <louise@openrobotics.org>
2021-04-13 16:35:50 -0700

    Bumps in fortress : ign-gazebo
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

---
+++ Formula/ignition-gazebo6.rb
@@ -0,0 +1,108 @@
+class IgnitionGazebo6 < Formula
+  desc "Ignition Gazebo robot simulator"
+  homepage "https://github.com/ignitionrobotics/ign-gazebo"
+  url "https://github.com/ignitionrobotics/ign-gazebo/archive/5426f212ecb01b22980aaade0a02f5ad9d7f426d.tar.gz"
+  version "5.999.999~0~20210413~5426f2"
+  sha256 "07c24d823dd37afaa225bc6d56ce33f43b1811e85f836a71d2b8b9c7527e3427"
+  license "Apache-2.0"
+
+  head "https://github.com/ignitionrobotics/ign-gazebo.git", branch: "main"
+
+  depends_on "cmake" => :build
+  depends_on "gflags"
+  depends_on "google-benchmark"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common4"
+  depends_on "ignition-fuel-tools7"
+  depends_on "ignition-gui6"
+  depends_on "ignition-math6"
+  depends_on "ignition-msgs8"
+  depends_on "ignition-physics5"
+  depends_on "ignition-plugin1"
+  depends_on "ignition-rendering6"
+  depends_on "ignition-sensors6"
+  depends_on "ignition-tools"
+  depends_on "ignition-transport11"
+  depends_on "ignition-utils1"
+  depends_on macos: :mojave # c++17
+  depends_on "pkg-config"
+  depends_on "ruby"
+  depends_on "sdformat12"
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_TESTING=OFF"
+
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    ENV["IGN_CONFIG_PATH"] = "#{opt_share}/ignition"
+    system Formula["ruby"].opt_bin/"ruby",
+           Formula["ignition-tools"].opt_bin/"ign",
+           "gazebo", "-s", "--iterations", "5", "-r", "-v", "4"
+    (testpath/"test.cpp").write <<-EOS
+    #include <cstdint>
+    #include <ignition/gazebo/Entity.hh>
+    #include <ignition/gazebo/EntityComponentManager.hh>
+    #include <ignition/gazebo/components/World.hh>
+    #include <ignition/gazebo/components/Name.hh>
+
+    using namespace ignition;
+    using namespace gazebo;
+
+    //////////////////////////////////////////////////
+    int main(int argc, char **argv)
+    {
+      // Warm start. Initial allocation of resources can throw off calculations.
+      {
+        // Create the entity component manager
+        EntityComponentManager mgr;
+
+        // Create the matching entities
+        for (int i = 0; i < 100; ++i)
+        {
+          Entity entity = mgr.CreateEntity();
+          mgr.CreateComponent(entity, components::World());
+          mgr.CreateComponent(entity, components::Name("world_name"));
+        }
+
+        mgr.Each<components::World, components::Name>(
+            [&](const Entity &, const components::World *,
+              const components::Name *)->bool {return true;});
+      }
+
+      return 0;
+    }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
+      find_package(ignition-gazebo6 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-gazebo6::core)
+    EOS
+    # ENV.append_path "PKG_CONFIG_PATH", Formula["qt@5"].opt_lib/"pkgconfig"
+    # system "pkg-config", "--cflags", "ignition-gazebo6"
+    # cflags   = `pkg-config --cflags ignition-gazebo6`.split
+    # ldflags  = `pkg-config --libs ignition-gazebo6`.split
+    # system ENV.cc, "test.cpp",
+    #                *cflags,
+    #                *ldflags,
+    #                "-lc++",
+    #                "-o", "test"
+    # system "./test"
+    # test building with cmake
+    ENV.append_path "CMAKE_PREFIX_PATH", Formula["qt@5"].opt_prefix
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
