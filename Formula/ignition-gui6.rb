a8b29586697ed6e20f250325834701cf87baa166
Louise Poubel <louise@openrobotics.org>
2021-04-13 16:37:07 -0700

    Bumps in fortress : ign-gui
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

---
+++ Formula/ignition-gui6.rb
@@ -0,0 +1,103 @@
+class IgnitionGui6 < Formula
+  desc "Common libraries for robotics applications. GUI Library"
+  homepage "https://github.com/ignitionrobotics/ign-gui"
+  url "https://github.com/ignitionrobotics/ign-gui/archive/6c761b874d613f0a8a80018e20d18db88a0973f0.tar.gz"
+  version "5.999.999~0~20210413~6c761b"
+  sha256 "f0a6bed89c8a64590569369e67061dbdf4cf80d6d4b8ebb8a680ab12bda79042"
+  license "Apache-2.0"
+
+  head "https://github.com/ignitionrobotics/ign-gui.git", branch: "main"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common4"
+  depends_on "ignition-msgs8"
+  depends_on "ignition-plugin1"
+  depends_on "ignition-rendering6"
+  depends_on "ignition-transport11"
+  depends_on macos: :mojave # c++17
+  depends_on "qt@5"
+  depends_on "qwt"
+  depends_on "tinyxml2"
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DQWT_WIN_INCLUDE_DIR=#{HOMEBREW_PREFIX}/lib/qwt.framework/Headers"
+    cmake_args << "-DQWT_WIN_LIBRARY_DIR=#{HOMEBREW_PREFIX}/lib/qwt.framework"
+    cmake_args << "-DBUILD_TESTING=Off"
+
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+    #include <iostream>
+
+    #ifndef Q_MOC_RUN
+      #include <ignition/gui/qt.h>
+      #include <ignition/gui/Application.hh>
+      #include <ignition/gui/MainWindow.hh>
+    #endif
+
+    //////////////////////////////////////////////////
+    int main(int _argc, char **_argv)
+    {
+      std::cout << "Hello, GUI!" << std::endl;
+
+      // Increase verboosity so we see all messages
+      ignition::common::Console::SetVerbosity(4);
+
+      // Create app
+      ignition::gui::Application app(_argc, _argv);
+
+      // Load plugins / config
+      if (!app.LoadPlugin("Publisher"))
+      {
+        return 1;
+      }
+
+      // Customize main window
+      auto win = app.findChild<ignition::gui::MainWindow *>()->QuickWindow();
+      win->setProperty("title", "Hello Window!");
+
+      // Run window
+      // app.exec();
+
+      std::cout << "After run" << std::endl;
+
+      return 0;
+    }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
+      find_package(ignition-gui6 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-gui6::ignition-gui6)
+    EOS
+    ENV.append_path "PKG_CONFIG_PATH", Formula["qt@5"].opt_lib/"pkgconfig"
+    system "pkg-config", "ignition-gui6"
+    cflags   = `pkg-config --cflags ignition-gui6`.split
+    ldflags  = `pkg-config --libs ignition-gui6`.split
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   *ldflags,
+                   "-lc++",
+                   "-o", "test"
+    ENV["IGN_PARTITION"] = rand((1 << 32) - 1).to_s
+    system "./test"
+    # test building with cmake
+    ENV.append_path "CMAKE_PREFIX_PATH", Formula["qt@5"].opt_prefix
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
