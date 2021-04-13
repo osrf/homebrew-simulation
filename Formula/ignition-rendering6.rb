b6aa4706c4df73f21f57bf7b9e4a845fd0fc90f6
Louise Poubel <louise@openrobotics.org>
2021-04-13 16:31:43 -0700

    Bumps in fortress : ign-rendering
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

---
+++ Formula/ignition-rendering6.rb
@@ -0,0 +1,66 @@
+class IgnitionRendering6 < Formula
+  desc "Rendering library for robotics applications"
+  homepage "https://github.com/ignitionrobotics/ign-rendering"
+  url "https://github.com/ignitionrobotics/ign-rendering/archive/e81c97476bd9e45ef496efaabad6e381472c846e.tar.gz"
+  version "5.999.999~0~20210413~e81c97"
+  sha256 "d1039ed57c0b7a5490aa69837cc92aeb765279bf289f42e3e6fd42a81e5d546b"
+  license "Apache-2.0"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "pkg-config" => [:build, :test]
+
+  depends_on "freeimage"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common4"
+  depends_on "ignition-math6"
+  depends_on "ignition-plugin1"
+  depends_on macos: :mojave # c++17
+  depends_on "ogre1.9"
+  depends_on "ogre2.1"
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_TESTING=Off"
+    system "cmake", ".", *cmake_args
+    system "make", "install"
+  end
+
+  test do
+    github_actions = ENV["HOMEBREW_GITHUB_ACTIONS"].present?
+    (testpath/"test.cpp").write <<-EOS
+      #include <ignition/rendering/RenderEngine.hh>
+      #include <ignition/rendering/RenderingIface.hh>
+      int main(int _argc, char** _argv)
+      {
+        ignition::rendering::RenderEngine *engine =
+            ignition::rendering::engine("ogre");
+        return engine == nullptr;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.10.2 FATAL_ERROR)
+      find_package(ignition-rendering6 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-rendering6::ignition-rendering6)
+    EOS
+    # test building with pkg-config
+    system "pkg-config", "ignition-rendering6"
+    cflags   = `pkg-config --cflags ignition-rendering6`.split
+    ldflags  = `pkg-config --libs ignition-rendering6`.split
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   *ldflags,
+                   "-lc++",
+                   "-o", "test"
+    system "./test" unless github_actions
+    # test building with cmake
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake" unless github_actions
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
