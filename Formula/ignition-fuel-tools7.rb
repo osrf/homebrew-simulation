15c6578f84eb1832df06ae7c3d0a039404d7901a
Louise Poubel <louise@openrobotics.org>
2021-04-16 11:54:19 -0700

    Bumps in fortress : ign-fuel-tools (#1426)
    * update  bottle.
    
    Co-authored-by: OSRF Build Bot <osrfbuild@osrfoundation.org>

---
+++ Formula/ignition-fuel-tools7.rb
@@ -0,0 +1,73 @@
+class IgnitionFuelTools7 < Formula
+  desc "Tools for using Fuel API to download robot models"
+  homepage "https://ignitionrobotics.org"
+  url "https://github.com/ignitionrobotics/ign-fuel-tools/archive/59ca464418359b1867f7aae948ef38c78ab0810a.tar.gz"
+  version "6.999.999~0~20210413~59ca46"
+  sha256 "e1c2fa052b2afc79b654b51cf1a4995a47b18ec3e18f2102ae9341f4f48ec328"
+  license "Apache-2.0"
+
+  head "https://github.com/ignitionrobotics/ign-fuel-tools.git", branch: "main"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 cellar: :any, catalina: "cd10d8337bcb723d38df73fcd25fc25027b0f6144a7a0163df6222b50f1e0539"
+    sha256 cellar: :any, mojave:   "246f09fd6613700ee69e566d301694e951c082bc04c39590e98101cf6f008e53"
+  end
+
+  depends_on "cmake"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common4"
+  depends_on "ignition-msgs8"
+  depends_on "jsoncpp"
+  depends_on "libyaml"
+  depends_on "libzip"
+  depends_on macos: :high_sierra # c++17
+  depends_on "pkg-config"
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_TESTING=Off"
+
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <ignition/fuel_tools.hh>
+      int main() {
+        ignition::fuel_tools::ServerConfig srv;
+        return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 2.8 FATAL_ERROR)
+      find_package(ignition-fuel_tools7 QUIET REQUIRED)
+      include_directories(${IGNITION-FUEL_TOOLS_INCLUDE_DIRS})
+      link_directories(${IGNITION-FUEL_TOOLS_LIBRARY_DIRS})
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-fuel_tools7::ignition-fuel_tools7)
+    EOS
+    # test building with pkg-config
+    system "pkg-config", "ignition-fuel_tools7"
+    cflags = `pkg-config --cflags ignition-fuel_tools7`.split
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lignition-fuel_tools7",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
