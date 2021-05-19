4a61c35d961795945cab5c8c50ab3f1bbfa005c5
Steve Peters <scpeters@openrobotics.org>
2021-05-18 20:00:46 -0700

    ignition-msgs6: revision bump for protobuf

--- Formula/ignition-msgs6.rb
+++ Formula/ignition-msgs6.rb
@@ -4,6 +4,7 @@ class IgnitionMsgs6 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-msgs/releases/ignition-msgs6-6.5.0.tar.bz2"
   sha256 "8d04cdd95ff7daf4513abe02c5093b1176be7512b972258ae7e56995769a2968"
   license "Apache-2.0"
+  revision 1
 
   head "https://github.com/ignitionrobotics/ign-msgs.git", branch: "ign-msgs6"
 
