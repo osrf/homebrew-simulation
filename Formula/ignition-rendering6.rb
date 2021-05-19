928e61253d6bdf7bb9cd5fb856885372801fecd8
Steve Peters <scpeters@openrobotics.org>
2021-05-18 17:18:11 -0700

    ignition-rendering6: build main branch from source (#1462)
    * update  bottle.
    
    Co-authored-by: OSRF Build Bot <osrfbuild@osrfoundation.org>

--- Formula/ignition-rendering6.rb
+++ Formula/ignition-rendering6.rb
@@ -1,15 +1,14 @@
 class IgnitionRendering6 < Formula
   desc "Rendering library for robotics applications"
   homepage "https://github.com/ignitionrobotics/ign-rendering"
-  url "https://github.com/ignitionrobotics/ign-rendering/archive/e81c97476bd9e45ef496efaabad6e381472c846e.tar.gz"
-  version "5.999.999~0~20210413~e81c97"
-  sha256 "d1039ed57c0b7a5490aa69837cc92aeb765279bf289f42e3e6fd42a81e5d546b"
+  url "https://github.com/ignitionrobotics/ign-rendering.git", branch: "main"
+  version "5.999.999~1"
   license "Apache-2.0"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 catalina: "0252ed02501e2a2cee5f956526741d1625660b7a702d61d71c25b931108edae0"
-    sha256 mojave:   "8fefacd00bf071de174b4747d89e25efff8955a3f1ff7250bec37d7adc7ed35c"
+    sha256 catalina: "329c73bbe4839e08362321338cc3924da33c88f499ab01b9f66072adbc94d607"
+    sha256 mojave:   "75ae38c3cb9050775c2b236005ad93b05753c8b03ae7d889ed5c815acc1b5fcc"
   end
 
   depends_on "cmake" => [:build, :test]
