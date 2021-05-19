e83d778fb7c79064d21b900014ca087c6014a8c3
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-05-19 16:33:56 +0200

    ignition-gui4: revision bump

--- Formula/ignition-gui4.rb
+++ Formula/ignition-gui4.rb
@@ -4,6 +4,7 @@ class IgnitionGui4 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-gui/releases/ignition-gui4-4.3.0.tar.bz2"
   sha256 "744a8a5f8bbf8bfcff2630ab001ac54b856dd4baec6783a98d0b5a015f770773"
   license "Apache-2.0"
+  revision 1
 
   head "https://github.com/ignitionrobotics/ign-gui.git", branch: "ign-gui4"
 
