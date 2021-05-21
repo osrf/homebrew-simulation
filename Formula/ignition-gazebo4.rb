05161e72db2413bca4aa4b0f064b7dd34d0fe67b
osrfbuild <osrfbuild@osrfoundation.org>
2021-05-21 04:30:45 +0200

    ignition-gazebo4 4.9.0 (#1473)
    * update  bottle.
    
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/ignition-gazebo4.rb
+++ Formula/ignition-gazebo4.rb
@@ -1,11 +1,17 @@
 class IgnitionGazebo4 < Formula
   desc "Ignition Gazebo robot simulator"
   homepage "https://github.com/ignitionrobotics/ign-gazebo"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-gazebo/releases/ignition-gazebo4-4.8.0.tar.bz2"
-  sha256 "d422fd26aa9da1a796e7f9e2bf28565826e81a7a08f337af142d6f16918109ea"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-gazebo/releases/ignition-gazebo4-4.9.0.tar.bz2"
+  sha256 "a24ad6c0000c03696ad647b4a2f2799610199b6807b2fd9dc4274d5a953d3ce6"
   license "Apache-2.0"
 
   head "https://github.com/ignitionrobotics/ign-gazebo.git", branch: "ign-gazebo4"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 catalina: "cd4e623cb2af1659b1b5a540d4ee66e73bbedc63690c7f89dc9c90078484b32e"
+    sha256 mojave:   "a4679b28d31265eab0aa44f59e1c2e0488fbd85eb77fd84f6255432144da08a2"
+  end
 
   depends_on "cmake" => :build
   depends_on "gflags"
