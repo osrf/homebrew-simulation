7e1d9c518c4eb7884cfb27653f4aaf386765dc0a
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-05-21 14:38:04 +0200

    ignition-launch2: revision bump

--- Formula/ignition-launch2.rb
+++ Formula/ignition-launch2.rb
@@ -4,7 +4,7 @@ class IgnitionLaunch2 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-launch/releases/ignition-launch2-2.2.1.tar.bz2"
   sha256 "a44f3d874acd2e91323a48eba817fafe9a4c5f04860b4543677241bbbe5041dc"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/ignitionrobotics/ign-launch.git", branch: "ign-launch2"
 
