949db5184dffba3ed380e2dc670f7d972dada1f1
osrfbuild <osrfbuild@osrfoundation.org>
2021-07-19 19:19:28 +0200

    ignition-launch3 3.4.1 (#1538)
    * update  bottle.

--- Formula/ignition-launch3.rb
+++ Formula/ignition-launch3.rb
@@ -1,16 +1,16 @@
 class IgnitionLaunch3 < Formula
   desc "Launch libraries for robotics applications"
   homepage "https://github.com/ignitionrobotics/ign-launch"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-launch/releases/ignition-launch3-3.4.0.tar.bz2"
-  sha256 "6db15e028bf694f9456ee95cce9671d02778c91c99b1bbdef85adc96d2a9d197"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-launch/releases/ignition-launch3-3.4.1.tar.bz2"
+  sha256 "6f659eaa1b651b0a7f04d5805ef07c7f1035ca13d0248b9590a255b099b6de48"
   license "Apache-2.0"
 
   head "https://github.com/ignitionrobotics/ign-launch.git", branch: "ign-launch3"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 catalina: "08e2f132ad51c22216ac4b704aa718ee4065bd123aa039e735e9653472ed3172"
-    sha256 mojave:   "3e0ff3f13eca33c9e3a36553efed8edd3e7da5263f1650e74509437f95c8a2e3"
+    sha256 catalina: "1c214342be4970d64a36d061beccf09f3738fa7f0523cbd2d3fb8f9163fa4bf0"
+    sha256 mojave:   "2ad6e74813a1df1ed60e55335ace450c2e5409439b6f74a4402a339970c49d9c"
   end
 
   depends_on "cmake" => :build
