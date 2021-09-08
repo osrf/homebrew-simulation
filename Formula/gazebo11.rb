87714b137494870ee7edc2f1dc584d82fcc3db77
osrfbuild <osrfbuild@osrfoundation.org>
2021-09-08 14:16:33 +0200

    sdformat9 9.6.1 (#1563)
    * gazebo11: remove optional dependencies
    * update  bottle.
    
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/gazebo11.rb
+++ Formula/gazebo11.rb
@@ -4,23 +4,27 @@ class Gazebo11 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gazebo/releases/gazebo-11.8.1.tar.bz2"
   sha256 "162163f640ccaed319ffd0adb1d766fc7190259f1f3ce507f7d4d86794c87a35"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/osrf/gazebo.git", branch: "gazebo11"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 catalina: "4c86dadf5dac45b4da3ba9fcd42d5c98f975aa43a2d784f14cac3715c704255b"
-    sha256 mojave:   "fcd7c4827629736f538fe4cb75abdc14d9960818e15f04c232b6ddd22d88310b"
+    sha256 catalina: "811691a4d5f61cec0c04d4215dae5ecbee8a609a95ddc743d3f4bf39f76b5153"
+    sha256 mojave:   "f684ac2cc1a04b5f42c7d41c1614d81693bd6cd0e6435e437b618e46520d7944"
   end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
 
   depends_on "boost"
+  depends_on "bullet"
+  depends_on "dartsim"
   depends_on "doxygen"
+  depends_on "ffmpeg"
   depends_on "freeimage"
   depends_on "graphviz"
+  depends_on "gts"
   depends_on "ignition-common3"
   depends_on "ignition-fuel-tools4"
   depends_on "ignition-math6"
@@ -34,18 +38,14 @@ class Gazebo11 < Formula
   depends_on "qt@5"
   depends_on "qwt-qt5"
   depends_on "sdformat9"
+  depends_on "simbody"
   depends_on "tbb@2020_u3"
   depends_on "tinyxml"
   depends_on "tinyxml2"
   depends_on "zeromq" => :linked
 
-  depends_on "bullet" => :recommended
-  depends_on "dartsim" => :recommended
-  depends_on "ffmpeg" => :recommended
-  depends_on "gts" => :recommended
-  depends_on "simbody" => :recommended
-  depends_on "gdal" => :optional
-  depends_on "player" => :optional
+  # depends on "gdal" => :optional
+  # depends on "player" => :optional
 
   conflicts_with "gazebo7", because: "differing version of the same formula"
   conflicts_with "gazebo9", because: "differing version of the same formula"
