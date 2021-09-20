652f394054d82a715774425f511d17d6fe752e5f
osrfbuild <osrfbuild@osrfoundation.org>
2021-09-20 23:22:00 +0200

    sdformat12 12.0.0~pre1 (#1584)
    * sdformat12: update 12.0.0~pre1
    
    * update  bottle.

--- Formula/sdformat12.rb
+++ Formula/sdformat12.rb
@@ -1,10 +1,16 @@
 class Sdformat12 < Formula
   desc "Simulation Description Format"
   homepage "http://sdformat.org"
-  url "https://github.com/osrf/sdformat.git", branch: "main"
-  version "11.999.999~1"
+  url "https://osrf-distributions.s3.amazonaws.com/sdformat/releases/sdformat-12.0.0~pre1.tar.bz2"
+  version "12.0.0~pre1"
+  sha256 "b6185fb12348f5e021230b320fca2d11a5a971273ba33dde5213bba7a427b5be"
   license "Apache-2.0"
-  revision 1
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 catalina: "20c8099e6df46089d32102adaa30436a871e0a8998fba0a876d32f3eb0511f99"
+    sha256 mojave:   "9739a6521adc01ea8884474cfcc97e699e573487b8d1fa9853aa96dc9cfffb31"
+  end
 
   depends_on "cmake" => [:build, :test]
   depends_on "pkg-config" => [:build, :test]
