5cfea42f05e50e1cdada593f100506b8fabe9e7b
Steve Peters <scpeters@openrobotics.org>
2021-10-14 02:31:07 -0700

    ignition-gui5: rebuild 5.3.0 (#1631)
    * update  bottle.
    
    Co-authored-by: OSRF Build Bot <osrfbuild@osrfoundation.org>

--- Formula/ignition-gui5.rb
+++ Formula/ignition-gui5.rb
@@ -4,8 +4,15 @@ class IgnitionGui5 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-gui/releases/ignition-gui5-5.3.0.tar.bz2"
   sha256 "759cbacf205af2e78f79902952f1a48ed46891534f788c425cf3a0f028479061"
   license "Apache-2.0"
+  revision 1
 
   head "https://github.com/ignitionrobotics/ign-gui.git", branch: "main"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 big_sur:  "073f1f4ea19c7fa39e8b35a32f47a3177d59abef744bb9b460438dcd23a6d468"
+    sha256 catalina: "08c15a64238b420b0d53cae0b7804adab27ae697b28f6bb83886574669fbd4dc"
+  end
 
   depends_on "cmake" => [:build, :test]
   depends_on "pkg-config" => [:build, :test]
