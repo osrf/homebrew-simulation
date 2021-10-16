259782629af6d6b75ffe6d1c5fd3d156f3108937
osrfbuild <osrfbuild@osrfoundation.org>
2021-10-16 09:52:31 +0200

    ignition-tools 1.4.0 (#1637)
    * update  bottle.
    
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/ignition-tools.rb
+++ Formula/ignition-tools.rb
@@ -1,15 +1,15 @@
 class IgnitionTools < Formula
   desc "Entry point for ignition command-line tools"
   homepage "https://ignitionrobotics.org"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-tools/releases/ignition-tools-1.3.0.tar.bz2"
-  sha256 "6d8f03ba496e73fc298c4a725d258a35ad79d65cfa044472f79d0d89368b33e0"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-tools/releases/ignition-tools-1.4.0.tar.bz2"
+  sha256 "cc14f0df78991e8867c03c271c025eafd52fa67f633e2af11775aed20fe2be39"
   license "Apache-2.0"
   head "https://github.com/ignitionrobotics/ign-tools.git", branch: "ign-tools1"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any_skip_relocation, big_sur:  "1bf3f18776ffbb7192a3d8f67c52ed4f4bcce6557505006a884dc97fcfef7631"
-    sha256 cellar: :any_skip_relocation, catalina: "20f39c9650ad55df6f62257257c58c97e067c83f10db2ba5ffcf2028e77dc4ca"
+    sha256 cellar: :any, big_sur:  "d7d0cbf2ec0ca969a755d76a1bd517b57ef98ad78a937832d1b207862e5282c7"
+    sha256 cellar: :any, catalina: "296deab03804929c647db6e06b15332217ae0a6b7c9558726c4d58185ab25db9"
   end
 
   depends_on "cmake" => :build
