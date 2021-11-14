4e21ebd44d9fafecff823845db210295eeb6ce82
Steve Peters <scpeters@openrobotics.org>
2021-11-14 15:08:15 -0800

    ignition-cmake0: keep old bottles

--- Formula/ignition-cmake0.rb
+++ Formula/ignition-cmake0.rb
@@ -17,8 +17,12 @@ class IgnitionCmake0 < Formula
   def install
     cmake_args = std_cmake_args
     cmake_args << "-DBUILD_TESTING=Off"
-    system "cmake", ".", *cmake_args
-    system "make", "install"
+    cmake_args << "-DCMAKE_INSTALL_RPATH=#{rpath}"
+
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make", "install"
+    end
   end
 
   test do
