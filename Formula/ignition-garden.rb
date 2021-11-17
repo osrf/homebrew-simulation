6f9e4bc5c656646381a7202775abaa6fc05642a4
Steve Peters <scpeters@openrobotics.org>
2021-11-16 17:30:44 -0800

    Bumps in garden : ign-garden
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/ignition-garden.rb
+++ Formula/ignition-garden.rb
@@ -3,18 +3,11 @@ class IgnitionGarden < Formula
 
   desc "Collection of gazebo simulation software"
   homepage "https://github.com/ignitionrobotics/ign-garden"
-  url "https://github.com/ignitionrobotics/ign-garden/archive/a4732212255ef447f102587a346086fc11c1846c.tar.gz"
-  version "0.999.999~0~20211008~a47322"
-  sha256 "2f0fa319ba90f3840521b2d8544bd4528e3a2cf39c9899638e6ec3b6a6231226"
+  url "https://github.com/ignitionrobotics/ign-garden.git", branch: "main"
+  version "0.999.999~0~20211116"
   license "Apache-2.0"
 
   head "https://github.com/ignitionrobotics/ign-garden.git", branch: "main"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any, big_sur:  "67e6a3f6c421969c0a14070889cf30cf3e3bc8b05733683e04c6b2df5f67d5ca"
-    sha256 cellar: :any, catalina: "d148602e0dbe37b1212997f44420b335c6ab2c044b946727bcecf1e39b7315c5"
-  end
 
   depends_on "cmake" => :build
   depends_on "ignition-cmake2"
@@ -27,7 +20,7 @@ class IgnitionGarden < Formula
   depends_on "ignition-msgs8"
   depends_on "ignition-physics5"
   depends_on "ignition-plugin1"
-  depends_on "ignition-rendering6"
+  depends_on "ignition-rendering7"
   depends_on "ignition-sensors6"
   depends_on "ignition-tools"
   depends_on "ignition-transport11"
