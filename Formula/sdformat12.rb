f5873d79751b75d8b85aa03d530900e363f446c4
Steve Peters <scpeters@openrobotics.org>
2021-12-01 16:17:19 -0800

    Set CMAKE_INSTALL_RPATH

--- Formula/sdformat12.rb
+++ Formula/sdformat12.rb
@@ -23,8 +23,12 @@ class Sdformat12 < Formula
   depends_on "urdfdom"
 
   def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_TESTING=Off"
+    cmake_args << "-DCMAKE_INSTALL_RPATH=#{rpath}"
+
     mkdir "build" do
-      system "cmake", "..", *std_cmake_args
+      system "cmake", "..", *cmake_args
       system "make", "install"
     end
   end
