463c839e11008a852cbdd346db20adcd09696864
Steve Peters <scpeters@openrobotics.org>
2021-12-03 09:21:42 -0800

    Bumps in garden : ci_matching_branch/bump_garden_ign-msgs9
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

---
+++ Formula/ignition-msgs9.rb
@@ -0,0 +1,63 @@
+class IgnitionMsgs9 < Formula
+  desc "Middleware protobuf messages for robotics"
+  homepage "https://github.com/ignitionrobotics/ign-msgs"
+  url "https://github.com/ignitionrobotics/ign-msgs.git", branch: "main"
+  version "8.999.999~0~20211203"
+  license "Apache-2.0"
+
+  depends_on "protobuf-c" => :build
+  depends_on "cmake"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-math6"
+  depends_on "ignition-tools"
+  depends_on macos: :high_sierra # c++17
+  depends_on "pkg-config"
+  depends_on "protobuf"
+  depends_on "tinyxml2"
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_TESTING=Off"
+
+    system "cmake", ".", *cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <ignition/msgs.hh>
+      int main() {
+        ignition::msgs::UInt32;
+        return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.10 FATAL_ERROR)
+      find_package(ignition-msgs9 QUIET REQUIRED)
+      set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${IGNITION-MSGS_CXX_FLAGS}")
+      include_directories(${IGNITION-MSGS_INCLUDE_DIRS})
+      link_directories(${IGNITION-MSGS_LIBRARY_DIRS})
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-msgs9::ignition-msgs9)
+    EOS
+    # test building with pkg-config
+    system "pkg-config", "ignition-msgs9"
+    cflags = `pkg-config --cflags ignition-msgs9`.split
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lignition-msgs9",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
