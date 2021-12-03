b956679e8aebd22d0e2125ad57fb662727099104
Steve Peters <scpeters@openrobotics.org>
2021-12-03 10:57:01 -0800

    Add CMAKE_INSTALL_RPATH.

--- Formula/ignition-fuel-tools8.rb
+++ Formula/ignition-fuel-tools8.rb
@@ -18,6 +18,7 @@ class IgnitionFuelTools8 < Formula
   def install
     cmake_args = std_cmake_args
     cmake_args << "-DBUILD_TESTING=Off"
+    cmake_args << "-DCMAKE_INSTALL_RPATH=#{rpath}"
 
     mkdir "build" do
       system "cmake", "..", *cmake_args
