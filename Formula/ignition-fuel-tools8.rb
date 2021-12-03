7c73c66a237ebf04bb4369cb69d26cf66cd0dac8
Steve Peters <scpeters@openrobotics.org>
2021-12-03 10:28:33 -0800

    Bumps in garden : ci_matching_branch/bump_garden_ign-fuel-tools8
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

---
+++ Formula/ignition-fuel-tools8.rb
@@ -0,0 +1,64 @@
+class IgnitionFuelTools8 < Formula
+  desc "Tools for using Fuel API to download robot models"
+  homepage "https://ignitionrobotics.org"
+  url "https://github.com/ignitionrobotics/ign-fuel-tools.git", branch: "main"
+  version "7.999.999~0~20211203"
+  license "Apache-2.0"
+
+  depends_on "cmake"
+  depends_on "ignition-cmake2"
+  depends_on "ignition-common4"
+  depends_on "ignition-msgs9"
+  depends_on "jsoncpp"
+  depends_on "libyaml"
+  depends_on "libzip"
+  depends_on macos: :high_sierra # c++17
+  depends_on "pkg-config"
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_TESTING=Off"
+
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <ignition/fuel_tools.hh>
+      int main() {
+        ignition::fuel_tools::ServerConfig srv;
+        return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 2.8 FATAL_ERROR)
+      find_package(ignition-fuel_tools7 QUIET REQUIRED)
+      include_directories(${IGNITION-FUEL_TOOLS_INCLUDE_DIRS})
+      link_directories(${IGNITION-FUEL_TOOLS_LIBRARY_DIRS})
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ignition-fuel_tools7::ignition-fuel_tools7)
+    EOS
+    # test building with pkg-config
+    system "pkg-config", "ignition-fuel_tools7"
+    cflags = `pkg-config --cflags ignition-fuel_tools7`.split
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lignition-fuel_tools7",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
