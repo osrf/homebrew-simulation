d5e3831444c7405a7033aa48dd85b4e34415f08b
Steve Peters <scpeters@openrobotics.org>
2021-12-07 22:54:10 -0800

    ignition-physics4: revision bump for bullet

--- Formula/ignition-physics4.rb
+++ Formula/ignition-physics4.rb
@@ -4,6 +4,7 @@ class IgnitionPhysics4 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-physics/releases/ignition-physics4-4.3.0.tar.bz2"
   sha256 "66efb3f4e4f9f10b591200fea52f4d44297c596d26f8a88f951bb645c6fab7c2"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
