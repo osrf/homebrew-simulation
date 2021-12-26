37aff38004908b4afc9bc0ffa8f92b96750da7cc
Steve Peters <scpeters@openrobotics.org>
2021-12-25 20:06:53 -0800

    ignition-gui3: revision bump for protobuf

--- Formula/ignition-gui3.rb
+++ Formula/ignition-gui3.rb
@@ -4,7 +4,7 @@ class IgnitionGui3 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-gui/releases/ignition-gui3-3.8.0.tar.bz2"
   sha256 "a4b36eae36e7f629e4e92f32a8d04dd2b2d31a87a0631cdbecfc6f7fe98f4daa"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/ignitionrobotics/ign-gui.git", branch: "ign-gui3"
 
