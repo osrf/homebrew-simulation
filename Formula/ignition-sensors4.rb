67d356d1b6b89ceccd47090256cfa2ecb3c6df81
Steve Peters <scpeters@openrobotics.org>
2021-12-25 20:04:06 -0800

    ignition-sensors4: revision bump for protobuf

--- Formula/ignition-sensors4.rb
+++ Formula/ignition-sensors4.rb
@@ -4,7 +4,7 @@ class IgnitionSensors4 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-sensors/releases/ignition-sensors4-4.3.0.tar.bz2"
   sha256 "7a8b3fce6a825f3c4e9f3ff551c0147a0d0f064af27b40c9d8a08054c8926e1f"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/ignitionrobotics/ign-sensors.git", branch: "ign-sensors4"
 
