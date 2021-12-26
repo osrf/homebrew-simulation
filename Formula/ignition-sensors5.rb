c0c4adea9171dbd8ce88239ac52bf5843a8322e8
Steve Peters <scpeters@openrobotics.org>
2021-12-26 00:55:16 -0800

    Rebuild ignition-sensors* for protobuf 3.19.1 (#1751)
    * ignition-sensors3: revision bump for protobuf
    * ignition-sensors4: revision bump for protobuf
    * ignition-sensors5: revision bump for protobuf
    * ignition-sensors6: revision bump for protobuf
    * update  bottle.
    
    Co-authored-by: OSRF Build Bot <osrfbuild@osrfoundation.org>

--- Formula/ignition-sensors5.rb
+++ Formula/ignition-sensors5.rb
@@ -4,9 +4,15 @@ class IgnitionSensors5 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-sensors/releases/ignition-sensors5-5.1.0.tar.bz2"
   sha256 "03fc956b4747071ec3d1a9142b37a350c056eaf3caff1cab0abf0bbec1673f91"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/ignitionrobotics/ign-sensors.git", branch: "main"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 big_sur:  "7baaee89cb1ecbd73fd5bc1a2338877e21107e3a98d954ecd6aee2a6f2a2da3e"
+    sha256 catalina: "f222ab32aa3e94f9558f244905c3269858a12b72cfce667edd478b3632f0b6c0"
+  end
 
   depends_on "cmake" => [:build, :test]
   depends_on "pkg-config" => [:build, :test]
--- Formula/ignition-sensors6.rb
+++ Formula/ignition-sensors6.rb
@@ -4,7 +4,13 @@ class IgnitionSensors6 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-sensors/releases/ignition-sensors6-6.0.1.tar.bz2"
   sha256 "d1a5d4a7fdebd82e0763fe76d58590a72f2852e42342c3310e3af8cd09b86cec"
   license "Apache-2.0"
-  revision 1
+  revision 2
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 cellar: :any, big_sur:  "1ddc5103c1152e270ca5feb15b7f899a1f20d3fca862e32e67dcbbb3647112c3"
+    sha256 cellar: :any, catalina: "dc74fb2e98c59ad11a43e503e05cd820febf73d22696acfdc76d5891c0d24bc8"
+  end
 
   depends_on "cmake" => [:build, :test]
   depends_on "pkg-config" => [:build, :test]
