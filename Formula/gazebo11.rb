787eb67abeb348d60a3e2aaff5aa71d5168c651f
osrfbuild <osrfbuild@osrfoundation.org>
2022-01-13 22:00:38 +0100

    gazebo11 11.10.0 (#1779)
    * update  bottle.

--- Formula/gazebo11.rb
+++ Formula/gazebo11.rb
@@ -1,17 +1,16 @@
 class Gazebo11 < Formula
   desc "Gazebo robot simulator"
   homepage "https://gazebosim.org"
-  url "https://osrf-distributions.s3.amazonaws.com/gazebo/releases/gazebo-11.9.1.tar.bz2"
-  sha256 "2959f4bed5fa8b5aa7f86bcd518e74a65402fe21b91a8d125807117827731be6"
+  url "https://osrf-distributions.s3.amazonaws.com/gazebo/releases/gazebo-11.10.0.tar.bz2"
+  sha256 "03acd7c2b8ad96203ba7a974cdf0b148c1ba5f16bca6eca7fd36aa51421d3f86"
   license "Apache-2.0"
-  revision 3
 
   head "https://github.com/osrf/gazebo.git", branch: "gazebo11"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 big_sur:  "84304d8e0a6f6706b159ec368fac9f8b090556dbe732e284187a55c20ccab38c"
-    sha256 catalina: "f563e8b9f2ddcfd7e352af7d4b477db6a943220903f71b47952ffa2d484f349b"
+    sha256 big_sur:  "12fe0748f6744a9fe35833694b4cebc3c4e35fc11381e5daf388164245d2fb68"
+    sha256 catalina: "6133e632d545a705698e9ec475804e93ddafcaf47e83bdb434cbee8e4af85866"
   end
 
   depends_on "cmake" => :build
