cb26e283259323532e28e298896aea8526591852
Steve Peters <scpeters@openrobotics.org>
2022-02-06 00:42:56 -0800

    Fix ign-gazebo tests
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/ignition-gazebo6.rb
+++ Formula/ignition-gazebo6.rb
@@ -94,6 +94,7 @@ class IgnitionGazebo6 < Formula
     #                "-o", "test"
     # system "./test"
     # test building with cmake
+    ENV.append_path "CMAKE_PREFIX_PATH", Formula["ffmpeg@4"].opt_prefix
     ENV.append_path "CMAKE_PREFIX_PATH", Formula["qt@5"].opt_prefix
     mkdir "build" do
       system "cmake", ".."
