aec8d91633102b4306a0c3f49f4a21175d63a600
Steve Peters <scpeters@openrobotics.org>
2022-02-05 22:30:20 -0800

    ignition-gazebo6: revision bump for ffmpeg@4

--- Formula/ignition-gazebo6.rb
+++ Formula/ignition-gazebo6.rb
@@ -4,7 +4,7 @@ class IgnitionGazebo6 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-gazebo/releases/ignition-gazebo6-6.4.0.tar.bz2"
   sha256 "722dadfcfdc09f2e9fb97e71182a9db46ebd8f6f25f45868b830b26c7f0a97cb"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   depends_on "cmake" => :build
   depends_on "gflags"
