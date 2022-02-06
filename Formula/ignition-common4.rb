f25280baba6fc45da45e389068c287bf8332e57e
Steve Peters <scpeters@openrobotics.org>
2022-02-05 22:29:43 -0800

    ignition-common4: revision bump for ffmpeg@4

--- Formula/ignition-common4.rb
+++ Formula/ignition-common4.rb
@@ -4,7 +4,7 @@ class IgnitionCommon4 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-common/releases/ignition-common4-4.5.0.tar.bz2"
   sha256 "d01471b1abdbf5617d1bc1c0842fa9323094170e500f409b435049ec13fb19a2"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   depends_on "cmake"
   depends_on "ffmpeg@4"
