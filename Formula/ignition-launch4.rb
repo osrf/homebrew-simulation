41d4a68734c39418b02e8c21db33e8e24bf842e8
Steve Peters <scpeters@openrobotics.org>
2022-02-05 22:30:25 -0800

    ignition-launch4: revision bump for ffmpeg@4

--- Formula/ignition-launch4.rb
+++ Formula/ignition-launch4.rb
@@ -4,7 +4,7 @@ class IgnitionLaunch4 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-launch/releases/ignition-launch4-4.1.0.tar.bz2"
   sha256 "fd1e5a535bafb197360b168bce573bed3c4d7804b76c098d93afc5b17b2bdcef"
   license "Apache-2.0"
-  revision 3
+  revision 4
 
   head "https://github.com/ignitionrobotics/ign-launch.git", branch: "main"
 
