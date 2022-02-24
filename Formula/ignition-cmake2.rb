94bf1f83e89f741cd527f184579ca8942afa4057
osrfbuild <osrfbuild@osrfoundation.org>
2022-02-24 14:08:27 +0100

    ignition-cmake2 2.11.0 (#1819)
    * ignition-cmake2: update 2.11.0
    
    * update  bottle.

--- Formula/ignition-cmake2.rb
+++ Formula/ignition-cmake2.rb
@@ -1,14 +1,14 @@
 class IgnitionCmake2 < Formula
   desc "CMake helper functions for building robotic applications"
   homepage "https://ignitionrobotics.org"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-cmake/releases/ignition-cmake2-2.10.0.tar.bz2"
-  sha256 "a3bc6c52b8950f927f3d0ca4443945e4e20996daaa7354217177598c65196af0"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-cmake/releases/ignition-cmake2-2.11.0.tar.bz2"
+  sha256 "6058e579f83d44952379205916e39866b7c521709ac6372b4dd5bc0027722814"
   license "Apache-2.0"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any_skip_relocation, big_sur:  "b73afa3bd7679338b513e9087ff48a21e8ced1dec83c20425209f2bce299a6aa"
-    sha256 cellar: :any_skip_relocation, catalina: "4d7731740d3f38079e286cafaafdd5f85eadc16438bf14452c6c9a0ae080304f"
+    sha256 cellar: :any_skip_relocation, big_sur:  "0f496d9cb614cdf92f45abae4c6de63080768e3077fa5534c97d706a6897d9fb"
+    sha256 cellar: :any_skip_relocation, catalina: "fcfdc85333ccefaf6ddc5cbf2fda08412545a72a0fa94b82d48280dc6b828d72"
   end
 
   depends_on "cmake"
