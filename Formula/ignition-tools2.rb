5d289656d84a5fb4bfe3d70c6b08924ffc3ce079
methylDragon <methylDragon@gmail.com>
2022-04-14 14:08:47 -0700

    Bumps in garden : ign-tools2
    Signed-off-by: methylDragon <methylDragon@gmail.com>

--- Formula/ignition-tools2.rb
+++ Formula/ignition-tools2.rb
@@ -1,16 +1,10 @@
 class IgnitionTools2 < Formula
   desc "Entry point for ignition command-line tools"
   homepage "https://ignitionrobotics.org"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-tools/releases/ignition-tools-1.4.1.tar.bz2"
-  sha256 "2aa1f7999068ff9e01ad8029899fd00d575a95a2b7bd16c59e47f832eb47b1c6"
+  url "https://github.com/ignitionrobotics/ign-tools.git", branch: "main"
+  version "1.999.999~0~20220414"
   license "Apache-2.0"
-  head "https://github.com/ignitionrobotics/ign-tools.git", branch: "ign-tools1"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any, big_sur:  "cc38c02e39b95ea6a6b9052e3c28bf60647073c40c4cae41226583132e8b0456"
-    sha256 cellar: :any, catalina: "9e4788d9840133d4ca28c12499e659213406bb6c3c28f7b100563d5042c7fe4e"
-  end
+  head "https://github.com/ignitionrobotics/ign-tools.git", branch: "ign-tools2"
 
   depends_on "cmake" => :build
   depends_on "libyaml" => :test
