dfd838134f25d39c8ba1a2ab0123ba49284ba69f
Steve Peters <scpeters@openrobotics.org>
2022-07-15 16:34:57 -0700

    ignition-sensors6: revision bump for protobuf 21.2

--- Formula/ignition-sensors6.rb
+++ Formula/ignition-sensors6.rb
@@ -4,12 +4,7 @@ class IgnitionSensors6 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-sensors/releases/ignition-sensors6-6.6.0.tar.bz2"
   sha256 "a73b9a4b81b195dda5bf269deb9fb7151bbf8c9dbec7ed18aaed15def6538e56"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any, big_sur:  "1d65b39f3f595fe1f9b8378e8f5afea8fdaa2bb4b88c8b2dcf5e3604f79a3271"
-    sha256 cellar: :any, catalina: "f63b1090cdbbf9350a9eb9cda4718f858e394d3a38bca556a61c03f0b48d0cfc"
-  end
+  revision 1
 
   depends_on "cmake" => [:build, :test]
   depends_on "pkg-config" => [:build, :test]
