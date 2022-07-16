ef7a497b9bdb7d67e58c6fab89841654d6c88e7e
Steve Peters <scpeters@openrobotics.org>
2022-07-16 11:12:53 -0700

    ignition-sensors6: revision bump rebuild after protobuf

--- Formula/ignition-sensors6.rb
+++ Formula/ignition-sensors6.rb
@@ -4,7 +4,7 @@ class IgnitionSensors6 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-sensors/releases/ignition-sensors6-6.6.0.tar.bz2"
   sha256 "a73b9a4b81b195dda5bf269deb9fb7151bbf8c9dbec7ed18aaed15def6538e56"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   depends_on "cmake" => [:build, :test]
   depends_on "pkg-config" => [:build, :test]
