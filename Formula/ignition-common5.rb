1045d23a6ac18d498c897ea5c84bf9aaf0f04540
Michael Carroll <michael@openrobotics.org>
2022-07-29 19:07:17 -0500

    Revert "gz-common5: depend on libcsv (#1976)"
    This reverts commit 2fec540cafbaaae96c96ba2a4dfae02eb454fa12.

--- Formula/ignition-common5.rb
+++ Formula/ignition-common5.rb
@@ -14,7 +14,6 @@ class IgnitionCommon5 < Formula
   depends_on "gz-cmake3"
   depends_on "gz-math7"
   depends_on "gz-utils2"
-  depends_on "libcsv"
   depends_on macos: :high_sierra # c++17
   depends_on "ossp-uuid"
   depends_on "pkg-config"
