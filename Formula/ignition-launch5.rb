13385ce4f038537c7d047bd26abe532798d3f44d
Steve Peters <scpeters@openrobotics.org>
2022-08-02 08:17:20 -0700

    ignition-launch5: revision bump to fix linkage

--- Formula/ignition-launch5.rb
+++ Formula/ignition-launch5.rb
@@ -4,7 +4,7 @@ class IgnitionLaunch5 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-launch/releases/ignition-launch5-5.1.0.tar.bz2"
   sha256 "910f46ecb50503f86ec5753e367108c26bf9d74e9457d01c4099150c982b3e87"
   license "Apache-2.0"
-  revision 3
+  revision 4
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
