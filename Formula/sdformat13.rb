3fb544bd93207b9ebc7c97451ca9d2a9cf07370b
osrfbuild <osrfbuild@osrfoundation.org>
2022-08-31 06:15:20 +0200

    sdformat13 13.0.0~pre1 (#2020)
    * sdformat13: update 13.0.0~pre1
    
    * update  bottle.
    
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>
    Co-authored-by: Louise Poubel <louise@openrobotics.org>

--- Formula/sdformat13.rb
+++ Formula/sdformat13.rb
@@ -1,9 +1,16 @@
 class Sdformat13 < Formula
   desc "Simulation Description Format"
   homepage "http://sdformat.org"
-  url "https://github.com/gazebosim/sdformat.git", branch: "sdf13"
-  version "12.999.999~0~20220414"
+  url "https://osrf-distributions.s3.amazonaws.com/sdformat/releases/sdformat-13.0.0~pre1.tar.bz2"
+  version "13.0.0~pre1"
+  sha256 "742afdd8c2eaaf5c2d339c238726258ece4109851a97812874a9b8823a6304a6"
   license "Apache-2.0"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 big_sur:  "b4939109fd145ee3cdf1785ad1590aed9b2f2ec0569c1cd1fd56b96dcd12b8fc"
+    sha256 catalina: "6d897a784ddd7b6d20ed4a81668cbe6e8b189d60ab0c6b530ef098143f224e7f"
+  end
 
   depends_on "cmake" => [:build, :test]
   depends_on "pkg-config" => [:build, :test]
