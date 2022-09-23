a1383599d08e7e98e3a499c30a0d1193e1649379
osrfbuild <osrfbuild@osrfoundation.org>
2022-09-22 20:23:07 -0700

    gz-utils2 2.0.0 (#2045)
    * update  bottle.
    
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/gz-utils2.rb
+++ Formula/gz-utils2.rb
@@ -1,15 +1,14 @@
 class GzUtils2 < Formula
   desc "General purpose classes and functions designed for robotic applications"
   homepage "https://github.com/gazebosim/gz-utils"
-  url "https://osrf-distributions.s3.amazonaws.com/gz-utils/releases/gz-utils-2.0.0~pre2.tar.bz2"
-  version "2.0.0~pre2"
-  sha256 "b8263eeb2307a035ee12c97b647a2e7f59a0f7d6a5e9315a4b54d46dff22be98"
+  url "https://osrf-distributions.s3.amazonaws.com/gz-utils/releases/gz-utils-2.0.0.tar.bz2"
+  sha256 "a2fb092f49310c1d61cbdecc4d75d3ffbd4475352d510b8733cde8bc06555e46"
   license "Apache-2.0"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any, big_sur:  "b81e091fb4aa6d57d5c44e67f404b415ca607a6e24d685bf7e8b95e738d90b28"
-    sha256 cellar: :any, catalina: "c23bd28d07ff056536fe06ea6d4a6c8257d2bb8d700508b6144c8f84993a6468"
+    sha256 cellar: :any, big_sur:  "06232e41bb86374561a8b325429be785770fa108644286329f4453f429612d1d"
+    sha256 cellar: :any, catalina: "b59cfd89d536d1d5c6635f460a61f65649970718798ec2269c3c848be5456443"
   end
 
   depends_on "cmake" => [:build, :test]
