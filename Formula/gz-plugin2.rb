f183f285738628ea79cb6257adb3ab6777c20df8
osrfbuild <osrfbuild@osrfoundation.org>
2022-09-26 09:46:52 -0700

    gz-plugin2 2.0.0 (#2048)
    * update  bottle.
    
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/gz-plugin2.rb
+++ Formula/gz-plugin2.rb
@@ -1,15 +1,14 @@
 class GzPlugin2 < Formula
   desc "Plugin libraries for robotics applications"
   homepage "https://github.com/gazebosim/gz-plugin"
-  url "https://osrf-distributions.s3.amazonaws.com/gz-plugin/releases/gz-plugin-2.0.0~pre1.tar.bz2"
-  version "2.0.0~pre1"
-  sha256 "e3260857900cf50c104414d6efd68e9e3d75418469b3e022607db278c438b576"
+  url "https://osrf-distributions.s3.amazonaws.com/gz-plugin/releases/gz-plugin-2.0.0.tar.bz2"
+  sha256 "53ed6739275943dfbc5390f437ea12fdbeaa6dd5ec80347f59e7b4c6f11f3131"
   license "Apache-2.0"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any, big_sur:  "affbcf922610ef0a15e511d6808ced5ad8a53ed8c61bfd3a2e5649d2b8af5836"
-    sha256 cellar: :any, catalina: "7d6d2d987a97438aa9b7cafac2b17af2fe5ae13e389fbb6b7f6934dfef693765"
+    sha256 cellar: :any, big_sur:  "d8411a99175713a605030f848755154ad8d3c12ad023333e44e243cf2bfa1723"
+    sha256 cellar: :any, catalina: "cbb92efd156493eeab4eac4f26f504659b1b2b73906529539acd10d70b035a1d"
   end
 
   depends_on "cmake"
