ce07fc5be8476da75f119f0c0f6a6397a42eff06
Steve Peters <scpeters@openrobotics.org>
2022-09-30 23:08:36 -0700

    chrono-engine: fix homepage
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/chrono-engine.rb
+++ Formula/chrono-engine.rb
@@ -1,6 +1,6 @@
 class ChronoEngine < Formula
   desc "Chrono physics engine"
-  homepage "http://www.projectchrono.org/chronoengine/"
+  homepage "http://www.projectchrono.org"
   url "https://github.com/projectchrono/chrono/archive/7.0.3.tar.gz"
   sha256 "335923458fc75024baf2458c94d9d227da6ee91f989f5603b2d13498e2db0a81"
   license "BSD-3-Clause"
