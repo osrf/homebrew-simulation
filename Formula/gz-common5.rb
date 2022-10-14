5608a767df6544446642e37cc205b92b28cce6fa
osrfbuild <osrfbuild@osrfoundation.org>
2022-10-14 09:31:49 -0700

    gz-common5 5.1.0 (#2088)
    * gz-common5: update 5.1.0
    
    * update  bottle.

--- Formula/gz-common5.rb
+++ Formula/gz-common5.rb
@@ -1,17 +1,17 @@
 class GzCommon5 < Formula
   desc "Common libraries for robotics applications"
   homepage "https://gazebosim.org"
-  url "https://osrf-distributions.s3.amazonaws.com/gz-common/releases/gz-common-5.0.0.tar.bz2"
-  sha256 "2b1ef73d22e672bed80d1356d49e85c3e043206e3b274f6f52c6a5fc335ac22d"
+  url "https://osrf-distributions.s3.amazonaws.com/gz-common/releases/gz-common-5.1.0.tar.bz2"
+  sha256 "f6d32220b22ad645d74fda29799eb1f9ca2c3c6cf8491925831599c4b7827600"
   license "Apache-2.0"
 
   head "https://github.com/gazebosim/gz-common.git", branch: "gz-common5"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any, monterey: "c35d710ee066a1b67abba357733d649acfaa78e20034685e807e18753d31c064"
-    sha256 cellar: :any, big_sur:  "a362fbac6778758ba023675c08780bddedd25a4f1d52eea3b13ef8b6caf7a2fc"
-    sha256 cellar: :any, catalina: "ee69b523fb2846d93571f6191439031ed0f29b00489f634a59bb59073d358bcb"
+    sha256 cellar: :any, monterey: "b5d2d1adeda2571cc2e09513aeb7c4367b88199b03efc1783db565052dbce22e"
+    sha256 cellar: :any, big_sur:  "e1385d7bd1746cba7b5c43fd89e88d5af6c95081e7383662a01ab6acae6235f2"
+    sha256 cellar: :any, catalina: "f47d151d6cc4b0ff16cdffb0bf0cbf8bce482cdb16255750fc67cce769d411a1"
   end
 
   depends_on "assimp"
