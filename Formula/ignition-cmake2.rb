ac3266aff437ab29200aee7d4b458e5b5b895b24
osrfbuild <osrfbuild@osrfoundation.org>
2022-11-10 13:45:15 -0800

    ignition-cmake2 2.16.0 (#2121)
    * remove patch
    * update  bottle.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/ignition-cmake2.rb
+++ Formula/ignition-cmake2.rb
@@ -1,28 +1,21 @@
 class IgnitionCmake2 < Formula
   desc "CMake helper functions for building robotic applications"
   homepage "https://ignitionrobotics.org"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-cmake/releases/ignition-cmake2-2.15.0.tar.bz2"
-  sha256 "20a7b0c9e223db65fd3ed3c0a8e57ddafd93282c81badbf8bc3d493eeb5f37e6"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-cmake/releases/ignition-cmake2-2.16.0.tar.bz2"
+  sha256 "db1839df02ea0cd42fe0e2d266a96a747fcb1a354c50de91cbe5469525293335"
   license "Apache-2.0"
-  revision 1
 
   head "https://github.com/gazebosim/gz-cmake.git", branch: "ign-cmake2"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any_skip_relocation, monterey: "e451deff4849f7260661da980a611d460897fef00927051d772255743d82cb9a"
-    sha256 cellar: :any_skip_relocation, big_sur:  "ce7384690a4601962759d8e35edd43ef691ac0b487d942890f3d7bdc283f1804"
-    sha256 cellar: :any_skip_relocation, catalina: "8ab2080f06710fe2c35abac662bc10fbc9e7d64aa7db8c5f3d627545a37668ec"
+    sha256 cellar: :any_skip_relocation, monterey: "04fc73da0c6e677ad2d296d9b0fece0f23b95aaec0431ec12d16e6835657f742"
+    sha256 cellar: :any_skip_relocation, big_sur:  "387ab33bb278670c1df436af8f9b58b2e9ca6189743d372cf82f96e11e5b90e5"
+    sha256 cellar: :any_skip_relocation, catalina: "019c7e0b4218925c341ff50eeb7832aa8251f63233c741d044b85ae268fb1f6a"
   end
 
   depends_on "cmake"
   depends_on "pkg-config"
-
-  patch do
-    # fix for PKG_CONFIG_PATH in FindIgnOGRE2
-    url "https://github.com/gazebosim/gz-cmake/commit/038178552e56054c8908524df894dd818c50aa98.patch?full_index=1"
-    sha256 "1fcc06ca3345f713aa38b2b2ccad3feb572817de468c2de3e660ab99f162def7"
-  end
 
   def install
     cmake_args = std_cmake_args
