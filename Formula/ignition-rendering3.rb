20e372cc6ac0d1a1703fd12ac2ca0ebd1023dd09
Nate Koenig <nate@openrobotics.org>
2022-11-30 09:29:07 -0800

    rendering3: revision bump
    Signed-off-by: Nate Koenig <nate@openrobotics.org>

--- Formula/ignition-rendering3.rb
+++ Formula/ignition-rendering3.rb
@@ -4,7 +4,7 @@ class IgnitionRendering3 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-rendering/releases/ignition-rendering3-3.7.0.tar.bz2"
   sha256 "32acc59fd438a69ec73e4858b2431c9428a14ee60a6b60ef7e261072778a2020"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/gazebosim/gz-rendering.git", branch: "ign-rendering3"
 
