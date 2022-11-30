e6c2fb6bd454bf3459300c4d11daed267e1ba2af
Nate Koenig <nkoenig@users.noreply.github.com>
2022-11-30 12:54:06 -0800

    ignition-rendering3: revision bump (#2149)
    * update  bottle.
    
    Signed-off-by: Nate Koenig <nate@openrobotics.org>
    Co-authored-by: Nate Koenig <nate@openrobotics.org>
    Co-authored-by: OSRF Build Bot <osrfbuild@osrfoundation.org>

--- Formula/ignition-rendering3.rb
+++ Formula/ignition-rendering3.rb
@@ -4,9 +4,16 @@ class IgnitionRendering3 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-rendering/releases/ignition-rendering3-3.7.0.tar.bz2"
   sha256 "32acc59fd438a69ec73e4858b2431c9428a14ee60a6b60ef7e261072778a2020"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/gazebosim/gz-rendering.git", branch: "ign-rendering3"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 monterey: "57503ade61db2d9e6f63e465b932a3f700b15dff57e7077f393cb580a32d1ac2"
+    sha256 big_sur:  "65f6b2061326dfcd269a284a17048825c5fb574a2ecdc409ce3fd9aafbe40251"
+    sha256 catalina: "eef3495e96c0e7a826bc264f0a2aa5e531080f2cfc7b6ec80c5d207b8d9cf9d5"
+  end
 
   deprecate! date: "2024-12-31", because: "is past end-of-life date"
 
