41e24baf2a232f20b42c0ac67817208a8d38c153
osrfbuild <osrfbuild@osrfoundation.org>
2022-11-30 20:30:47 -0800

    ignition-gui3 3.12.0 (#2155)
    * update  bottle.
    
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/ignition-gui3.rb
+++ Formula/ignition-gui3.rb
@@ -1,17 +1,16 @@
 class IgnitionGui3 < Formula
   desc "Common libraries for robotics applications. GUI Library"
   homepage "https://github.com/gazebosim/gz-gui"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-gui/releases/ignition-gui3-3.11.2.tar.bz2"
-  sha256 "a8349a44e5b1df8ff25d33747b053a396e06739b66495c206257142270fcad64"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-gui/releases/ignition-gui3-3.12.0.tar.bz2"
+  sha256 "f53ee05d844449b900ecb30d5e1f812fd3f7e9e28630d309b7d8d11add3c3b1c"
   license "Apache-2.0"
 
   head "https://github.com/gazebosim/gz-gui.git", branch: "ign-gui3"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 monterey: "899a5304fd2f56a5c5585ef7f21a68491619fce3203c7ef13c93c0b053eddd3a"
-    sha256 big_sur:  "6f8c09fde83038788691da475e948317c62ef56c2d410dcbebaa6c5b1ee5b7f1"
-    sha256 catalina: "45c370441807a4d283bd3b94561b0536e707ad885d0ae0098b934ef209220e30"
+    sha256 monterey: "958287e1c8eacb59738188cc053976ec2446024e9040a1192fff3aa47aaefa1d"
+    sha256 big_sur:  "7f21b5cca242dd1db92a82973fcc1ceeec9c738ea9d3882f9976ee46886b555c"
   end
 
   deprecate! date: "2024-12-31", because: "is past end-of-life date"
