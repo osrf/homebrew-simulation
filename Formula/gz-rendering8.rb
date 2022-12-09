b66a20dfd43f626e411589f09c205da7f60cb186
Ian Chen <ichen@osrfoundation.org>
2022-12-09 03:24:48 -0800

    gz-rendering8: new formula (#2165)
    Signed-off-by: Ian Chen <ichen@osrfoundation.org>
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>

---
+++ Formula/gz-rendering8.rb
@@ -0,0 +1,72 @@
+class GzRendering8 < Formula
+  desc "Rendering library for robotics applications"
+  homepage "https://gazebosim.org"
+  url "https://github.com/gazebosim/gz-rendering.git"
+  version "7.999.999~0~20221208"
+  license "Apache-2.0"
+
+  head "https://github.com/gazebosim/gz-rendering.git", branch: "main"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "pkg-config" => [:build, :test]
+
+  depends_on "freeimage"
+  depends_on "gz-cmake3"
+  depends_on "gz-common5"
+  depends_on "gz-math7"
+  depends_on "gz-plugin2"
+  depends_on "gz-utils2"
+  depends_on macos: :mojave # c++17
+  depends_on "ogre1.9"
+  depends_on "ogre2.3"
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_TESTING=Off"
+    cmake_args << "-DCMAKE_INSTALL_RPATH=#{rpath}"
+
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    github_actions = ENV["HOMEBREW_GITHUB_ACTIONS"].present?
+    (testpath/"test.cpp").write <<-EOS
+      #include <gz/rendering/RenderEngine.hh>
+      #include <gz/rendering/RenderingIface.hh>
+      int main(int _argc, char** _argv)
+      {
+        gz::rendering::RenderEngine *engine =
+            gz::rendering::engine("ogre");
+        return engine == nullptr;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.10.2 FATAL_ERROR)
+      find_package(gz-rendering8 REQUIRED COMPONENTS ogre ogre2)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake gz-rendering8::gz-rendering8)
+    EOS
+    # test building with pkg-config
+    system "pkg-config", "gz-rendering8"
+    cflags   = `pkg-config --cflags gz-rendering8`.split
+    ldflags  = `pkg-config --libs gz-rendering8`.split
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   *ldflags,
+                   "-lc++",
+                   "-o", "test"
+    system "./test" unless github_actions
+    # test building with cmake
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake" unless github_actions
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
