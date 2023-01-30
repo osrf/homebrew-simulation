0f1d72ad47679b0eaf27b6636d8ac0c08d859655
Carlos Agüero <caguero@openrobotics.org>
2023-01-30 19:29:23 +0100

    gz-msgs10: new formula (#2185)
    * New msgs10 formula.
    Signed-off-by: Carlos Agüero <caguero@openrobotics.org>
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

---
+++ Formula/gz-msgs10.rb
@@ -0,0 +1,63 @@
+class GzMsgs10 < Formula
+  desc "Middleware protobuf messages for robotics"
+  homepage "https://gazebosim.org"
+  url "https://github.com/gazebosim/gz-msgs.git", branch: "main"
+  version "9.999.999~0~20230125"
+  license "Apache-2.0"
+
+  depends_on "protobuf-c" => :build
+  depends_on "cmake"
+  depends_on "gz-cmake3"
+  depends_on "gz-math7"
+  depends_on "gz-tools2"
+  depends_on macos: :high_sierra # c++17
+  depends_on "pkg-config"
+  depends_on "protobuf"
+  depends_on "tinyxml2"
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_TESTING=Off"
+    cmake_args << "-DCMAKE_INSTALL_RPATH=#{rpath}"
+
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <gz/msgs.hh>
+      int main() {
+        gz::msgs::UInt32;
+        return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.10 FATAL_ERROR)
+      find_package(gz-msgs10 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake gz-msgs10::gz-msgs10)
+    EOS
+    # test building with pkg-config
+    system "pkg-config", "gz-msgs10"
+    cflags = `pkg-config --cflags gz-msgs10`.split
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lgz-msgs10",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
