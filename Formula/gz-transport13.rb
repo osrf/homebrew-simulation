18fcd55e32c917dd9c527840b02732e5e17170d8
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-02-04 19:01:35 +0100

    gz-transport13: depend on gz-msgs10 (#2196)
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>
    Signed-off-by: Carlos Agüero <caguero@openrobotics.org>
    Co-authored-by: Carlos Agüero <caguero@openrobotics.org>

---
+++ Formula/gz-transport13.rb
@@ -0,0 +1,71 @@
+class GzTransport13 < Formula
+  desc "Transport middleware for robotics"
+  homepage "https://gazebosim.org"
+  url "https://github.com/gazebosim/gz-transport.git", branch: "main"
+  version "12.999.999~0~20230203"
+  license "Apache-2.0"
+
+  head "https://github.com/gazebosim/gz-transport.git", branch: "main"
+
+  depends_on "doxygen" => [:build, :optional]
+  depends_on "protobuf-c" => :build
+
+  depends_on "cmake"
+  depends_on "cppzmq"
+  depends_on "gz-cmake3"
+  depends_on "gz-msgs10"
+  depends_on "gz-tools2"
+  depends_on "gz-utils2"
+  depends_on macos: :mojave # c++17
+  depends_on "ossp-uuid"
+  depends_on "pkg-config"
+  depends_on "protobuf"
+  depends_on "zeromq"
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_TESTING=Off"
+    cmake_args << "-DCMAKE_INSTALL_RPATH=#{rpath}"
+
+    # Use build folder
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <iostream>
+      #include <gz/transport.hh>
+      int main() {
+        gz::transport::NodeOptions options;
+        return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.10 FATAL_ERROR)
+      find_package(gz-transport13 QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake gz-transport13::gz-transport13)
+    EOS
+    system "pkg-config", "gz-transport13"
+    cflags = `pkg-config --cflags gz-transport13`.split
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lgz-transport13",
+                   "-lc++",
+                   "-o", "test"
+    ENV["GZ_PARTITION"] = rand((1 << 32) - 1).to_s
+    system "./test"
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
