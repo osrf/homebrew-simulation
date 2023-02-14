30b1005956c8ff7ac26eebe038a9adb9dcd3285e
Steve Peters <scpeters@openrobotics.org>
2023-02-14 14:05:37 -0800

    ignition-math6: remove broken bottle (#2206)
    Revision bump and remove broken bottle.

--- Formula/ignition-math6.rb
+++ Formula/ignition-math6.rb
@@ -4,15 +4,9 @@ class IgnitionMath6 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-math/releases/ignition-math6-6.13.0.tar.bz2"
   sha256 "4d412a53644ecb984a0f64ca5df6e7043514673a406cd6228173278ce4eaf924"
   license "Apache-2.0"
+  revision 1
 
   head "https://github.com/gazebosim/gz-math.git", branch: "ign-math6"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any, monterey: "454848e5baaa89e55aeed4099745cfd7f8f664f37bc1a60c794d131ef7e989fc"
-    sha256 cellar: :any, big_sur:  "f0a2c4558bbba2e7d5decf2ca2cdece0126638c50074d29b4f46ae302b5bf0ee"
-    sha256 cellar: :any, catalina: "e1b5423ca61a04d4275c60a22f466175bb077addb8c9aef74656b74f46b56997"
-  end
 
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
