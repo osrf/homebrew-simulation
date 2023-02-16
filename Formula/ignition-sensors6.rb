f215cf19f15015a6af2700e95ca2c1a315a93f35
osrfbuild <osrfbuild@osrfoundation.org>
2023-02-16 13:02:21 -0800

    ignition-sensors6 6.7.0~pre1 (#2204)
    * update  bottle.
    
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/ignition-sensors6.rb
+++ Formula/ignition-sensors6.rb
@@ -1,18 +1,17 @@
 class IgnitionSensors6 < Formula
   desc "Sensors library for robotics applications"
   homepage "https://github.com/gazebosim/gz-sensors"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-sensors/releases/ignition-sensors6-6.6.0.tar.bz2"
-  sha256 "a73b9a4b81b195dda5bf269deb9fb7151bbf8c9dbec7ed18aaed15def6538e56"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-sensors/releases/ignition-sensors6-6.7.0~pre1.tar.bz2"
+  version "6.7.0~pre1"
+  sha256 "90465f4e3093610eb74341e68078be9c5a205da8b6e5dea15d0cf38343581af6"
   license "Apache-2.0"
-  revision 2
 
   head "https://github.com/gazebosim/gz-sensors.git", branch: "ign-sensors6"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any, monterey: "ec7396571f84d7c4b225ee56f9e1256761b2d944ab260f8b6ab52eba6b81139a"
-    sha256 cellar: :any, big_sur:  "021fcddc3bbb1bf4bba53d14e46d16503d32a26df459ea347a1e52e559661b3d"
-    sha256 cellar: :any, catalina: "e2fd696851e4a7d9f86c63b7ed76fe8b0513102fc52a943df508eef96399ceeb"
+    sha256 cellar: :any, monterey: "5774de0ba51a9fb85e49d3a9ad031384c1e1fc32a542a589c31e2a6490a64d47"
+    sha256 cellar: :any, big_sur:  "b834b55461d98cb54ab64a5ac8f9259610edc07b9f4f78744671e830182fa136"
   end
 
   depends_on "cmake" => [:build, :test]
