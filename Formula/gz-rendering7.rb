61fea64e9a85abbedd50782943f24ec9acb493bf
osrfbuild <osrfbuild@osrfoundation.org>
2023-03-06 01:55:42 -0800

    gz-rendering7 7.3.0 (#2224)
    * update  bottle.
    
    Co-authored-by: Ian Chen <ichen@openrobotics.org>

--- Formula/gz-rendering7.rb
+++ Formula/gz-rendering7.rb
@@ -1,17 +1,16 @@
 class GzRendering7 < Formula
   desc "Rendering library for robotics applications"
   homepage "https://gazebosim.org"
-  url "https://osrf-distributions.s3.amazonaws.com/gz-rendering/releases/gz-rendering-7.2.0.tar.bz2"
-  sha256 "677c0c7e48d3b741f6d62de53a391e2d368588a2bf6438eda6cb2cb0c53b17a9"
+  url "https://osrf-distributions.s3.amazonaws.com/gz-rendering/releases/gz-rendering-7.3.0.tar.bz2"
+  sha256 "fb688095185da8fe21fc0896197fd35e44cf1fafc6f87e55585b4d44a6f39c84"
   license "Apache-2.0"
 
   head "https://github.com/gazebosim/gz-rendering.git", branch: "gz-rendering7"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 monterey: "87bd1f55ce300262f6adc243f6a2622094966d4f9e1eba536c5db31ea9d9463d"
-    sha256 big_sur:  "69c9d91834648547b0a12daba45512edcac66541b68270a9717abaf7b00926aa"
-    sha256 catalina: "0796f9cb2085860812ae6bc9e2269191db0958dcef22fedb203f06268103cc3d"
+    sha256 monterey: "861767037252dd0d1ea52a41bb7e3075e44decb9a78bdfd92fdb90836245a577"
+    sha256 big_sur:  "c645f63460618c61a1d611c8a911aeef0373922a8faac68f1b8e9ae88d190544"
   end
 
   depends_on "cmake" => [:build, :test]
