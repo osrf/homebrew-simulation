8dc79b9ca910818ba91a4c5069e2ab79e75717d1
osrfbuild <osrfbuild@osrfoundation.org>
2023-03-09 09:39:47 -0800

    ignition-transport11 11.4.0 (#2225)
    * ignition-transport11: update 11.4.0
    
    * update  bottle.

--- Formula/ignition-transport11.rb
+++ Formula/ignition-transport11.rb
@@ -1,8 +1,8 @@
 class IgnitionTransport11 < Formula
   desc "Transport middleware for robotics"
   homepage "https://ignitionrobotics.org"
-  url "https://osrf-distributions.s3.amazonaws.com/ign-transport/releases/ignition-transport11-11.3.2.tar.bz2"
-  sha256 "56f2481860e033e9f18a3a4882410b2c53e4d8be83397213a4ce75f65279eb09"
+  url "https://osrf-distributions.s3.amazonaws.com/ign-transport/releases/ignition-transport11-11.4.0.tar.bz2"
+  sha256 "504310cec3c097abb8d9a45e694924a70fadbf785c9e679545f85cb72cfdc434"
   license "Apache-2.0"
   version_scheme 1
 
@@ -10,8 +10,8 @@ class IgnitionTransport11 < Formula
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 monterey: "08117700e2989a3b9f6f4389c1eb1f1351a042236f5034815d2dc15d3e1e0e84"
-    sha256 big_sur:  "85b5c77f32b65d77c7685067c6d712b1d07ecf1b9eca319caeb13e0e40b9ef9d"
+    sha256 monterey: "d3031f5bf614e81a5cdc622eaf85eff70515b3009a1ce0c8431e057e84eecd8d"
+    sha256 big_sur:  "aac5cf9ed9a09b29d1778dbdb71650aa5df15925ddd116bacfbb49e81e6cda90"
   end
 
   depends_on "doxygen" => [:build, :optional]
