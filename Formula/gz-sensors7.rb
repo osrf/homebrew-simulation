a0a070c64b9d757d1d0acb824b9a47cab673bcdf
osrfbuild <osrfbuild@osrfoundation.org>
2023-04-13 13:58:16 -0700

    gz-sensors7 7.2.0 (#2236)
    * gz-sensors7: update 7.2.0
    
    * update  bottle.

--- Formula/gz-sensors7.rb
+++ Formula/gz-sensors7.rb
@@ -1,16 +1,16 @@
 class GzSensors7 < Formula
   desc "Sensors library for robotics applications"
   homepage "https://github.com/gazebosim/gz-sensors"
-  url "https://osrf-distributions.s3.amazonaws.com/gz-sensors/releases/gz-sensors-7.1.0.tar.bz2"
-  sha256 "d02c2b1fae287264b833bf9db77628dfb797d0d4999616c2e6857f371cf6a4c5"
+  url "https://osrf-distributions.s3.amazonaws.com/gz-sensors/releases/gz-sensors-7.2.0.tar.bz2"
+  sha256 "bf158cb7bde71866a3bf70e649888fe8f6f8eb0b7b77d87e991385c3b8a565af"
   license "Apache-2.0"
 
   head "https://github.com/gazebosim/gz-sensors.git", branch: "gz-sensors7"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any, monterey: "382a73ef751728a34e05d14b2ba1d9eacc2d395b10d426670267128eb468f19a"
-    sha256 cellar: :any, big_sur:  "9f12325ae3471bfdca61d13acd7385adb8ce03facd0a9582bace7c7fdbaed608"
+    sha256 cellar: :any, monterey: "588897e850ec3c99fb2923c09ea257bf33fdcf686454665119edf5af32c6dd9c"
+    sha256 cellar: :any, big_sur:  "995a20bc559b4f159b5794ca3c29af79b60d80d2032fe1d481bd8d1d570c0258"
   end
 
   depends_on "cmake" => [:build, :test]
