38aac533bf090fa0e8f8cbc31ee0a472de582a33
Steve Peters <scpeters@openrobotics.org>
2023-04-21 09:03:02 -0700

    ignition-common3: revision bump for rebuild

--- Formula/ignition-common3.rb
+++ Formula/ignition-common3.rb
@@ -4,7 +4,7 @@ class IgnitionCommon3 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-common/releases/ignition-common3-3.15.1.tar.bz2"
   sha256 "8c0ac36b97160ecfcb8eeae116fd0a6f07b62bf19004b0992ad2f3c7bc271294"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/gazebosim/gz-common.git", branch: "ign-common3"
 
