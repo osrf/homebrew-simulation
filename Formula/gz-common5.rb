a4707eaefe7209a34e4313aa4e1e83304c865b44
Steve Peters <scpeters@openrobotics.org>
2023-04-20 23:32:08 -0700

    gz-common5: revision bump for ffmpeg6

--- Formula/gz-common5.rb
+++ Formula/gz-common5.rb
@@ -4,15 +4,9 @@ class GzCommon5 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-common/releases/gz-common-5.3.1.tar.bz2"
   sha256 "607b8c409464362e29d2ff1ac2979b6c1784eb6e583627a6503320e1e7024601"
   license "Apache-2.0"
-  revision 2
+  revision 3
 
   head "https://github.com/gazebosim/gz-common.git", branch: "gz-common5"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any, monterey: "81d1a023fe87b0863ece72abbb5bbe5219a1d69ec1834946df81385831c67604"
-    sha256 cellar: :any, big_sur:  "6f9af685f16e90844e3a188c455cad974a31e3a913e9addad8d011286fec44a6"
-  end
 
   depends_on "assimp"
   depends_on "cmake"
