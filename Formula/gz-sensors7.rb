d78a1ec90e4d2eb550b7bc55d60eb040bf763f59
Steve Peters <scpeters@openrobotics.org>
2023-06-08 15:59:27 -0700

    gz-sensors7: revision bump for rebuild

--- Formula/gz-sensors7.rb
+++ Formula/gz-sensors7.rb
@@ -4,7 +4,7 @@ class GzSensors7 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-sensors/releases/gz-sensors-7.2.0.tar.bz2"
   sha256 "bf158cb7bde71866a3bf70e649888fe8f6f8eb0b7b77d87e991385c3b8a565af"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/gazebosim/gz-sensors.git", branch: "gz-sensors7"
 
@@ -48,6 +48,7 @@ class GzSensors7 < Formula
       target_link_libraries(test_cmake gz-sensors7::gz-sensors7)
     EOS
     # test building with pkg-config
+    ENV.append_path "PKG_CONFIG_PATH", Formula["protobuf@21"].opt_lib/"pkgconfig"
     system "pkg-config", "gz-sensors7"
     cflags   = `pkg-config --cflags gz-sensors7`.split
     ldflags  = `pkg-config --libs gz-sensors7`.split
@@ -58,6 +59,7 @@ class GzSensors7 < Formula
                    "-o", "test"
     system "./test"
     # test building with cmake
+    ENV.append_path "CMAKE_PREFIX_PATH", Formula["protobuf@21"].opt_prefix
     mkdir "build" do
       system "cmake", ".."
       system "make"
