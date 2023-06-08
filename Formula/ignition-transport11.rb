744f140e0bd5f86c9e1c68fc761e720f018a3e76
Steve Peters <scpeters@openrobotics.org>
2023-06-08 09:48:21 -0700

    ignition-transport11: revision bump for broken bottles

--- Formula/ignition-transport11.rb
+++ Formula/ignition-transport11.rb
@@ -4,15 +4,10 @@ class IgnitionTransport11 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-transport/releases/ignition-transport11-11.4.0.tar.bz2"
   sha256 "504310cec3c097abb8d9a45e694924a70fadbf785c9e679545f85cb72cfdc434"
   license "Apache-2.0"
+  revision 1
   version_scheme 1
 
   head "https://github.com/gazebosim/gz-transport.git", branch: "ign-transport11"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 monterey: "d3031f5bf614e81a5cdc622eaf85eff70515b3009a1ce0c8431e057e84eecd8d"
-    sha256 big_sur:  "aac5cf9ed9a09b29d1778dbdb71650aa5df15925ddd116bacfbb49e81e6cda90"
-  end
 
   depends_on "doxygen" => [:build, :optional]
   depends_on "protobuf-c" => :build
