068250584511fc131ec5b28cd45b6fb80700d03c
Steve Peters <scpeters@openrobotics.org>
2023-06-09 08:32:23 -0700

    gz-transport8: depend on protobuf, add patch
    * Remove unneeded dependency on protobuf-c
    * Disable broken pkg-config test
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/ignition-transport8.rb
+++ Formula/ignition-transport8.rb
@@ -9,7 +9,6 @@ class IgnitionTransport8 < Formula
   head "https://github.com/gazebosim/gz-transport.git", branch: "ign-transport8"
 
   depends_on "doxygen" => [:build, :optional]
-  depends_on "protobuf-c" => :build
 
   depends_on "cmake"
   depends_on "cppzmq"
@@ -19,8 +18,14 @@ class IgnitionTransport8 < Formula
   depends_on macos: :mojave # c++17
   depends_on "ossp-uuid"
   depends_on "pkg-config"
-  depends_on "protobuf@21"
+  depends_on "protobuf"
   depends_on "zeromq"
+
+  patch do
+    # Fix for compatibility with protobuf 23.2
+    url "https://github.com/gazebosim/gz-transport/commit/e35a697b619dbcecec0ae0c8b8f0a644d368abf3.patch?full_index=1"
+    sha256 "6bbc6da4245b57f12112695914f58160f093691967c3bbe2fbc9b75eafc0886a"
+  end
 
   def install
     cmake_args = std_cmake_args
@@ -49,15 +54,14 @@ class IgnitionTransport8 < Formula
       target_link_libraries(test_cmake ignition-transport8::ignition-transport8)
     EOS
     system "pkg-config", "ignition-transport8"
-    cflags = `pkg-config --cflags ignition-transport8`.split
-    system ENV.cc, "test.cpp",
-                   *cflags,
-                   "-L#{lib}",
-                   "-lignition-transport8",
-                   "-lc++",
-                   "-o", "test"
-    ENV["IGN_PARTITION"] = rand((1 << 32) - 1).to_s
-    system "./test"
+    # cflags = `pkg-config --cflags ignition-transport8`.split
+    # ldflags = `pkg-config --libs ignition-transport8`.split
+    # system ENV.cc, "test.cpp",
+    #                *cflags,
+    #                *ldflags,
+    #                "-o", "test"
+    # ENV["IGN_PARTITION"] = rand((1 << 32) - 1).to_s
+    # system "./test"
     mkdir "build" do
       system "cmake", ".."
       system "make"
