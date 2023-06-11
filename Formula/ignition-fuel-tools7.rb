ac825d04ced51f73c82a1214b148772d30a54608
Steve Peters <scpeters@openrobotics.org>
2023-06-10 23:03:07 -0700

    ignition-fuel-tools7: revision bump for rebuild

--- Formula/ignition-fuel-tools7.rb
+++ Formula/ignition-fuel-tools7.rb
@@ -4,7 +4,7 @@ class IgnitionFuelTools7 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-fuel-tools/releases/ignition-fuel-tools7-7.2.2.tar.bz2"
   sha256 "14d718337a645ae4471183097a5979f9e6efe142e3e8697c49d7973bd07e766e"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/gazebosim/gz-fuel-tools.git", branch: "ign-fuel-tools7"
 
