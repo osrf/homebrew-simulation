dccf4b37015e66eaafacf07c392bb2c3f82baf23
Addisu Z. Taddese <addisu@openrobotics.org>
2023-08-03 08:20:40 -0700

    gz-sensors7: revision bump

--- Formula/gz-sensors7.rb
+++ Formula/gz-sensors7.rb
@@ -4,15 +4,9 @@ class GzSensors7 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-sensors/releases/gz-sensors-7.2.0.tar.bz2"
   sha256 "bf158cb7bde71866a3bf70e649888fe8f6f8eb0b7b77d87e991385c3b8a565af"
   license "Apache-2.0"
-  revision 4
+  revision 5
 
   head "https://github.com/gazebosim/gz-sensors.git", branch: "gz-sensors7"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any, monterey: "820f369f378b5f84655afe4646807ae451904f23bd551837fa5916f944389878"
-    sha256 cellar: :any, big_sur:  "812d85f89428e4851e347b0bef8c5e57118f533636df6993acaf1367238a124f"
-  end
 
   depends_on "cmake" => [:build, :test]
   depends_on "pkg-config" => [:build, :test]
