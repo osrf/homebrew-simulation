aa557bcf0115bbbcfc8a786e5d9c3297e285c27f
Addisu Z. Taddese <addisu@openrobotics.org>
2023-08-07 11:46:51 -0500

    Bumps in harmonic : ci_matching_branch/bump_harmonic_gz-physics7 (#2325)
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

---
+++ Formula/gz-physics7.rb
@@ -0,0 +1,67 @@
+class GzPhysics7 < Formula
+  desc "Physics library for robotics applications"
+  homepage "https://github.com/gazebosim/gz-physics"
+  url "https://github.com/gazebosim/gz-physics.git", branch: "main"
+  version "6.999.999~0~20230728"
+  license "Apache-2.0"
+
+  depends_on "cmake" => :build
+
+  depends_on "bullet"
+  depends_on "dartsim"
+  depends_on "google-benchmark"
+  depends_on "gz-cmake3"
+  depends_on "gz-common5"
+  depends_on "gz-math7"
+  depends_on "gz-plugin2"
+  depends_on "gz-utils2"
+  depends_on macos: :mojave # c++17
+  depends_on "pkg-config"
+  depends_on "sdformat14"
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_TESTING=Off"
+    cmake_args << "-DCMAKE_INSTALL_RPATH=#{rpath}"
+    system "cmake", ".", *cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include "gz/plugin/Loader.hh"
+      #include "gz/physics/ConstructEmpty.hh"
+      #include "gz/physics/RequestEngine.hh"
+      int main()
+      {
+        gz::plugin::Loader loader;
+        loader.LoadLib("#{opt_lib}/libgz-physics7-dartsim-plugin.dylib");
+        gz::plugin::PluginPtr dartsim =
+            loader.Instantiate("gz::physics::dartsim::Plugin");
+        using featureList = gz::physics::FeatureList<
+            gz::physics::ConstructEmptyWorldFeature>;
+        auto engine =
+            gz::physics::RequestEngine3d<featureList>::From(dartsim);
+        return engine == nullptr;
+      }
+    EOS
+    system "pkg-config", "gz-physics7"
+    cflags   = `pkg-config --cflags gz-physics7`.split
+    ldflags  = `pkg-config --libs gz-physics7`.split
+    system "pkg-config", "gz-plugin2-loader"
+    loader_cflags   = `pkg-config --cflags gz-plugin2-loader`.split
+    loader_ldflags  = `pkg-config --libs gz-plugin2-loader`.split
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   *ldflags,
+                   *loader_cflags,
+                   *loader_ldflags,
+                   "-lc++",
+                   "-o", "test"
+    # Disable test due to gazebosim/gz-physics#442
+    # system "./test"
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
