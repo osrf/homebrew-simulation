aec0057d58e81e0c64d5b621f52b21772c8c6cc0
Addisu Z. Taddese <addisu@openrobotics.org>
2023-08-25 18:51:15 -0500

    gz-msgs10: install python bindings
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- Formula/gz-msgs10.rb
+++ Formula/gz-msgs10.rb
@@ -12,6 +12,7 @@ class GzMsgs10 < Formula
   depends_on macos: :high_sierra # c++17
   depends_on "pkg-config"
   depends_on "protobuf"
+  depends_on "python@3.11"
   depends_on "tinyxml2"
 
   def install
@@ -23,6 +24,9 @@ class GzMsgs10 < Formula
       system "cmake", "..", *cmake_args
       system "make", "install"
     end
+
+    (lib/"python3.11/site-packages").install Dir[lib/"python/*"]
+    rmdir prefix/"lib/python"
   end
 
   test do
@@ -58,5 +62,7 @@ class GzMsgs10 < Formula
     # check for Xcode frameworks in bottle
     cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
     system cmd_not_grep_xcode
+    # check python import
+    system Formula["python@3.11"].opt_bin/"python3.11", "-c", "import gz.msgs10"
   end
 end
