4fbfc3672131e40c20d9ebfec8310e29054f9956
Steve Peters <scpeters@openrobotics.org>
2023-08-29 09:14:24 -0700

    ignition-gui6: revision bump broken bottle

--- Formula/ignition-gui6.rb
+++ Formula/ignition-gui6.rb
@@ -4,15 +4,9 @@ class IgnitionGui6 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-gui/releases/ignition-gui6-6.8.0.tar.bz2"
   sha256 "dd4f26100f4d1343f068ba36f2b8394a0cddb337efde7b4a21c1b0f66ce496c9"
   license "Apache-2.0"
-  revision 6
+  revision 7
 
   head "https://github.com/gazebosim/gz-gui.git", branch: "ign-gui6"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 monterey: "10de663f0502e07683841a270a1c1b8deeeedd7025b05a852716fcdb5ce402d2"
-    sha256 big_sur:  "3c55baad8501485e7d55c58f654808a92b981aab91193e84d4d561a98c2f7b66"
-  end
 
   depends_on "cmake" => [:build, :test]
   depends_on "pkg-config" => [:build, :test]
