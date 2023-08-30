13526bd564ee19b248d28938301db8d132ae84d5
osrfbuild <osrfbuild@osrfoundation.org>
2023-08-30 14:29:41 -0700

    gz-physics6 6.5.0 (#2355)
    * update  bottle.
    
    Co-authored-by: Jose Luis Rivero <jrivero@osrfoundation.org>
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/gz-physics6.rb
+++ Formula/gz-physics6.rb
@@ -1,16 +1,15 @@
 class GzPhysics6 < Formula
   desc "Physics library for robotics applications"
   homepage "https://github.com/gazebosim/gz-physics"
-  url "https://osrf-distributions.s3.amazonaws.com/gz-physics/releases/gz-physics-6.4.0.tar.bz2"
-  sha256 "20c47b96d430eee05c3f3ef24606e798a922077b099e9796713d2e7d8a893cf3"
+  url "https://osrf-distributions.s3.amazonaws.com/gz-physics/releases/gz-physics-6.5.0.tar.bz2"
+  sha256 "807345b1e8ca3b33678cd5424331f50d7e609be453491247c88267ec8af0adee"
   license "Apache-2.0"
-  revision 5
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any, ventura:  "e079c3c24956c2cd612ae46eeefbee3376662fccad46587ec3fb84027d6b47f2"
-    sha256 cellar: :any, monterey: "1e103be3df7461f1794f78a8ef3d62b9d7e68fdb59277021e7c7bd162f33a5b2"
-    sha256 cellar: :any, big_sur:  "c09e34bcea880205692db2c911084f09cbce67eacae9ee75d7f2e75200f3f2ec"
+    sha256 ventura:  "11339ffc950754980e92302f2721784a7ad1d1cc76869ea13d1da61bc6fbc47f"
+    sha256 monterey: "6981b3711f914e88cdbf86464559adb0d5d146b4e3161385ced44789bb4447d5"
+    sha256 big_sur:  "fe19abc81ae3e805ce5fe4ff90fc071a24b11c1828f5b4acd5605821740f6df9"
   end
 
   depends_on "cmake" => :build
