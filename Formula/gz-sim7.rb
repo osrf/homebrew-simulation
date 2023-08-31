af43ce32f45ebd0ce16d9f3099c628f73d8f3210
Steve Peters <scpeters@openrobotics.org>
2023-08-31 01:24:59 -0700

    gz-sim7: revision bump rebuild bottle

--- Formula/gz-sim7.rb
+++ Formula/gz-sim7.rb
@@ -4,7 +4,7 @@ class GzSim7 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-sim/releases/gz-sim-7.5.0.tar.bz2"
   sha256 "e4a641bef1a747dd9a35c01beee3a1ac08f95bdaae06aa23b115e0b1a4ee42f8"
   license "Apache-2.0"
-  revision 7
+  revision 8
 
   head "https://github.com/gazebosim/gz-sim.git", branch: "gz-sim7"
 
