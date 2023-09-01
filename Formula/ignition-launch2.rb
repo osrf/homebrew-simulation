9e59da84088140e03011a5e2c15ab2effefbc529
Steve Peters <scpeters@openrobotics.org>
2023-08-31 20:27:50 -0700

    Citadel: build remaining Ventura bottles
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/ignition-launch2.rb
+++ Formula/ignition-launch2.rb
@@ -36,6 +36,7 @@ class IgnitionLaunch2 < Formula
     cmake_args << "-DBUILD_TESTING=OFF"
     cmake_args << "-DCMAKE_INSTALL_RPATH=#{rpath}"
 
+    # Use build folder
     mkdir "build" do
       system "cmake", "..", *cmake_args
       system "make", "install"
