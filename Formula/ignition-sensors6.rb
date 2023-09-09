58a6692ecedd2b68611cb2c9d84de2b42ed11a8c
Steve Peters <scpeters@openrobotics.org>
2023-09-08 22:01:14 -0700

    ignition-sensors6: revision bump broken bottle

--- Formula/ignition-sensors6.rb
+++ Formula/ignition-sensors6.rb
@@ -4,16 +4,9 @@ class IgnitionSensors6 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-sensors/releases/ignition-sensors6-6.7.1.tar.bz2"
   sha256 "5b01cb99ff8b1effc0bdaa0309f4378894ace2640644ee3f2b96d44ff3aa10f0"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/gazebosim/gz-sensors.git", branch: "ign-sensors6"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any, ventura:  "7cd6696ff080b36c46fd15ee4da82f23d1d569f1a0e4a7a5a0db10d9b51e2353"
-    sha256 cellar: :any, monterey: "0d24debd89942757529fa6b90edc83c811303be55b30450bd9a700790d001915"
-    sha256 cellar: :any, big_sur:  "b00d71b1f1e3bd0b9e5e2e7100f487e10c9d1769f78ef1198c63933481da1175"
-  end
 
   depends_on "cmake" => [:build, :test]
   depends_on "pkg-config" => [:build, :test]
