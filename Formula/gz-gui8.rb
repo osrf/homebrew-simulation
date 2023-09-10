0b780af4a559447e9f80f3c68c7b2d862c461d8f
Steve Peters <scpeters@openrobotics.org>
2023-09-09 18:19:56 -0700

    gz-gui8: revision bump rebuild for protobuf

--- Formula/gz-gui8.rb
+++ Formula/gz-gui8.rb
@@ -5,7 +5,7 @@ class GzGui8 < Formula
   version "8.0.0~pre1"
   sha256 "30af47eb3fb9e83406a5d5dd921c9e6798a1b7abe636ccc364233f68b54187cb"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/gazebosim/gz-gui.git", branch: "main"
 
