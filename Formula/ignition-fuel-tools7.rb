3fd37adad7e4cb39e615f755b4573ec70f3564e8
Steve Peters <scpeters@openrobotics.org>
2023-09-13 18:01:14 -0700

    Fortress: patch physics and rebuild bottles (#2400)
    * ignition-physics5: revision bump dartsim patch
    * gz-sim6: enable monterey test
    * ignition-fuel-tools7: revision bump rebuild for protobuf
    * ignition-gazebo6: revision bump rebuild for protobuf
    * ignition-gui6: revision bump rebuild for protobuf
    * ignition-launch5: revision bump rebuild for protobuf
    * ignition-msgs8: revision bump rebuild for protobuf
    * ignition-sensors6: revision bump rebuild for protobuf
    * ignition-transport11: revision bump rebuild for protobuf
    * update  bottle.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>
    Co-authored-by: OSRF Build Bot <osrfbuild@osrfoundation.org>

--- Formula/ignition-fuel-tools7.rb
+++ Formula/ignition-fuel-tools7.rb
@@ -4,9 +4,16 @@ class IgnitionFuelTools7 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-fuel-tools/releases/ignition-fuel-tools7-7.3.0.tar.bz2"
   sha256 "59d06f23a054742e1f97c1f0f709e2a38c341ce96f560d6e09b3dba011dd79a5"
   license "Apache-2.0"
-  revision 8
+  revision 9
 
   head "https://github.com/gazebosim/gz-fuel-tools.git", branch: "ign-fuel-tools7"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 cellar: :any, ventura:  "6dc71ce7889abe2687571c8c052bad075f1475daee509615980703722f868c57"
+    sha256 cellar: :any, monterey: "396ca1c1965695afa6eff10e6c200f2a869d2c642abee09e18046172102ad6a4"
+    sha256 cellar: :any, big_sur:  "5a06ac35eb8ff173459765c0af68399a2858b4e874ca5ffa173937954aba0b49"
+  end
 
   depends_on "cmake"
   depends_on "ignition-cmake2"
