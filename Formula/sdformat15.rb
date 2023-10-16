37acdb4d1c58736e8666e3b20d91fce66017f6e8
Addisu Z. Taddese <addisu@openrobotics.org>
2023-10-16 17:58:01 -0500

    Bumps in ionic : ci_matching_branch/bump_ionic_sdformat15
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- Formula/sdformat15.rb
+++ Formula/sdformat15.rb
@@ -15,7 +15,7 @@ class Sdformat15 < Formula
   depends_on "gz-cmake4"
   depends_on "gz-math7"
   depends_on "gz-tools2"
-  depends_on "gz-utils2"
+  depends_on "gz-utils3"
   depends_on macos: :mojave # c++17
   depends_on "python@3.11"
   depends_on "tinyxml2"
