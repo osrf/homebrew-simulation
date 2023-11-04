54833c9d9ec081a3f13c114ba9d2ca9c021cb529
Steve Peters <scpeters@openrobotics.org>
2023-11-04 00:26:04 -0700

    gz-msgs9: revision bump broken bottle

--- Formula/gz-msgs9.rb
+++ Formula/gz-msgs9.rb
@@ -4,15 +4,9 @@ class GzMsgs9 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-msgs/releases/gz-msgs-9.5.0.tar.bz2"
   sha256 "693f403fca86e9956b393a86fd46505d94e27b7b2c1d39bc631ba9c3029b91f9"
   license "Apache-2.0"
-  revision 8
+  revision 9
 
   head "https://github.com/gazebosim/gz-msgs.git", branch: "gz-msgs9"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any, ventura:  "80cea2bf91c49c420eccd878fcdb44902bb8189c9a20043ad5b256cd1a977110"
-    sha256 cellar: :any, monterey: "80d4ea14e6801033628e1de31661035b8041dd557f14b4964c41c0f9e926e2c9"
-  end
 
   depends_on "cmake"
   depends_on "gz-cmake3"
