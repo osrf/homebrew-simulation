025f3d16f1cd923dd2ddd034f05e31702eaa28ee
Steve Peters <scpeters@openrobotics.org>
2023-11-20 11:07:34 -0800

    gz-common5: revision bump for gdal

--- Formula/gz-common5.rb
+++ Formula/gz-common5.rb
@@ -4,7 +4,7 @@ class GzCommon5 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-common/releases/gz-common-5.4.2.tar.bz2"
   sha256 "ed8d1053b306115f70a0cd8b4b666fc122491022afe4a5e11e612e6bf4729289"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/gazebosim/gz-common.git", branch: "gz-common5"
 
