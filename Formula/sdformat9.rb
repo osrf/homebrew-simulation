4d7bfa99fd3c73490eff3ac3d80ddadf3a1b2f98
Steve Peters <scpeters@openrobotics.org>
2024-01-04 18:45:36 -0800

    sdformat9: revision bump broken bottle

--- Formula/sdformat9.rb
+++ Formula/sdformat9.rb
@@ -4,15 +4,9 @@ class Sdformat9 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/sdformat/releases/sdformat-9.10.0.tar.bz2"
   sha256 "5660d4d1547f4e4039e3ad64830b29d2ebfc6aab2aa86ed86d8d8305ee4eb3c2"
   license "Apache-2.0"
+  revision 1
 
   head "https://github.com/gazebosim/sdformat.git", branch: "sdf9"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 ventura:  "c41c3b209374a08b22943b547f747b73cca91832b2af27b5a470886873624b0c"
-    sha256 monterey: "d756a1a7e0f5b838ce2fc50f494dda5e149d95878378caec1d7ccb5f845451ee"
-    sha256 big_sur:  "ee28c636471a819a3c8896a25ac0e8b8b45b80aa798fe68876ac96363aa65c7a"
-  end
 
   depends_on "cmake" => [:build, :test]
   depends_on "pkg-config" => [:build, :test]
