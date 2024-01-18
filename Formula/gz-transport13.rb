1e29dbfda64e8d1baf3685757ed78c9c2213a48d
Steve Peters <scpeters@openrobotics.org>
2024-01-18 15:12:50 -0800

    Rebuild harmonic bottles for protobuf 25.2 (#2532)
    * gz-fuel-tools9: revision bump rebuild bottle
    * gz-gui8: revision bump rebuild bottle
    * gz-launch7: revision bump rebuild bottle
    * gz-sensors8: revision bump rebuild bottle
    * gz-sim8: revision bump rebuild bottle
    * gz-transport13: revision bump rebuild bottle
    * update  bottle.
    
    Co-authored-by: OSRF Build Bot <osrfbuild@osrfoundation.org>

--- Formula/gz-transport13.rb
+++ Formula/gz-transport13.rb
@@ -4,9 +4,15 @@ class GzTransport13 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-transport/releases/gz-transport-13.0.0.tar.bz2"
   sha256 "d44b4ed089110a1a7dfc3b59f782e33f27668a980ededdbf3c21171f72a82968"
   license "Apache-2.0"
-  revision 7
+  revision 8
 
   head "https://github.com/gazebosim/gz-transport.git", branch: "gz-transport13"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 ventura:  "3f8f51b25e6e5dff6afd3d9a568a6f67cea96d329651e53c24e2101b9da2ad56"
+    sha256 monterey: "aa51abfae7a1f67b651f1a99597b82261352d4cdcb87f1e0c913543a9fee373b"
+  end
 
   depends_on "doxygen" => [:build, :optional]
   depends_on "pybind11" => :build
