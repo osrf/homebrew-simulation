5fdfe946be6bdaddbe0a5a1cce5dee961011f853
Steve Peters <scpeters@openrobotics.org>
2024-01-22 17:29:41 -0800

    gz-math7: explicitly use python@3.11
    Set Python3_EXECUTABLE cmake variable explicitly.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/gz-math7.rb
+++ Formula/gz-math7.rb
@@ -27,6 +27,7 @@ class GzMath7 < Formula
     cmake_args = std_cmake_args
     cmake_args << "-DBUILD_TESTING=OFF"
     cmake_args << "-DCMAKE_INSTALL_RPATH=#{rpath}"
+    cmake_args << "-DPython3_EXECUTABLE=#{which("python3")}"
 
     # Use build folder
     mkdir "build" do
