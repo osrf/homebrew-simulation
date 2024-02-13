395c5ed487110294b9950da09c3fe3234555cd90
Steve Peters <scpeters@openrobotics.org>
2024-02-13 08:18:02 -0800

    ignition-physics2: revision bump broken bottle

--- Formula/ignition-physics2.rb
+++ Formula/ignition-physics2.rb
@@ -4,15 +4,9 @@ class IgnitionPhysics2 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-physics/releases/ignition-physics2-2.6.2.tar.bz2"
   sha256 "4aa0dcd1a254da63f55a93d2cc928a5ff517f19ac1603c0fb5f810dd29c70e1d"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/gazebosim/gz-physics.git", branch: "gz-physics2"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any, ventura:  "8a21efb2a45f1947e7ecf35f7e789452c5247d51843bafc6b5205cc7b1ca3cbf"
-    sha256 cellar: :any, monterey: "dbb879aa6e80c5111a905d0353344e005aa05764164a87581bd0af09e1451a78"
-  end
 
   deprecate! date: "2024-12-31", because: "is past end-of-life date"
 
