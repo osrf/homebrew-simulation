bc333ffaae7c9b36f69a99215276f87e19ed0de5
Steve Peters <scpeters@openrobotics.org>
2024-02-18 21:06:21 -0800

    gz-launch7: revision bump rebuild bottle

--- Formula/gz-launch7.rb
+++ Formula/gz-launch7.rb
@@ -4,7 +4,7 @@ class GzLaunch7 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-launch/releases/gz-launch-7.0.0.tar.bz2"
   sha256 "252cb170fd97d074e9d13536cda736cd481c8c2e6df30a4ee225cfb9dcd92e77"
   license "Apache-2.0"
-  revision 12
+  revision 13
 
   head "https://github.com/gazebosim/gz-launch.git", branch: "gz-launch7"
 
