5d45f168acd76297c62e1976dd9f70429df60f0f
Steve Peters <scpeters@openrobotics.org>
2024-02-21 15:29:15 -0800

    gazebo11: rebuild bottle for protobuf (#2579)
    * update  bottle.
    
    Co-authored-by: OSRF Build Bot <osrfbuild@osrfoundation.org>

--- Formula/gazebo11.rb
+++ Formula/gazebo11.rb
@@ -4,9 +4,15 @@ class Gazebo11 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gazebo/releases/gazebo-11.14.0.tar.bz2"
   sha256 "7e9842c046c9e0755355b274c240a8abbf4e962be7ce7b7f59194e5f4b584f45"
   license "Apache-2.0"
-  revision 11
+  revision 12
 
   head "https://github.com/osrf/gazebo.git", branch: "gazebo11"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 ventura:  "48bd5b6a14e0e500a314a006ff4dda8ca9af27e325ad942f09f5f908b980f50a"
+    sha256 monterey: "f2fd393f3acef55b16433957b768fd878e373da09201fcbe65c7a96c524485c8"
+  end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
