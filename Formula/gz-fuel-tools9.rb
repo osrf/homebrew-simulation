61145f08bd0eae25a1413ebaeb6e454ebcbc0c05
Jose Luis Rivero <jrivero@osrfoundation.org>
2024-03-14 18:26:35 +0100

    gz-fuel-tools9: revision bump

--- Formula/gz-fuel-tools9.rb
+++ Formula/gz-fuel-tools9.rb
@@ -4,7 +4,7 @@ class GzFuelTools9 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-fuel-tools/releases/gz-fuel_tools-9.0.0.tar.bz2"
   sha256 "f6bff36311390bbdf169a767d94efd7657f6b44ee620dbe096ab122135f7c780"
   license "Apache-2.0"
-  revision 13
+  revision 14
 
   head "https://github.com/gazebosim/gz-fuel-tools.git", branch: "gz-fuel-tools9"
 
