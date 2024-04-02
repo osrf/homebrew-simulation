c31bfad83ba65b13444149a3ce309c2ad84a5c8b
Steve Peters <scpeters@openrobotics.org>
2024-04-02 11:30:54 -0700

    ignition-launch2: revision bump broken bottle

--- Formula/ignition-launch2.rb
+++ Formula/ignition-launch2.rb
@@ -4,15 +4,9 @@ class IgnitionLaunch2 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-launch/releases/ignition-launch2-2.3.1.tar.bz2"
   sha256 "984e2a5df03ca220960470b6b59728edf3cd570314fbad6435b67cb26c9b7e4e"
   license "Apache-2.0"
-  revision 8
+  revision 9
 
   head "https://github.com/gazebosim/gz-launch.git", branch: "ign-launch2"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 ventura:  "15576b3e746a89920b1d0d0f5238e62c5716d55e798ff6e1816b0dbf6fc1ff0f"
-    sha256 monterey: "3b9645ad9cf04bba8ea702a74df1d68738f12e6366047a726d1d00e81a434d84"
-  end
 
   deprecate! date: "2024-12-31", because: "is past end-of-life date"
 
