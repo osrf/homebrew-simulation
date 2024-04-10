c93ea7c130178c35a0ad8be526709948ffc9f8da
osrfbuild <osrfbuild@osrfoundation.org>
2024-04-09 17:37:00 -0700

    gz-transport13 13.2.0 (#2624)
    * update  bottle.
    
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/gz-transport13.rb
+++ Formula/gz-transport13.rb
@@ -1,17 +1,16 @@
 class GzTransport13 < Formula
   desc "Transport middleware for robotics"
   homepage "https://gazebosim.org"
-  url "https://osrf-distributions.s3.amazonaws.com/gz-transport/releases/gz-transport-13.2.0~pre2.tar.bz2"
-  version "13.2.0-pre2"
-  sha256 "8146c1c888f631cb16b5fbd3397d5db2b71b385f95b3b54b951a890762072e85"
+  url "https://osrf-distributions.s3.amazonaws.com/gz-transport/releases/gz-transport-13.2.0.tar.bz2"
+  sha256 "628f49fd44c158a49fd52c7b3172e44b8607ad74cbf1953e4404f1b003d8a9be"
   license "Apache-2.0"
 
   head "https://github.com/gazebosim/gz-transport.git", branch: "gz-transport13"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 ventura:  "f69eff60dc479d910943787ee9dad9fa43628d593f8158c987b965bbc776cbea"
-    sha256 monterey: "57a5f5780ee1312e205d71a1d14d073b64a8cd44d43cb41d47ad452fa3aa2ec9"
+    sha256 ventura:  "d27ba0cc0b614fc0ed90e1fcaaaa84b0c01d124b63e32313c14297d20035a12c"
+    sha256 monterey: "1ff415e3c527d42adc41f00209bbcd4698861a81e7aed0be9678ec725618d10e"
   end
 
   depends_on "doxygen" => [:build, :optional]
