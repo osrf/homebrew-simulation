384c8f768ea95d516876e97cbf86a6becf4c814a
Steve Peters <scpeters@openrobotics.org>
2024-04-11 00:44:13 -0700

    gz-launch6: revision bump for ffmpeg

--- Formula/gz-launch6.rb
+++ Formula/gz-launch6.rb
@@ -4,7 +4,7 @@ class GzLaunch6 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-launch/releases/gz-launch-6.1.0.tar.bz2"
   sha256 "7c789c85ffb422ebbc4adb6f93c9b2aa7fdd7eccd521b7895297a6b8c525acc1"
   license "Apache-2.0"
-  revision 17
+  revision 18
 
   head "https://github.com/gazebosim/gz-launch.git", branch: "gz-launch6"
 
