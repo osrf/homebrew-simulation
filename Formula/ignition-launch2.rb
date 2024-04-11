9b375e5ffc9e32e178352fa537e122b06fc0dc62
Steve Peters <scpeters@openrobotics.org>
2024-04-11 00:48:24 -0700

    ignition-launch2: revision bump for ffmpeg

--- Formula/ignition-launch2.rb
+++ Formula/ignition-launch2.rb
@@ -4,7 +4,7 @@ class IgnitionLaunch2 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-launch/releases/ignition-launch2-2.3.1.tar.bz2"
   sha256 "984e2a5df03ca220960470b6b59728edf3cd570314fbad6435b67cb26c9b7e4e"
   license "Apache-2.0"
-  revision 11
+  revision 12
 
   head "https://github.com/gazebosim/gz-launch.git", branch: "ign-launch2"
 
