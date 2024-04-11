c83f052b4eeea2160de1e7a733af23512e568453
Steve Peters <scpeters@openrobotics.org>
2024-04-11 12:06:25 -0700

    Rebuild citadel bottles for ffmpeg 7 (#2632)
    * ignition-common3: revision bump for ffmpeg
    * ignition-fuel-tools4: revision bump for ffmpeg
    * ignition-gazebo3: revision bump for ffmpeg
    * ignition-gui3: revision bump for ffmpeg
    * ignition-launch2: revision bump for ffmpeg
    * ignition-physics2: revision bump for ffmpeg
    * ignition-rendering3: revision bump for ffmpeg
    * ignition-sensors3: revision bump for ffmpeg
    * update  bottle.
    
    Co-authored-by: OSRF Build Bot <osrfbuild@osrfoundation.org>

--- Formula/ignition-physics2.rb
+++ Formula/ignition-physics2.rb
@@ -4,9 +4,15 @@ class IgnitionPhysics2 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-physics/releases/ignition-physics2-2.6.2.tar.bz2"
   sha256 "4aa0dcd1a254da63f55a93d2cc928a5ff517f19ac1603c0fb5f810dd29c70e1d"
   license "Apache-2.0"
-  revision 6
+  revision 7
 
   head "https://github.com/gazebosim/gz-physics.git", branch: "gz-physics2"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 cellar: :any, ventura:  "cbd8b95f2a1a6c28398783a22ff0631454fdd9979cf4156b50e72dd58d743084"
+    sha256 cellar: :any, monterey: "fdcd6a292848a1da7398cd5dfb5bf97a98846dc316ccd2b78dc0bb877c92fcf3"
+  end
 
   deprecate! date: "2024-12-31", because: "is past end-of-life date"
 
