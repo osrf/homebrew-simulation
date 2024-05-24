ba9bd5abce582547990d2097e161f01450bcb3a1
osrfbuild <osrfbuild@osrfoundation.org>
2024-05-24 08:50:25 -0700

    gz-gui7 7.2.2 (#2658)
    * gz-gui7: update 7.2.2
    
    * update  bottle.

--- Formula/gz-gui7.rb
+++ Formula/gz-gui7.rb
@@ -1,17 +1,16 @@
 class GzGui7 < Formula
   desc "Common libraries for robotics applications. GUI Library"
   homepage "https://github.com/gazebosim/gz-gui"
-  url "https://osrf-distributions.s3.amazonaws.com/gz-gui/releases/gz-gui-7.2.1.tar.bz2"
-  sha256 "d6a9d0a14fc535773b23e06106a58238b03c04f7551aa642d28768119272042f"
+  url "https://osrf-distributions.s3.amazonaws.com/gz-gui/releases/gz-gui-7.2.2.tar.bz2"
+  sha256 "dbfdfbb5e588d0bc6e163c554b1b5dc5397c85dda0a696ffaa79052420eb5dc0"
   license "Apache-2.0"
-  revision 20
 
   head "https://github.com/gazebosim/gz-gui.git", branch: "gz-gui7"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 ventura:  "ff666102bad59f3848b37891aef2a9cb53cc5cc441c02a42ff59c09fc343280b"
-    sha256 monterey: "f2a0544b0ff41477ceb0ec42c21195532ae6384d520b7e1af5b9c6ce3d260da7"
+    sha256 ventura:  "bd87da3510ea078da40ad52a19b633c4afcffc7d9dbc95b5b94d13f3731ac925"
+    sha256 monterey: "c861e6ddb40f6349207fe4241d6f6c7acd4d518b24845a6887778a6a616f2289"
   end
 
   depends_on "cmake" => [:build, :test]
