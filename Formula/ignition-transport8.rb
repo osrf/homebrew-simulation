afc3e6d9590639715df1818c2e580dfffd2ebe3a
Steve Peters <scpeters@openrobotics.org>
2024-08-01 12:00:45 -0700

    ignition-transport8: revision bump broken bottle

--- Formula/ignition-transport8.rb
+++ Formula/ignition-transport8.rb
@@ -4,15 +4,9 @@ class IgnitionTransport8 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-transport/releases/ignition-transport8-8.5.0.tar.bz2"
   sha256 "5edd15699e35ade5ad2f814af1f5e96a866f7908e16b55333abb23978f44d4c6"
   license "Apache-2.0"
-  revision 14
+  revision 15
 
   head "https://github.com/gazebosim/gz-transport.git", branch: "ign-transport8"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 ventura:  "3d214b310a53b7246d50039ff4e38ffafe0f4ea7ebce0a40b24d6e3c98b3e586"
-    sha256 monterey: "5ed6fff52b4c1c901868a83ebe28dea996e75528d23a5730a0466ef1ba5c7616"
-  end
 
   depends_on "doxygen" => [:build, :optional]
 
