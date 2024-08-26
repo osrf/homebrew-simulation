f7b9865be33692e7efe762b8d6c118cb6a3baa68
Ian Chen <ichen@openrobotics.org>
2024-08-26 23:31:56 +0000

    fix
    Signed-off-by: Ian Chen <ichen@openrobotics.org>

--- Formula/gz-common6.rb
+++ Formula/gz-common6.rb
@@ -18,8 +18,8 @@ class GzCommon6 < Formula
   depends_on macos: :high_sierra # c++17
   depends_on "ossp-uuid"
   depends_on "pkg-config"
-  depends_on "tinyxml2"
   depends_on "spdlog"
+  depends_on "tinyxml2"
 
   def install
     cmake_args = std_cmake_args
