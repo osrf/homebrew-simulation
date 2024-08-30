6f6ab36ab57a59399345b1a1c4ef1c2158809da4
Steve Peters <scpeters@openrobotics.org>
2024-08-30 22:11:53 +0400

    gazebo11: revision bump for broken bottle

--- Formula/gazebo11.rb
+++ Formula/gazebo11.rb
@@ -4,15 +4,9 @@ class Gazebo11 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gazebo/releases/gazebo-11.14.0.tar.bz2"
   sha256 "7e9842c046c9e0755355b274c240a8abbf4e962be7ce7b7f59194e5f4b584f45"
   license "Apache-2.0"
-  revision 28
+  revision 29
 
   head "https://github.com/osrf/gazebo.git", branch: "gazebo11"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 ventura:  "f69ff17d852c02b9793a18de7eccb4d245d3d1029cb45ce9403ace275de00bb2"
-    sha256 monterey: "467894462dc1ce5cff1135848a96c3c6df4e3be3b4425ee5a32f9fefbf8fed17"
-  end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
