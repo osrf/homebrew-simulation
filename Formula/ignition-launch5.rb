2e7eec4690c6193f8b1e506d8e7f5324522b6fb9
Steve Peters <scpeters@openrobotics.org>
2024-09-04 03:34:41 +0400

    ignition-launch5: revision bump broken bottle

--- Formula/ignition-launch5.rb
+++ Formula/ignition-launch5.rb
@@ -4,15 +4,9 @@ class IgnitionLaunch5 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/ign-launch/releases/ignition-launch5-5.3.0.tar.bz2"
   sha256 "84d356b9c85609da1bb7feda2f90ae6d1a1fd2d6713b284799d5605de42e2613"
   license "Apache-2.0"
-  revision 34
+  revision 35
 
   head "https://github.com/gazebosim/gz-launch.git", branch: "ign-launch5"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 ventura:  "af1dc142e95495f3baf19582637d2527fd8b76919d5a8faf093345ffbb5534c0"
-    sha256 monterey: "27d1d81b3789cca77e73a5a682c5807c8dd99cef7d5837bfd1a96f4e05c198c7"
-  end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
