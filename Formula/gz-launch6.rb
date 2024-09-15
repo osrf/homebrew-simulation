22a9b26ec7643d9e00d492e7888d14c107ce00de
Steve Peters <scpeters@openrobotics.org>
2024-09-15 13:48:41 -0700

    gz-launch6: revision bump broken bottle

--- Formula/gz-launch6.rb
+++ Formula/gz-launch6.rb
@@ -4,15 +4,9 @@ class GzLaunch6 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-launch/releases/gz-launch-6.1.0.tar.bz2"
   sha256 "7c789c85ffb422ebbc4adb6f93c9b2aa7fdd7eccd521b7895297a6b8c525acc1"
   license "Apache-2.0"
-  revision 29
+  revision 30
 
   head "https://github.com/gazebosim/gz-launch.git", branch: "gz-launch6"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 ventura:  "b95a68315d6af90dd94a84b709b7b551c7882d4c26ca95029021e5c06ddf5d42"
-    sha256 monterey: "403579dc1d01fb3ae82063d07af331138b7d4b4295a052fbb98ff9013417ab2d"
-  end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
