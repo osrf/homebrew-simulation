f4dc68901ac3ffee5b8af8a814610444ee4b9a96
Steve Peters <scpeters@openrobotics.org>
2024-11-18 11:50:36 -0800

    gz-plugin3: revision bump rebuild

--- Formula/gz-plugin3.rb
+++ Formula/gz-plugin3.rb
@@ -4,7 +4,7 @@ class GzPlugin3 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-plugin/releases/gz-plugin-3.0.0.tar.bz2"
   sha256 "5f69afaec39cb26224bbfb7d84f80dc814221e3606181ed0753a5d5ef7a15ee0"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/gazebosim/gz-plugin.git", branch: "gz-plugin3"
 
