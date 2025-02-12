dd297f8c8f365377552dc2a23ecff938d7bc5920
osrfbuild <osrfbuild@osrfoundation.org>
2025-02-12 02:57:40 +0100

    gz-launch7 7.1.1 (#2966)
    * update  bottle.
    
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/gz-launch7.rb
+++ Formula/gz-launch7.rb
@@ -1,17 +1,16 @@
 class GzLaunch7 < Formula
   desc "Launch libraries for robotics applications"
   homepage "https://github.com/gazebosim/gz-launch"
-  url "https://osrf-distributions.s3.amazonaws.com/gz-launch/releases/gz-launch-7.1.0.tar.bz2"
-  sha256 "320606c71b6a2cbbcab683cd8569af9145ece157d5ebd23bc80218e8d148cd09"
+  url "https://osrf-distributions.s3.amazonaws.com/gz-launch/releases/gz-launch-7.1.1.tar.bz2"
+  sha256 "e29f8b4663474cfed1364c45afa3aee8b44d816ffe1679c26c699f7c805cdffd"
   license "Apache-2.0"
-  revision 21
 
   head "https://github.com/gazebosim/gz-launch.git", branch: "gz-launch7"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 sonoma:  "6388d96976843890b82b71824920f7dfee4fe851cd839fd3dc1f1035670557c0"
-    sha256 ventura: "bc24c1565ef511b78a343d1d4642396b64a6b867d6699193827223599638e091"
+    sha256 sonoma:  "a1f57fb358d60006a10dca042cb1241993478bef8e24f98f04082a6844602e6c"
+    sha256 ventura: "4ff1b2180d50b5d2c9656a40744f30f24048c80385dbc4d20c77ad7f948b4f75"
   end
 
   depends_on "cmake" => :build
