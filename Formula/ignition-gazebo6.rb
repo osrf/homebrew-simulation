c0be78380150685e0a1981a3c10efe61dc87f3d1
Steve Peters <scpeters@openrobotics.org>
2025-03-03 11:13:08 -0800

    ignition-gazebo6: revision bump broken bottle

--- Formula/ignition-gazebo6.rb
+++ Formula/ignition-gazebo6.rb
@@ -4,14 +4,9 @@ class IgnitionGazebo6 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-sim/releases/ignition-gazebo-6.17.0.tar.bz2"
   sha256 "3a51ba77e5cbbc3d0b6b3dd44d66bfd3076bed228c8c9face9678f1bd7a51ced"
   license "Apache-2.0"
+  revision 1
 
   head "https://github.com/gazebosim/gz-sim.git", branch: "ign-gazebo6"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 sonoma:  "8ec2551ba8dc6df21b171e06e001be7b37e027db43ee7026feddb2ae80ae1f33"
-    sha256 ventura: "5ea0e079d6caa242a79aebbaab44764a58a6af969fd3b996d2ca939324d8174c"
-  end
 
   depends_on "cmake" => :build
   depends_on "pybind11" => :build
