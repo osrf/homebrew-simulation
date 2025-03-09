b19e50b7790e839bd53c1e0e093b9f04c1d19756
Steve Peters <scpeters@openrobotics.org>
2025-03-09 08:10:17 -0700

    gz-common6: revision bump broken bottle

--- Formula/gz-common6.rb
+++ Formula/gz-common6.rb
@@ -4,14 +4,9 @@ class GzCommon6 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-common/releases/gz-common-6.0.2.tar.bz2"
   sha256 "d87f805c5b79b06d610c50f4ed3123afcc6018d2e12759a18ca4131f7d4a6921"
   license "Apache-2.0"
+  revision 1
 
   head "https://github.com/gazebosim/gz-common.git", branch: "gz-common6"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any, sonoma:  "2e5a156acee13dcc126eae587cb265884b3653b427d1e9a6400cbb0f59fbbd7a"
-    sha256 cellar: :any, ventura: "d44ceb8526526a061c4207a12f0c7ac8aa9ab757fdc0aeb9538ff17b09a289d7"
-  end
 
   depends_on "assimp"
   depends_on "cmake"
