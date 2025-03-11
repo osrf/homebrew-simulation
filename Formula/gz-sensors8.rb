80e44e53f098f293cb815804afd37921f4a1a8c0
Steve Peters <scpeters@openrobotics.org>
2025-03-11 10:42:42 -0700

    gz-sensors8: revision bump rebuild

--- Formula/gz-sensors8.rb
+++ Formula/gz-sensors8.rb
@@ -4,7 +4,7 @@ class GzSensors8 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-sensors/releases/gz-sensors-8.2.2.tar.bz2"
   sha256 "9ddc16d5cab0a86f27771732f5cfcfde1efe7611f27da61176ea122273806c42"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/gazebosim/gz-sensors.git", branch: "gz-sensors8"
 
