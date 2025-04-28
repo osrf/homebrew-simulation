059e51af3a1928416f64e6ad2f2588dfd5bb0a98
Steve Peters <scpeters@openrobotics.org>
2025-04-28 21:10:14 +0000

    gz-plugin4: Bump gz-cmake and others in jetty
    Bumping gz-cmake and others except gz-tools.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

---
+++ Formula/gz-plugin4.rb
@@ -0,0 +1,69 @@
+class GzPlugin4 < Formula
+  desc "Plugin libraries for robotics applications"
+  homepage "https://github.com/gazebosim/gz-plugin"
+  url "https://github.com/gazebosim/gz-plugin.git", branch: "main"
+  version "3.999.999-0-20250428"
+  license "Apache-2.0"
+
+  head "https://github.com/gazebosim/gz-plugin.git", branch: "gz-plugin4"
+
+  depends_on "cmake"
+  depends_on "gz-cmake5"
+  depends_on "gz-tools2"
+  depends_on "gz-utils4"
+  depends_on macos: :high_sierra # c++17
+  depends_on "pkgconf"
+
+  def install
+    rpaths = [
+      rpath,
+      rpath(source: libexec/"gz/plugin4", target: lib),
+    ]
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_TESTING=OFF"
+    cmake_args << "-DCMAKE_INSTALL_RPATH=#{rpaths.join(";")}"
+
+    # Use a build folder
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    # test CLI executable
+    system libexec/"gz/plugin4/gz-plugin"
+    # build against API
+    (testpath/"test.cpp").write <<-EOS
+      #include <gz/plugin/Loader.hh>
+      int main() {
+        gz::plugin::Loader loader;
+        return loader.InterfacesImplemented().size();
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.22.1 FATAL_ERROR)
+      find_package(gz-plugin QUIET REQUIRED COMPONENTS loader)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake gz-plugin::loader)
+    EOS
+    system "pkg-config", "gz-plugin-loader"
+    cflags = `pkg-config --cflags gz-plugin-loader`.split
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lgz-plugin-loader",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+  end
+end
