45a4b6f3c07b11c3f415931dea35ec992dddaa2b
Steve Peters <scpeters@openrobotics.org>
2025-04-29 01:26:43 -0700

    sdformat16: Bump gz-cmake and others in jetty (#3028)
    Bumping gz-cmake and others except gz-tools.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>
    Co-authored-by: Ian Chen <ichen@openrobotics.org>

---
+++ Formula/sdformat16.rb
@@ -0,0 +1,105 @@
+class Sdformat16 < Formula
+  desc "Simulation Description Format"
+  homepage "http://sdformat.org"
+  url "https://github.com/gazebosim/sdformat.git", branch: "main"
+  version "15.999.999-0-20250428"
+  license "Apache-2.0"
+
+  head "https://github.com/gazebosim/sdformat.git", branch: "sdf16"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "pkgconf" => [:build, :test]
+  depends_on "pybind11" => :build
+  depends_on "python@3.12" => [:build, :test]
+  depends_on "python@3.13" => [:build, :test]
+
+  depends_on "doxygen"
+  depends_on "gz-cmake5"
+  depends_on "gz-math9"
+  depends_on "gz-tools2"
+  depends_on "gz-utils4"
+  depends_on macos: :mojave # c++17
+  depends_on "tinyxml2"
+  depends_on "urdfdom"
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.match?(/^python@3\.\d+$/) }
+  end
+
+  def python_cmake_arg(python = Formula["python@3.13"])
+    "-DPython3_EXECUTABLE=#{python.opt_libexec}/bin/python"
+  end
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_TESTING=Off"
+    cmake_args << "-DCMAKE_INSTALL_RPATH=#{rpath}"
+
+    # first build without python bindings
+    mkdir "build" do
+      system "cmake", "..", *cmake_args, "-DSKIP_PYBIND11=ON"
+      system "make", "install"
+    end
+
+    # now build only the python bindings
+    pythons.each do |python|
+      # remove @ from formula name
+      python_name = python.name.tr("@", "")
+      mkdir "build_#{python_name}" do
+        system "cmake", "../python", *cmake_args, python_cmake_arg(python)
+        system "make", "install"
+        (lib/"#{python_name}/site-packages").install Dir[lib/"python/*"]
+        rmdir prefix/"lib/python"
+      end
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <iostream>
+      #include "sdf/sdf.hh"
+      const std::string sdfString(
+        "<sdf version='1.5'>"
+        "  <model name='example'>"
+        "    <link name='link'>"
+        "      <sensor type='gps' name='mysensor' />"
+        "    </link>"
+        "  </model>"
+        "</sdf>");
+      int main() {
+        sdf::SDF modelSDF;
+        modelSDF.SetFromString(sdfString);
+        std::cout << modelSDF.ToString() << std::endl;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<-EOS
+      cmake_minimum_required(VERSION 3.22.1 FATAL_ERROR)
+      find_package(sdformat QUIET REQUIRED)
+      add_executable(test_cmake test.cpp)
+      target_link_libraries(test_cmake ${SDFormat_LIBRARIES})
+    EOS
+    system "pkg-config", "sdformat"
+    cflags = `pkg-config --cflags sdformat`.split
+    system ENV.cc, "test.cpp",
+                   *cflags,
+                   "-L#{lib}",
+                   "-lsdformat",
+                   "-lc++",
+                   "-o", "test"
+    system "./test"
+    # test building with cmake
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      system "./test_cmake"
+    end
+    # check for Xcode frameworks in bottle
+    cmd_not_grep_xcode = "! grep -rnI 'Applications[/]Xcode' #{prefix}"
+    system cmd_not_grep_xcode
+    # check python import
+    pythons.each do |python|
+      system python.opt_libexec/"bin/python", "-c", "import sdformat"
+    end
+  end
+end
