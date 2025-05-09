b0b6416933cf70cf1848c35109b89734a74e6312
Ramon Roche <mrpollo@gmail.com>
2025-05-08 19:28:32 -0700

    ogre1.9: patch for latest clang (#3029)
    macOS’s libc++ lacks std::char_traits for uint16 and unsigned int,
    which breaks OgreUTFString’s use of basic_string<code_point>.  Inject
    specializations for both uint16 (UTF-16) and uint32 (UTF-32) so that
    std::basic_string<code_point> instantiates cleanly without touching
    Ogre’s overloads or core logic.
    
    Fixes #3003.
    
    * ogre1.9: enable rpath
    * ogre1.9: revision bump
    * ogre1.9: update homepage
      Certificate has expired at ogre3d.org
    * ignition-rendering6: revision bump for ogre1.9
    * gz-rendering8: revision bump for ogre1.9
    * gz-rendering9: revision bump for ogre1.9
    * update  bottle.
    
    Signed-off-by: Ramon Roche <mrpollo@gmail.com>
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>
    Co-authored-by: OSRF Build Bot <osrfbuild@osrfoundation.org>

--- Formula/gz-rendering8.rb
+++ Formula/gz-rendering8.rb
@@ -4,13 +4,14 @@ class GzRendering8 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-rendering/releases/gz-rendering-8.2.2.tar.bz2"
   sha256 "5029db79e098ca23b95bf186c046f8996a7b0f4490e2f9e95da7aaa8eb37d130"
   license "Apache-2.0"
+  revision 1
 
   head "https://github.com/gazebosim/gz-rendering.git", branch: "gz-rendering8"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 sonoma:  "34b11dc3f19bb760c6e9bc88e10c24c87d74407250044c0cd653341844d35d35"
-    sha256 ventura: "67023f5cd9ca011d275a0eee5c60cd28e0926fe6bc997d6f518f6a15e5f5d06c"
+    sha256 sonoma:  "e6b5978c7557be90e00b2165828b80d26d7f64cceeef58843d1b4cddae33005f"
+    sha256 ventura: "ebaaf5702d914159a566134efcf98425013cedc07a3826561aba8ae42aa6eb3c"
   end
 
   depends_on "cmake" => [:build, :test]
