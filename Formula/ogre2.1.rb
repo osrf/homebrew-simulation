14ec3b9eea5b2bfbafc270180f3e444605fadd47
Steve Peters <scpeters@openrobotics.org>
2025-05-12 14:24:47 -0700

    ogre2.1: add deprecation date
    Part of gazebo-tooling/release-tools#1252.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/ogre2.1.rb
+++ Formula/ogre2.1.rb
@@ -20,6 +20,8 @@ class Ogre21 < Formula
     sha256 cellar: :any, mojave:   "f0b505985d282e7dd8b1aecc7daf51ff15c8fbb58b4b30c556fbc139ec878075"
   end
 
+  deprecate! date: "2024-12-31", because: "is past end-of-life date"
+
   depends_on "cmake" => :build
   depends_on "pkgconf" => :test
 
