77e0271d27eaf35ef5ce7b24ec8252f39279c406
Steve Peters <scpeters@openrobotics.org>
2025-05-12 08:17:28 +0000

    gz-gui8: revision bump broken bottle

--- Formula/gz-gui8.rb
+++ Formula/gz-gui8.rb
@@ -4,15 +4,9 @@ class GzGui8 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-gui/releases/gz-gui-8.4.0.tar.bz2"
   sha256 "1731b01a134afb11b1b3e049fc65e74fa7b5c50532406d3d68366d54016d5498"
   license "Apache-2.0"
-  revision 4
+  revision 5
 
   head "https://github.com/gazebosim/gz-gui.git", branch: "gz-gui8"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 sonoma:  "23ad8222f1b7c209298077ef76ba6cf89196f3f2fe79b0def3b6fd75b14544f6"
-    sha256 ventura: "53ee3d31dd4ceaac4094ac28a14a509e12a051479623b8d21d7f9595295e1b16"
-  end
 
   depends_on "cmake" => [:build, :test]
   depends_on "pkgconf" => [:build, :test]
