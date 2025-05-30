1c05440590449804de6850e00b242720c49e9a1e
Steve Peters <scpeters@openrobotics.org>
2025-05-30 19:07:46 +0000

    gz-tools3: new formula using gz-cmake
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

---
+++ Formula/gz-tools3.rb
@@ -0,0 +1,44 @@
+class GzTools3 < Formula
+  desc "Entry point for Gazebo command-line tools"
+  homepage "https://gazebosim.org"
+  url "https://github.com/gazebosim/gz-tools.git", branch: "main"
+  version "2.999.999-0-20250530"
+  license "Apache-2.0"
+
+  head "https://github.com/gazebosim/gz-tools.git", branch: "gz-tools3"
+
+  depends_on "cmake" => :build
+  depends_on "libyaml" => :test
+  depends_on "ruby" => :test
+  depends_on "gz-cmake"
+
+  conflicts_with "gazebo11", because: "both install bin/gz"
+
+  def install
+    inreplace "src/gz.in" do |s|
+      s.gsub! "@CMAKE_INSTALL_PREFIX@", HOMEBREW_PREFIX
+    end
+
+    # Use a build folder
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    mkdir testpath/"config"
+    (testpath/"config/test.yaml").write <<~EOS
+      --- # Test subcommand
+      format: 1.0.0
+      library_name: test
+      library_path: path
+      library_version: 2.0.0
+      commands:
+          - test  : Test utility
+      ---
+    EOS
+    ENV["GZ_CONFIG_PATH"] = testpath/"config/"
+    system "#{bin}/gz", "test", "--versions"
+  end
+end
