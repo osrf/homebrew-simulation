15343d79d2b38cc33f115a17c7886ece34de7d92
osrfbuild <osrfbuild@osrfoundation.org>
2026-01-28 03:33:20 +0100

    gz-cmake3 3.5.6 (#3327)
    * update bottle
    
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>

--- Formula/gz-cmake3.rb
+++ Formula/gz-cmake3.rb
@@ -1,18 +1,17 @@
 class GzCmake3 < Formula
   desc "CMake helper functions for building robotic applications"
   homepage "https://gazebosim.org"
-  url "https://osrf-distributions.s3.amazonaws.com/gz-cmake/releases/gz-cmake-3.5.5.tar.bz2"
-  sha256 "bc7b14d07e47e1783002b567e1a4267624fd09b89993101726a0ddc561b5d159"
+  url "https://osrf-distributions.s3.amazonaws.com/gz-cmake/releases/gz-cmake-3.5.6.tar.bz2"
+  sha256 "47a9104aa447ca2a307c81b3cc88314fa40db005c0a5881417412ff7e98368e8"
   license "Apache-2.0"
 
   # head "https://github.com/gazebosim/gz-cmake.git", branch: "gz-cmake3"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "00b011d9b59df8d8a9e329b196f2f80b4bf25ef29a478d9e8d6c6c5e48bbe380"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d397ace2e8797c364e6c31d3965920bc588f8dcf77ad8f22789663ac8eaa0b91"
-    sha256 cellar: :any_skip_relocation, sonoma:        "db13764b26729818b3d2018bedbac3bdc74b887a66b8570fe75de07754139a28"
-    sha256 cellar: :any_skip_relocation, ventura:       "9e17ba417e9a25b7e11c9f5e3e98b1ff4535a05b7a93502fc0b8c9e1d13222ab"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3c891b773ff8e95d57a4d8cba77fe77cf66d725957eb6f3bf3b1ea6f02dfda39"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b159cc8169ad41cd9c4471b5611fdd9447a82b30e13e8c70b8dc2d3b65a6da73"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7789962ec4814892b2a37c4763fc0540917785936e0ef1487d440a0536e8b977"
   end
 
   depends_on "cmake"
