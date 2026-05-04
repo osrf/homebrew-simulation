431b9e8d03b9fb1be0ca28d8776a77094736122b
osrfbuild <hosting+osrfbuild@openrobotics.org>
2026-05-04 05:11:07 +0200

    Remove bottles broken by jsoncpp 1.9.7 (#3427)
    * gz-fuel-tools11: revision bump broken bottle
    * gz-launch9: revision bump broken bottle
    * gz-sim10: revision bump broken bottle
    * gz-fuel-tools10: revision bump broken bottle
    * gz-launch8: revision bump broken bottle
    * gz-sim9: revision bump broken bottle
    * gz-fuel-tools9: revision bump broken bottle
    * gz-launch7: revision bump broken bottle
    * gz-sim8: revision bump broken bottle
    * ignition-fuel-tools7: revision bump broken bottle
    * ignition-gazebo6: revision bump broken bottle
    * ignition-launch5: revision bump broken bottle

--- Formula/gz-sim10.rb
+++ Formula/gz-sim10.rb
@@ -4,15 +4,9 @@ class GzSim10 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-sim/releases/gz-sim-10.2.0.tar.bz2"
   sha256 "9621541f8fcd29a50f677c0fe5f7e3cd9ed2f005dd2bb64df75c87073a0e5203"
   license "Apache-2.0"
+  revision 1
 
   head "https://github.com/gazebosim/gz-sim.git", branch: "gz-sim10"
-
-  bottle do
-    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 arm64_sequoia: "883c8a2e08b55fbe8873118e1206a3d8ed26ab3298f2c0782205c8127bf3f8f0"
-    sha256 arm64_sonoma:  "9a30b21885a090ef5de206c5ec99f6952c911b2d3255c4c6df1e74806f19bd1a"
-    sha256 sonoma:        "35cb4fbc867a18a72596507c3676d5cde746eb4429237d66c62efbf3e7d299c2"
-  end
 
   depends_on "cmake" => :build
   depends_on "pybind11" => :build
