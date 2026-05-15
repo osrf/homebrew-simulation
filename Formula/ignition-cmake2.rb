f2d9414b1e0caae384ce8e0ee7fa261ef710e0b2
osrfbuild <hosting+osrfbuild@openrobotics.org>
2026-05-15 14:22:08 +0200

    ignition-cmake2 2.18.0 (#3443)
    * update bottle
    
    Co-authored-by: Steve Peters <scpeters@intrinsic.ai>

--- Formula/ignition-cmake2.rb
+++ Formula/ignition-cmake2.rb
@@ -1,17 +1,17 @@
 class IgnitionCmake2 < Formula
   desc "CMake helper functions for building robotic applications"
   homepage "https://ignitionrobotics.org"
-  url "https://osrf-distributions.s3.amazonaws.com/gz-cmake/releases/ignition-cmake-2.17.3.tar.bz2"
-  sha256 "8743d3f0149b5f16c7c95e5b66934475eff698ac5ee9af52a469adcd56085fb9"
+  url "https://osrf-distributions.s3.amazonaws.com/gz-cmake/releases/ignition-cmake-2.18.0.tar.bz2"
+  sha256 "d404567bbb59b61bd676f0c8571b7eac420f6ddd7b8e2a5f96fcaee349560dd8"
   license "Apache-2.0"
 
   # head "https://github.com/gazebosim/gz-cmake.git", branch: "ign-cmake2"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c21f94c7c14537acd2c4d15115505da7a095737db4e25e9afe0dcef69ca82939"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "91d04c5c86c128c5a706dbf3b9242c799def31e08852f436c43b1296bc7de5ad"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f44d5c93418f1781c9cfe80139af9dac66b027bd2491e162bb40a27a71206f6b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7a1cf71c683c9a92f211ec620c172ab1458185392fce41cf88fc2550e54fdc93"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "68558e278c1ea762409edd638368515143f4021bf3a4f66a120174c752beb5de"
+    sha256 cellar: :any_skip_relocation, sonoma:        "304d34804ccc19aff414e88eb0f5585f777a7a2b490672181c169d33ad96885a"
   end
 
   depends_on "cmake"
