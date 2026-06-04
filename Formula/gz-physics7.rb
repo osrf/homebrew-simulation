97af91c039c3b9dd3d275abc1eafce628f309d52
osrfbuild <hosting+osrfbuild@openrobotics.org>
2026-06-04 22:52:12 +0200

    gz-physics7 7.8.0 (#3457)
    * update bottle
    
    Co-authored-by: Steve Peters <scpeters@intrinsic.ai>

--- Formula/gz-physics7.rb
+++ Formula/gz-physics7.rb
@@ -1,15 +1,15 @@
 class GzPhysics7 < Formula
   desc "Physics library for robotics applications"
   homepage "https://github.com/gazebosim/gz-physics"
-  url "https://osrf-distributions.s3.amazonaws.com/gz-physics/releases/gz-physics-7.7.0.tar.bz2"
-  sha256 "a863955588570a66a628fd128d71599e461b0821ff6d1b26b03c847722ba2124"
+  url "https://osrf-distributions.s3.amazonaws.com/gz-physics/releases/gz-physics-7.8.0.tar.bz2"
+  sha256 "120b7ea6e1dd9c34515948de3c69ab5e27ab098e56379a3ef61a414c6e606927"
   license "Apache-2.0"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 arm64_sequoia: "28dbef4b6f036639ba13b3b282789a550f3e1b07affd9672c796e4633f73d958"
-    sha256 arm64_sonoma:  "1aa15b0ffc2549564023354e417ff1a1efffdd668ee69ff9faf80819f8216a39"
-    sha256 sonoma:        "7074792a2466e590cb20fba3518dd2bce3ae2abaf7590378c962f98644595024"
+    sha256 arm64_sequoia: "42c591daa8aabad1b5013fb6d5a27edc36327cea11322eb8dc069a18ffe2faa7"
+    sha256 arm64_sonoma:  "fa6040adadcca526bd551666a37691aa2de7e9510fef31663a9c3f01f90b7def"
+    sha256 sonoma:        "d0d6cafdec1eabc9bc109d50e05f99f32f26dd500bda7b29e403e8dbf6d6dc37"
   end
 
   # head "https://github.com/gazebosim/gz-physics.git", branch: "gz-physics7"
