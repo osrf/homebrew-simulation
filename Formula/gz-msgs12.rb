7d9bfe2b90aedcbb6380c5f55b426feda7a7caa1
osrfbuild <hosting+osrfbuild@openrobotics.org>
2026-07-14 01:36:35 +0200

    gz-msgs12 12.0.2 (#3492)
    * update bottle

--- Formula/gz-msgs12.rb
+++ Formula/gz-msgs12.rb
@@ -1,18 +1,17 @@
 class GzMsgs12 < Formula
   desc "Middleware protobuf messages for robotics"
   homepage "https://gazebosim.org"
-  url "https://osrf-distributions.s3.amazonaws.com/gz-msgs/releases/gz-msgs-12.0.1.tar.bz2"
-  sha256 "f43316821204c37836a23640fdfe161241ec7eafd683b1e721a8ed879301ce75"
+  url "https://osrf-distributions.s3.amazonaws.com/gz-msgs/releases/gz-msgs-12.0.2.tar.bz2"
+  sha256 "cca452d55937998330801fbef97e3cfdb6298e6807bb53b64fbd826266950bb6"
   license "Apache-2.0"
-  revision 10
 
   head "https://github.com/gazebosim/gz-msgs.git", branch: "gz-msgs12"
 
   bottle do
     root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
-    sha256 arm64_sequoia: "d086078ccb36b0717afaf3a0f1883c9b7a251f2c60a3ad02c1df6cb175cfc5bd"
-    sha256 arm64_sonoma:  "5584a8100d29bc388a0ab491593cbcf31926e35a91a235d2d3228ed78d27e549"
-    sha256 sonoma:        "8e007b99be2ec190b2269a8e56a2d06d6a18ad426a4797dd12ad41def7f94d4c"
+    sha256 arm64_sequoia: "84e756dba146f86991d750f37ecdac4291eb325cebb5b7d395a7a027f8eb5143"
+    sha256 arm64_sonoma:  "6898ec58c33bbae040b57cd8f869461315b95458a28682fccfd0424444d899b0"
+    sha256 sonoma:        "608b6c9735abf8da7a6a7c1dcdb7a89ca27c198083969f439c36e38344aee3ed"
   end
 
   depends_on "python@3.12" => [:build, :test]
