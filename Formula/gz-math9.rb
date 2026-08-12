abc71d4fadc82abb3ffdfe1f6c40e64a467ba913
osrfbuild <hosting+osrfbuild@openrobotics.org>
2026-08-12 04:41:57 +0200

    Jetty: rebuild for pybind11 (#3516)
    * gz-math9: revision bump rebuild
    * gz-sim10: revision bump rebuild
    * gz-transport15: revision bump rebuild
    * sdformat16: revision bump rebuild
    * update bottle

--- Formula/gz-math9.rb
+++ Formula/gz-math9.rb
@@ -4,9 +4,16 @@ class GzMath9 < Formula
   url "https://osrf-distributions.s3.amazonaws.com/gz-math/releases/gz-math-9.2.0.tar.bz2"
   sha256 "0fd6d0b992c16b60ab9c4dae29f10abb389557ec7ccd3b9450b5f4a66ee52140"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   head "https://github.com/gazebosim/gz-math.git", branch: "gz-math9"
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 cellar: :any, arm64_sequoia: "5b22d8f52cacb31f0a511d3e21ea575aebe77a56c3e12c48364dcd645883d2bc"
+    sha256 cellar: :any, arm64_sonoma:  "f697a6127e49d049fc877468f8129126d427bac17464dbe575648698ed0f1318"
+    sha256 cellar: :any, sonoma:        "4b398176e1970a07bb7b90dd5d31d833837587834cd1e11760c31990eed7762a"
+  end
 
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
