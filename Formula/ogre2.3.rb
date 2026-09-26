3e5111e77e9c3f891bc8b66b637448d52c525b8b
Steve Peters <scpeters@intrinsic.ai>
2026-09-25 20:36:22 -0400

    ogre2.3: revision bump rebuild after rename (#3582)
    * update bottle
    
    Co-authored-by: OSRF Build Bot <osrfbuild@osrfoundation.org>

--- Formula/ogre2.3.rb
+++ Formula/ogre2.3.rb
@@ -4,6 +4,12 @@ class Ogre23 < Formula
   url "https://github.com/OGRECave/ogre-next/archive/refs/tags/v2.3.3.tar.gz"
   sha256 "92ce7765d892d6424df3d8d4a56a8fc0b2f4f91c216b1b1d5b231caa9abaaa38"
   license "MIT"
+  revision 1
+
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-simulation"
+    sha256 cellar: :any, arm64_sequoia: "ccf561925bf1cef8b31afbb0d9a1adefb9d21cff28d245c75bc27f90908d87ea"
+  end
 
   # head "https://github.com/OGRECave/ogre-next.git", branch: "v2-3"
 
